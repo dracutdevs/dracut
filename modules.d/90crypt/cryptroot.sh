538a94d9e55653198ed1192dc035defc4292df25
Harald Hoyer <harald@redhat.com>
2009-03-05 14:20:48 +0100

    use dash instead of underscore for udev control commands

--- modules.d/90crypt/cryptroot.sh
+++ modules.d/90crypt/cryptroot.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 [ -s /cryptroot ] && { 
-    udevadm control --stop_exec_queue
+    udevadm control --stop-exec-queue
     while read cryptopts; do
        (   exec >/dev/console 2>&1 </dev/console
            /sbin/cryptsetup luksOpen $cryptopts || emergency_shell
        )
     done </cryptroot
     >/cryptroot
-    udevadm control --start_exec_queue
+    udevadm control --start-exec-queue
     udevadm settle --timeout=30
 }
