fa0115c3c06176bd0fdfcd8966021979ec84f3d7
Victor Lowther <victor.lowther@gmail.com>
2009-03-19 09:59:06 -0500

    Unlock our encrypted root in a udev rule.

--- modules.d/90crypt/cryptroot.sh
+++ modules.d/90crypt/cryptroot.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-[ -s /cryptroot ] && { 
-    udevadm control --stop_exec_queue
-    while read cryptopts; do
-       (   exec >/dev/console 2>&1 </dev/console
-	   set $cryptopts
-	   [ -b /dev/mapper/$2 ] || ( /sbin/cryptsetup luksOpen $cryptopts || emergency_shell )
-       )
-    done </cryptroot
-    >/cryptroot
-    udevadm control --start_exec_queue
-    udevadm settle --timeout=30
-}
+exec >/dev/console 2>&1 </dev/console
+[ -b /dev/mapper/$2 ] && exit 0
+/sbin/cryptsetup -T 3 -t 30 luksOpen $1 $2
