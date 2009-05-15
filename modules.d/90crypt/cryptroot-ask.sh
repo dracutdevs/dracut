b6e0fea80c4aef66421af74dc1d29154cdae0264
Harald Hoyer <harald@redhat.com>
2009-05-15 12:48:11 +0200

    change file descriptor for flock locking
    dash does not like filedescriptors > 9

--- modules.d/90crypt/cryptroot-ask.sh
+++ modules.d/90crypt/cryptroot-ask.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 [ -b /dev/mapper/$2 ] && exit 0
-(
-	flock -s 200
+{
+	flock -s 9
 	/sbin/cryptsetup luksOpen -T1 $1 $2 </dev/console >/dev/console 2>&1
-) 200>/.console.lock
+} 9>/.console.lock
 
