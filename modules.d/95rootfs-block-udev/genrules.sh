e419faf652b7323963df00046b0fa6109ea6d12e
Harald Hoyer <harald@redhat.com>
2009-05-15 12:49:25 +0200

    dash echo does not support -e option
    use /bin/echo, instead of dash builtin echo
    do not use "sh -c"

--- modules.d/95rootfs-block-udev/genrules.sh
+++ modules.d/95rootfs-block-udev/genrules.sh
@@ -1,15 +1,14 @@
 
 resume=$(getarg resume=) && ! getarg noresume && [ -b "$resume" ] && {
-    # parsing the output of ls is Bad, but until there is a better way...
     (
-    	echo -e 'KERNEL=="'${resume#/dev/}'", RUN+="/bin/sh -c \047 echo %M:%m > /sys/power/resume \047 "'
-    	echo -e 'SYMLINK=="'${resume#/dev/}'", RUN+="/bin/sh -c \047 echo %M:%m > /sys/power/resume \047 "'
+    	/bin/echo -e 'KERNEL=="'${resume#/dev/}'", RUN+="/bin/echo %M:%m > /sys/power/resume"'
+    	/bin/echo -e 'SYMLINK=="'${resume#/dev/}'", RUN+="/bin/echo %M:%m > /sys/power/resume"'
     ) >> /etc/udev/rules.d/99-resume.rules
 }
 
 (
- echo -e 'KERNEL=="'${root#/dev/}'", RUN+="/bin/sh -c \047 mount '$fstype' -o '$rflags' '$root' '$NEWROOT' \047 " '
- echo -e 'SYMLINK=="'${root#/dev/}'", RUN+="/bin/sh -c \047 mount '$fstype' -o '$rflags' '$root' '$NEWROOT' \047 " '
+ echo 'KERNEL=="'${root#/dev/}'", RUN+="/bin/mount '$fstype' -o '$rflags' '$root' '$NEWROOT'" '
+ echo 'SYMLINK=="'${root#/dev/}'", RUN+="/bin/mount '$fstype' -o '$rflags' '$root' '$NEWROOT'" '
 ) >> /etc/udev/rules.d/99-mount.rules
 
 
