32f32c14b6e39190f9ad71f07d2f76b96b49537e
Harald Hoyer <harald@redhat.com>
2009-05-18 15:22:33 +0200

    fixed 40network module

--- modules.d/40network/run-dhclient.sh
+++ modules.d/40network/run-dhclient.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 for i in /net.*.dhcp; do
-    [ "$dev" = "/net.*.dhcp" ] && break
-    dev=${i#net.}; dev=${i%.dhcp}
+    [ "$dev" = '/net.*.dhcp' ] && break
+    dev=${i#/net.}; dev=${dev%.dhcp}
     [ -f "/net.$dev.up" ] && continue
-    dhclient -1 -q $dev &
+    dhclient  -R 'subnet-mask,broadcast-address,time-offset,routers,domain-name,domain-name-servers,host-name,nis-domain,nis-servers,ntp-servers,root-path' -1 -q $dev &
 done
 wait
     
