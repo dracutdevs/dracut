d4244cb25bb9fd72854ae8da313a5a40ce02aec4
Harald Hoyer <harald@redhat.com>
2009-05-18 19:55:58 +0200

    support normal nfs root definition syntax

--- modules.d/95NFS/nfs-mount-root.sh
+++ modules.d/95NFS/nfs-mount-root.sh
@@ -1,21 +1,29 @@
 #!/bin/sh
 set -x
-
-for dev in /net.*.dhcpopts; do 
-    if [ -f "$dev" ]; then 
-	. "$dev"
-	[ -n "$new_root_path" ] && nfsroot="$new_root_path"
-	if [ ! -s /.resume -a "$nfsroot" ]; then
-	    if [ "${nfsroot#nfs://}" != "$nfsroot" ]; then
-		nfsroot="${nfsroot#nfs://}"
-		nfsroot="${nfsroot/\//:/}"
-		#
-		#modprobe nfs
-		#
-		# start rpc.statd ??
-		mount -t nfs "$nfsroot" -o nolock "$NEWROOT" && ROOTFS_MOUNTED=yes
+if [ "$root" = "dhcp" ]; then 
+    for dev in /net.*.dhcpopts; do 
+	if [ -f "$dev" ]; then 
+	    . "$dev"
+	    [ -n "$new_root_path" ] && nfsroot="$new_root_path"
+	    if [ ! -s /.resume -a "$nfsroot" ]; then
+		if [ "${nfsroot#nfs://}" != "$nfsroot" ]; then
+		    nfsroot="${nfsroot#nfs://}"
+		    nfsroot="${nfsroot/\//:/}"
+		fi
 	    fi
 	fi
-    fi
-done
+    done
+fi
 
+if [ "${root#/dev/}" = "$root" -a "${root/:\///}" != "$root" ]; then
+    nfsroot="$root"
+fi
+
+
+if [ -n "$nfsroot" ]; then
+    #
+    # modprobe nfs 
+    #
+    # start rpc.statd ??
+    mount -t nfs "$nfsroot" -o nolock "$NEWROOT" && ROOTFS_MOUNTED=yes
+fi
