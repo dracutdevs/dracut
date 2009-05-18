b80a6fda4001fa690c4fdeeb1bf82c4f6061e348
Harald Hoyer <harald@redhat.com>
2009-05-18 15:31:07 +0200

    fixed wildcard test of run-dhclient.sh

--- modules.d/40network/run-dhclient.sh
+++ modules.d/40network/run-dhclient.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for i in /net.*.dhcp; do
-    [ "$dev" = '/net.*.dhcp' ] && break
+    [ "$i" = '/net.*.dhcp' ] && break
     dev=${i#/net.}; dev=${dev%.dhcp}
     [ -f "/net.$dev.up" ] && continue
     dhclient  -R 'subnet-mask,broadcast-address,time-offset,routers,domain-name,domain-name-servers,host-name,nis-domain,nis-servers,ntp-servers,root-path' -1 -q $dev &
