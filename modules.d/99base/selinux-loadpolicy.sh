76434898adfb2ea43c2b0291af70dfe260dd8bed
Warren Togami <wtogami@redhat.com>
2009-05-21 14:34:38 -0400

    Do not attempt to load policy if selinux-policy is not installed.

--- modules.d/99base/selinux-loadpolicy.sh
+++ modules.d/99base/selinux-loadpolicy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FIXME: load selinux policy.  this should really be done after we switchroot 
-[ -x "$NEWROOT/usr/sbin/load_policy" ] && {
+[ -x "$NEWROOT/usr/sbin/load_policy" ] && [ -x "$NEWROOT/etc/sysconfig/selinux" ] {
     chroot $NEWROOT /usr/sbin/load_policy -i
     if [ $? -eq 3 ]; then
 	echo "Initial SELinux policy load failed and enforcing mode requested."
