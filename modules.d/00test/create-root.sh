9335cb383a9aba49c29dff9b5df4c7184df40c9b
Victor Lowther <victor.lowther@gmail.com>
2009-05-20 19:55:06 -0700

    We create our test image with root as an LVM menber.
    We purport to support booting off an encrypted LVM PV, may as well test it.

--- modules.d/00test/create-root.sh
+++ modules.d/00test/create-root.sh
@@ -5,12 +5,10 @@ sfdisk /dev/sda <<EOF
 ;
 ;
 EOF
-cryptsetup -q luksFormat /dev/sda1 <<EOF
-test
-EOF
-cryptsetup luksOpen /dev/sda1 dracut_crypt_test <<EOF
-test
-EOF
+echo -n test >keyfile
+cryptsetup -q luksFormat /dev/sda1 /keyfile
+echo "The passphrase is test"
+cryptsetup luksOpen /dev/sda1 dracut_crypt_test </keyfile
 lvm pvcreate -ff  -y /dev/mapper/dracut_crypt_test
 lvm vgcreate dracut /dev/mapper/dracut_crypt_test
 lvm lvcreate -l 100%FREE -n root dracut
