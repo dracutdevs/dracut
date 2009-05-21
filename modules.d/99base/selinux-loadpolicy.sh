4ae46e575d5147b6b4f19a7256158d23dbd4935d
Warren Togami <wtogami@redhat.com>
2009-05-21 15:16:44 -0400

    Oops, make it work

--- modules.d/99base/selinux-loadpolicy.sh
+++ modules.d/99base/selinux-loadpolicy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 # FIXME: load selinux policy.  this should really be done after we switchroot 
-[ -x "$NEWROOT/usr/sbin/load_policy" ] && [ -x "$NEWROOT/etc/sysconfig/selinux" ] {
+if [ -x "$NEWROOT/usr/sbin/load_policy" ] && [ -e "$NEWROOT/etc/sysconfig/selinux" ]; then
     chroot $NEWROOT /usr/sbin/load_policy -i
     if [ $? -eq 3 ]; then
 	echo "Initial SELinux policy load failed and enforcing mode requested."
@@ -8,4 +8,4 @@
 	sleep 100d
 	exit 1
     fi
-}
+fi
