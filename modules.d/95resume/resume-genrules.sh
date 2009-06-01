7132e6a34677691d428967462bbe7134fb2cdf52
David Dillow <dave@thedillows.org>
2009-06-01 19:46:43 -0400

    Remove dependency on /bin/echo
    Actually cut the last dependency on //bin/echo for resume.

--- modules.d/95resume/resume-genrules.sh
+++ modules.d/95resume/resume-genrules.sh
@@ -1,8 +1,8 @@
 if [ -n "$resume" ]; then
     (
-    printf 'KERNEL=="%s", RUN+="/bin/echo %%M:%%m > /sys/power/resume"\n' \
+    printf "KERNEL==\"%s\", RUN+=\"/bin/sh -c 'echo %%M:%%m > /sys/power/resume'\"\n" \
 		${resume#/dev/}
-    printf 'SYMLINK=="%s", RUN+="/bin/echo %%M:%%m > /sys/power/resume"\n' \
+    printf "SYMLINK==\"%s\", RUN+=\"/bin/sh -c 'echo %%M:%%m > /sys/power/resume'\"\n" \
 		${resume#/dev/}
     ) >> /etc/udev/rules.d/99-resume.rules
 fi
