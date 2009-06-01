9810ce7e38ec1ba52a9c2f5f5047ea779d1006a3
David Dillow <dave@thedillows.org>
2009-06-01 00:25:09 -0400

    resume: add support for labeled resume devices
    I noticed when doing the block root= parser that resume
    didn't support labeled resume devices. Fix that up and
    get rid of a dependency on /bin/echo in the process.

--- modules.d/95resume/parse-resume.sh
+++ modules.d/95resume/parse-resume.sh
@@ -5,3 +5,11 @@ if resume=$(getarg resume=) && ! getarg noresume; then
 else
     unset resume
 fi
+
+case "$resume" in
+    LABEL=*)
+	resume="$(echo $resume | sed 's,/,\\x2f,g')"
+	resume="/dev/disk/by-label/${resume#LABEL=}" ;;
+    UUID=*)
+	resume="/dev/disk/by-uuid/${resume#UUID=}" ;;
+esac
--- modules.d/95resume/resume-genrules.sh
+++ modules.d/95resume/resume-genrules.sh
@@ -1,6 +1,8 @@
-resume=$(getarg resume=) && ! getarg noresume  && {
-(
-	/bin/echo -e 'KERNEL=="'${resume#/dev/}'", RUN+="/bin/echo %M:%m > /sys/power/resume"'
-	/bin/echo -e 'SYMLINK=="'${resume#/dev/}'", RUN+="/bin/echo %M:%m > /sys/power/resume"'
-) >> /etc/udev/rules.d/99-resume.rules
-}
+if [ -n "$resume" ]; then
+    (
+    printf 'KERNEL=="%s", RUN+="/bin/echo %%M:%%m > /sys/power/resume"\n' \
+		${resume#/dev/}
+    printf 'SYMLINK=="%s", RUN+="/bin/echo %%M:%%m > /sys/power/resume"\n' \
+		${resume#/dev/}
+    ) >> /etc/udev/rules.d/99-resume.rules
+fi
