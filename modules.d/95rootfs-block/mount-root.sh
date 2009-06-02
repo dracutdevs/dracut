cff3457e328d172a85f865370822fa3feaee0aa9
Harald Hoyer <harald@redhat.com>
2009-06-02 09:25:24 +0200

    fixed fstype mount options

--- modules.d/95rootfs-block/mount-root.sh
+++ modules.d/95rootfs-block/mount-root.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 if [ ! -s /.resume -a -n "$root" -a -z "${root%%block:*}" ]; then
-    mount ${fstype:--t auto} -o "$rflags" "${root#block:}" "$NEWROOT" && ROOTFS_MOUNTED=yes
+    mount -t ${fstype:-auto} -o "$rflags" "${root#block:}" "$NEWROOT" && ROOTFS_MOUNTED=yes
 fi
