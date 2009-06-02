ebd5615489d017ac232550188133cd7f15178c15
David Dillow <dave@thedillows.org>
2009-06-01 21:43:07 -0400

    strip block: off root= when using the mount hooks
    I'm not sure they are still used, but if so they need to
    trim the prefix.

--- modules.d/95rootfs-block/mount-root.sh
+++ modules.d/95rootfs-block/mount-root.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-if [ ! -s /.resume -a "$root" ]; then
-    mount ${fstype:--t auto} -o "$rflags" "$root" "$NEWROOT" && ROOTFS_MOUNTED=yes
+if [ ! -s /.resume -a -n "$root" -a -z "${root%%block:*}" ]; then
+    mount ${fstype:--t auto} -o "$rflags" "${root#block:}" "$NEWROOT" && ROOTFS_MOUNTED=yes
 fi
