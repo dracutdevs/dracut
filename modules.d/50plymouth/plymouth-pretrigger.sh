27a101f47c754c2dd2b3286cd13da594e1134b20
Harald Hoyer <harald@redhat.com>
2009-06-03 17:31:48 +0200

    simplify plymouth startup

--- modules.d/50plymouth/plymouth-pretrigger.sh
+++ modules.d/50plymouth/plymouth-pretrigger.sh
@@ -1,7 +1,16 @@
 #!/bin/sh
 
 # first trigger graphics subsystem
-udevadm trigger --subsystem-match=graphics >/dev/null 2>&1
+udevadm trigger --subsystem-match=graphics --subsystem-match=tty >/dev/null 2>&1
 udevadm settle --timeout=30 >/dev/null 2>&1
+[ -c /dev/null ] || mknod /dev/null c 1 3
+[ -c /dev/zero ] || mknod /dev/zero c 1 5
+[ -c /dev/systty ] || mknod /dev/systty c 4 0
+[ -c /dev/fb ] || mknod /dev/fb c 29 0
+[ -c /dev/hvc0 ] || mknod /dev/hvc0 c 229 0
+
+[ -x /bin/plymouthd ] && /bin/plymouthd
+
+/lib/udev/console_init tty0
 /bin/plymouth --show-splash
 
