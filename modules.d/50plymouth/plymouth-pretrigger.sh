91b1157500b0df8507748dd5c55546693ec832c1
Harald Hoyer <harald@redhat.com>
2009-06-04 13:07:01 +0200

    add support to specify additional udev trigger options

--- modules.d/50plymouth/plymouth-pretrigger.sh
+++ modules.d/50plymouth/plymouth-pretrigger.sh
@@ -2,7 +2,11 @@
 
 # first trigger graphics subsystem
 udevadm trigger --attr-match=class=0x030000
+# first trigger graphics and tty subsystem
 udevadm trigger --subsystem-match=graphics --subsystem-match=tty >/dev/null 2>&1
+# add nomatch for full trigger
+echo " --subsystem-nomatch=graphics --subsystem-nomatch=tty " >> /tmp/udevtriggeropts
+
 udevadm settle --timeout=30 >/dev/null 2>&1
 [ -c /dev/null ] || mknod /dev/null c 1 3
 [ -c /dev/zero ] || mknod /dev/zero c 1 5
