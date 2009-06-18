e2ab42236b875d15d28f1d88f0b09a82536f0823
Harald Hoyer <harald@redhat.com>
2009-06-18 10:03:51 +0200

    resume from any suspend partition
    If not "noresume" is specified and no explicit "resume=" given, try to resume
    from any partition with type "suspend"

--- modules.d/95resume/resume-genrules.sh
+++ modules.d/95resume/resume-genrules.sh
@@ -5,4 +5,7 @@ if [ -n "$resume" ]; then
     printf "SYMLINK==\"%s\", RUN+=\"/bin/sh -c 'echo %%M:%%m > /sys/power/resume'\"\n" \
 		${resume#/dev/}
     ) >> /etc/udev/rules.d/99-resume.rules
+elif  ! getarg noresume; then
+    echo "SUBSYSTEM==\"block\", ACTION==\"add\", ENV{ID_FS_TYPE}==\"suspend\", RUN+=\"/bin/sh -c 'echo %M:%m > /sys/power/resume'\"" \
+    >> /etc/udev/rules.d/99-resume.rules
 fi
