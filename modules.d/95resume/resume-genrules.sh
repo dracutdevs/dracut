a5cb5df1892bd54b3c142b22a8ff04ff34598d8c
Harald Hoyer <harald@redhat.com>
2009-06-18 13:57:14 +0200

    also resume from "swsuspend" partitions

--- modules.d/95resume/resume-genrules.sh
+++ modules.d/95resume/resume-genrules.sh
@@ -1,11 +1,15 @@
 if [ -n "$resume" ]; then
-    (
+    {
     printf "KERNEL==\"%s\", RUN+=\"/bin/sh -c 'echo %%M:%%m > /sys/power/resume'\"\n" \
-		${resume#/dev/}
+		${resume#/dev/};
     printf "SYMLINK==\"%s\", RUN+=\"/bin/sh -c 'echo %%M:%%m > /sys/power/resume'\"\n" \
-		${resume#/dev/}
-    ) >> /etc/udev/rules.d/99-resume.rules
+		${resume#/dev/};
+    } >> /etc/udev/rules.d/99-resume.rules
 elif  ! getarg noresume; then
-    echo "SUBSYSTEM==\"block\", ACTION==\"add\", ENV{ID_FS_TYPE}==\"suspend\", RUN+=\"/bin/sh -c 'echo %M:%m > /sys/power/resume'\"" \
-    >> /etc/udev/rules.d/99-resume.rules
+    {
+    echo "SUBSYSTEM==\"block\", ACTION==\"add\", ENV{ID_FS_TYPE}==\"suspend\"," \
+         " RUN+=\"/bin/sh -c 'echo %M:%m > /sys/power/resume'\"";
+    echo "SUBSYSTEM==\"block\", ACTION==\"add\", ENV{ID_FS_TYPE}==\"swsuspend\"," \
+         " RUN+=\"/bin/sh -c 'echo %M:%m > /sys/power/resume'\"";
+    } >> /etc/udev/rules.d/99-resume.rules
 fi
