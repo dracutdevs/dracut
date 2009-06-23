d0d1ea3d875281774f7f455ad2bb2e0aa7b5c974
Harald Hoyer <harald@redhat.com>
2009-06-23 07:47:42 +0200

    prompt the crypto disk, when asking for a password
    also check, if root is already mounted and flock against
    console activities.

--- modules.d/50plymouth/cryptroot-ask.sh
+++ modules.d/50plymouth/cryptroot-ask.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
-[ -b /dev/mapper/$2 ] || exec /bin/plymouth ask-for-password --command="/sbin/cryptsetup luksOpen -T1 $1 $2"
+# do not ask, if we already have root
+[ -f /sysroot/proc ] && exit 0
+# check if destination already exists
+[ -b /dev/mapper/$2 ] && exit 0
+
+# flock against other interactive activities
+{ flock -s 9; 
+/bin/plymouth ask-for-password --prompt "$1 is password protected" --command="/sbin/cryptsetup luksOpen -T1 $1 $2"
+} 9>/.console.lock
+
+exit 0
 
