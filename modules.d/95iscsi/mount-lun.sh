4ce1991856e33d3aa6a3f8483ca790282dc86b48
Harald Hoyer <harald@redhat.com>
2009-07-02 11:40:05 +0200

    let iSCSI try to mount the complete LUN specified

---
+++ modules.d/95iscsi/mount-lun.sh
@@ -0,0 +1,14 @@
+if [ -z $iscsi_lun ]; then
+    iscsi_lun=0
+fi
+NEWROOT=${NEWROOT:-"/sysroot"}
+
+for disk in /dev/disk/by-path/*-iscsi-*-$iscsi_lun; do
+   if mount -t ${fstype:-auto} -o "$rflags" $disk $NEWROOT; then
+       if [ ! -d /sysroot/proc ]; then
+          umount $disk
+          continue
+       fi
+       break
+   fi
+done
--- test/TEST-30-ISCSI/create-root.sh
+++ test/TEST-30-ISCSI/create-root.sh
@@ -4,27 +4,9 @@ for x in 63-luks.rules 64-lvm.rules 70-mdadm.rules 99-mount-rules; do
     > "/etc/udev/rules.d/$x"
 done
 udevadm control --reload-rules
-# save a partition at the beginning for future flagging purposes
-sfdisk -C 640 -H 2 -S 32 -L /dev/sda <<EOF
-,,
-EOF
-#mdadm --create /dev/md0 --run --auto=yes --level=5 --raid-devices=3 /dev/sda2 /dev/sda3 /dev/sda4
-# wait for the array to finish initailizing, otherwise this sometimes fails
-# randomly.
-#mdadm -W /dev/md0
-#echo -n test >keyfile
-#cryptsetup -q luksFormat /dev/sda1 /keyfile
-#echo "The passphrase is test"
-#cryptsetup luksOpen /dev/sda1 dracut_crypt_test </keyfile && \
-#lvm pvcreate -ff  -y /dev/mapper/dracut_crypt_test && \
-#lvm vgcreate dracut /dev/mapper/dracut_crypt_test && \
-#lvm pvcreate -ff  -y /dev/sda1 && \
-#lvm vgcreate dracut /dev/sda1 && \
-#lvm lvcreate -l 100%FREE -n root dracut && \
-#lvm vgchange -ay && \
-mke2fs -L ROOT /dev/sda1 && \
+mke2fs -F /dev/sda && \
 mkdir -p /sysroot && \
-mount /dev/sda1 /sysroot && \
+mount /dev/sda /sysroot && \
 cp -a -t /sysroot /source/* && \
 umount /sysroot && \
 echo "dracut-root-block-created" >/dev/sdb
