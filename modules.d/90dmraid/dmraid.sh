2a652c0b95776c6a5ffc997848bb05cef37a9c69
Harald Hoyer <harald@redhat.com>
2009-07-13 13:51:33 +0200

    add environment variables for different udev versions

--- modules.d/90dmraid/dmraid.sh
+++ modules.d/90dmraid/dmraid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if udevadm settle --timeout=1 >/dev/null 2>&1; then
+if $UDEV_QUEUE_EMPTY >/dev/null 2>&1; then
     [ -h "$job" ] && rm -f "$job"
     # run dmraid if udev has settled
     dmraid -ay -Z
