4fafddf78f5b0ef7833157992665d50575b65420
Harald Hoyer <harald@redhat.com>
2009-07-13 16:41:05 +0200

    add test for simple root on LVM

---
+++ test/TEST-11-LVM/create-root.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+# don't let udev and this script step on eachother's toes
+for x in 63-luks.rules 64-lvm.rules 70-mdadm.rules 99-mount-rules; do
+    > "/etc/udev/rules.d/$x"
+done
+udevadm control --reload-rules
+# save a partition at the beginning for future flagging purposes
+sfdisk -C 640 -H 2 -S 32 -L /dev/sda <<EOF
+,1
+,213
+,213
+,213
+EOF
+for i in sda2 sda3 sda4; do
+lvm pvcreate -ff  -y /dev/$i ;
+done && \
+lvm vgcreate dracut /dev/sda[234] && \
+lvm lvcreate -l 100%FREE -n root dracut && \
+lvm vgchange -ay && \
+mke2fs /dev/dracut/root && \
+mkdir -p /sysroot && \
+mount /dev/dracut/root /sysroot && \
+cp -a -t /sysroot /source/* && \
+umount /sysroot && \
+lvm lvchange -a n /dev/dracut/root && \
+echo "dracut-root-block-created" >/dev/sda1
+poweroff -f
