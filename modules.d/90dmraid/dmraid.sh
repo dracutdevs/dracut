d72eec354ae34827d34a802283e006aafcce4d08
Harald Hoyer <harald@redhat.com>
2009-07-13 11:56:04 +0200

    test for empty udev queue with timeout=1 instead of 0
    older udev versions do not support timeout=0

--- modules.d/90dmraid/dmraid.sh
+++ modules.d/90dmraid/dmraid.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if udevadm settle --timeout=0 >/dev/null 2>&1; then
+if udevadm settle --timeout=1 >/dev/null 2>&1; then
     # run dmraid if udev has settled
     dmraid -ay -Z
     [ -e "$job" ] && rm -f "$job"
