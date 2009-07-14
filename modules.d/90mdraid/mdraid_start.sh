3564cb7c2cfe8e3e4543e5769e6364da4046eaaa
Hans de Goede <hdegoede@redhat.com>
2009-07-14 22:24:42 +0200

    Make Intel BIOS raid use mdraid instead of dmraid when mdadm 3.0 is available
    Intel BIOS raid is being shifted from dmraid to mdraid because mdraid offers
    more features. So if an imsm metadata capable mdadm is present use mdraid
    instead of dmraid for isw_raid_member's
    
    This patch also adds code to mdraid_start.sh so that the raidsets
    inside the imsm containers get started once udev is done probing
    (doing this earlier leads to potentially degraded use of the sets and
     an unwanted resync).

--- modules.d/90mdraid/mdraid_start.sh
+++ modules.d/90mdraid/mdraid_start.sh
@@ -4,5 +4,14 @@ if $UDEV_QUEUE_EMPTY >/dev/null 2>&1; then
     [ -h "$job" ] && rm -f "$job"
     # run mdadm if udev has settled
     mdadm -IRs
+    # and activate any containers
+    for md in /dev/md?*; do
+        case $md in
+            /dev/md*p*) ;;
+            *)
+                if mdadm --export --detail $md | grep -q container; then
+                    mdadm -IR $md
+                fi
+        esac
+    done
 fi
-
