1adaf3372c97bad79b0cae97187f7abb58b82f45
Harald Hoyer <harald@redhat.com>
2009-07-15 09:33:44 +0200

    limit the devices for which lvm vgscan will scan
    Build the filter list and create /etc/lvm/lvm.conf for all LVM_member
    devices found by udev.

--- modules.d/90lvm/lvm_scan.sh
+++ modules.d/90lvm/lvm_scan.sh
@@ -3,6 +3,25 @@
 if $UDEV_QUEUE_EMPTY >/dev/null 2>&1; then
     [ -h "$job" ] && rm -f "$job"
     # run lvm scan if udev has settled
+
+    [ -d /etc/lvm ] || mkdir -p /etc/lvm
+    # build a list of devices to scan
+    lvmdevs=$(
+	for f in /tmp/.lvm_scan-*; do
+	    [ -e "$f" ] || continue
+	    echo ${f##/tmp/.lvm_scan-}
+	done
+	)
+    {
+	echo 'devices {';
+	echo -n '    filter = [ '
+	for dev in $lvmdevs; do
+	    printf '"a|^/dev/%s$|", ' $dev;
+	done;
+	echo '"r/.*/" ]';
+	echo '}';	  
+    } > /etc/lvm/lvm.conf
+
     lvm vgscan
     lvm vgchange -ay
 fi
