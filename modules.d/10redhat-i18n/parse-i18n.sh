e6df8175256de28f9eccb7ad6c8df123a36a42c1
Harald Hoyer <harald@redhat.com>
2009-07-15 09:09:26 +0200

    install all /lib/kbd to initramfs, if not host only
    KEYBOARDTYPE=sun|pc
       will be written to /etc/sysconfig/keyboard in the initramfs
    
    KEYTABLE=<keytable file>
       will be written to /etc/sysconfig/keyboard in the initramfs
    
    SYSFONT= Console font
       will be written to /etc/sysconfig/i18n in the initramfs
    
    SYSFONTACM= Console map.
       will be written to /etc/sysconfig/i18n in the initramfs
    
    UNIMAP= Unicode font map.
       will be written to /etc/sysconfig/i18n in the initramfs
    
    LANG=<locale>
       will be written to /etc/sysconfig/i18n in the initramfs
       LANG= set locale for all categories, can be any two letter ISO
       language code

---
+++ modules.d/10redhat-i18n/parse-i18n.sh
@@ -0,0 +1,14 @@
+inst_key_val()
+{
+    local value
+    value=$(getarg $1)
+    [ -n "$value" ] && printf '%s="%s"\n' $1 $value >> $2
+    initrdargs="$initrdargs $1" 
+}
+mkdir -p /etc/sysconfig
+inst_key_val KEYBOARDTYPE /etc/sysconfig/keyboard
+inst_key_val KEYTABLE /etc/sysconfig/keyboard
+inst_key_val SYSFONT /etc/sysconfig/i18n
+inst_key_val SYSFONTACM /etc/sysconfig/i18n
+inst_key_val UNIMAP /etc/sysconfig/i18n
+inst_key_val LANG /etc/sysconfig/i18n
