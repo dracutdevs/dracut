6c034582d2841c560e70b737ce88ba46e1a77b87
Harald Hoyer <harald@eeepc.(none)>
2009-07-21 11:53:56 +0200

    redhat-i18n: export LANG

--- modules.d/10redhat-i18n/parse-i18n.sh
+++ modules.d/10redhat-i18n/parse-i18n.sh
@@ -5,6 +5,7 @@ inst_key_val()
     [ -n "$value" ] && printf '%s="%s"\n' $1 $value >> $2
     initrdargs="$initrdargs $1" 
 }
+
 mkdir -p /etc/sysconfig
 inst_key_val KEYBOARDTYPE /etc/sysconfig/keyboard
 inst_key_val KEYTABLE /etc/sysconfig/keyboard
@@ -12,3 +13,9 @@ inst_key_val SYSFONT /etc/sysconfig/i18n
 inst_key_val SYSFONTACM /etc/sysconfig/i18n
 inst_key_val UNIMAP /etc/sysconfig/i18n
 inst_key_val LANG /etc/sysconfig/i18n
+
+if [ -f /etc/sysconfig/i18n ]; then
+    . /etc/sysconfig/i18n
+    export LANG
+fi
+
