906dedf4d57f03545094e75482072b12873926f8
Harald Hoyer <harald@redhat.com>
2009-09-07 17:53:37 +0200

    mdraid-cleanup: do not stop mdraid container

--- modules.d/90mdraid/mdraid-cleanup.sh
+++ modules.d/90mdraid/mdraid-cleanup.sh
@@ -1,4 +1,15 @@
 # stop everything which is not busy
-for i in /dev/md*; do
-    mdadm --stop $i >/dev/null 2>&1
+for i in /dev/md* /dev/md/*; do
+    [ -b $i ] || continue
+
+    mddetail=$(udevadm info --query=property --name=$i)
+    case "$mddetail" in 
+	*MD_LEVEL=container*) 
+	    ;;
+	*DEVTYPE=partition*)
+	    ;;
+	*)
+	    mdadm --stop $i >/dev/null 2>&1
+	    ;;
+    esac
 done
