f25c9697f89a5e4a211bde4e972b192e513275c5
Harald Hoyer <harald@redhat.com>
2009-09-08 15:30:17 +0200

    mdraid: stop udev queue until mdraid assembled

--- modules.d/90mdraid/mdcontainer_start.sh
+++ modules.d/90mdraid/mdcontainer_start.sh
@@ -2,5 +2,7 @@
 . /lib/dracut-lib.sh
 
 md=$1
+udevadm control --stop-exec-queue
 # and activate any containers
 mdadm -IR $md 2>&1 | vinfo
+udevadm control --start-exec-queue
--- modules.d/90mdraid/mdraid_start.sh
+++ modules.d/90mdraid/mdraid_start.sh
@@ -3,4 +3,6 @@
 . /lib/dracut-lib.sh
 # run mdadm if udev has settled
 info "Assembling MD RAID arrays"
+udevadm control --stop-exec-queue
 mdadm -IRs 2>&1 | vinfo
+udevadm control --start-exec-queue
