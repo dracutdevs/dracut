e8cf498dccab29fb412e8efd45986201ba38ec91
Harald Hoyer <harald@redhat.com>
2009-09-15 08:57:24 +0200

    95rootfs-block/mount-root.sh: ignore /.resume

--- modules.d/95rootfs-block/mount-root.sh
+++ modules.d/95rootfs-block/mount-root.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-if [ ! -s /.resume -a -n "$root" -a -z "${root%%block:*}" ]; then
+
+if [ -n "$root" -a -z "${root%%block:*}" ]; then
     mount -t ${fstype:-auto} -o "$rflags" "${root#block:}" "$NEWROOT" && ROOTFS_MOUNTED=yes
 fi
