c27ec060da450220bc6f136eee239c2540bf60e1
Harald Hoyer <harald@redhat.com>
2009-09-15 11:12:13 +0200

    95rootfs-block/block-genrules.sh: beautify code

--- modules.d/95rootfs-block/block-genrules.sh
+++ modules.d/95rootfs-block/block-genrules.sh
@@ -5,10 +5,9 @@ if [ "${root%%:*}" = "block" ]; then
     printf 'SYMLINK=="%s", SYMLINK+="root"\n' \
 	${root#block:/dev/} 
     ) >> /etc/udev/rules.d/99-mount.rules
-    (
+    
     printf '[ -e "%s" ] && { ln -s "%s" /dev/root 2>/dev/null; rm "$job"; }\n' \
-	"${root#block:}" "${root#block:}"
-    ) >> /initqueue-settled/blocksymlink.sh
+	"${root#block:}" "${root#block:}" >> /initqueue-settled/blocksymlink.sh
 
     echo '[ -e /dev/root ]' > /initqueue-finished/block.sh
 fi
