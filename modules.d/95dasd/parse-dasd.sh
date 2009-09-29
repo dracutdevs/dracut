9a2860a3eecfd6f3b2cafac312d7073a12619f3a
Harald Hoyer <harald@redhat.com>
2009-09-29 14:50:49 +0200

    95dasd: fixed typo in modules.conf generation

--- modules.d/95dasd/parse-dasd.sh
+++ modules.d/95dasd/parse-dasd.sh
@@ -2,6 +2,6 @@
 
 dasd_arg=$(getarg dasd=)
 if [ -n "$dasd_arg" ]; then
-	echo "option dasd_mod dasd=$dasd_arg" >> /etc/modprobe.d/dasd.conf
+	echo "options dasd_mod dasd=$dasd_arg" >> /etc/modprobe.d/dasd.conf
 fi
 unset dasd_arg
