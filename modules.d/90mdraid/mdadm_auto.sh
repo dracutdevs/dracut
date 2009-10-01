8bfed103d8156717854d4267a6aa34697d26ce03
Harald Hoyer <harald@redhat.com>
2009-10-01 11:45:54 +0200

    mdraid: do not cleanup on auto assembly with mdadm.conf

--- modules.d/90mdraid/mdadm_auto.sh
+++ modules.d/90mdraid/mdadm_auto.sh
@@ -3,5 +3,3 @@
 
 info "Autoassembling MD Raid"    
 /sbin/mdadm -As --auto=yes --run 2>&1 | vinfo
-ln -s /sbin/mdraid-cleanup /pre-pivot/30-mdraid-cleanup.sh 2>/dev/null
-ln -s /sbin/mdraid-cleanup /pre-pivot/31-mdraid-cleanup.sh 2>/dev/null
