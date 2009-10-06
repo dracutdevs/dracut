9fbe8a001b688441d45709451091e9df2a97b756
Harald Hoyer <harald@redhat.com>
2009-10-06 18:09:18 +0200

    TEST-50-MULTINIC/test.sh: add selinux=0

--- test/TEST-50-MULTINIC/test.sh
+++ test/TEST-50-MULTINIC/test.sh
@@ -15,7 +15,7 @@ run_server() {
 	-net socket,mcast=230.0.0.1:1234 \
 	-serial udp:127.0.0.1:9999 \
 	-kernel /boot/vmlinuz-$KVERSION \
-	-append "root=/dev/sda rw quiet console=ttyS0,115200n81" \
+	-append "selinux=0 root=/dev/sda rw quiet console=ttyS0,115200n81" \
 	-initrd initramfs.server -pidfile server.pid -daemonize || return 1
     sudo chmod 644 server.pid || return 1
 
@@ -48,7 +48,7 @@ client_test() {
   	-net nic,macaddr=52:54:00:12:34:$mac3,model=e1000 \
   	-net socket,mcast=230.0.0.1:1234 \
   	-kernel /boot/vmlinuz-$KVERSION \
-  	-append "$cmdline $DEBUGFAIL ro quiet console=ttyS0,115200n81 rdcopystate" \
+  	-append "$cmdline $DEBUGFAIL ro quiet console=ttyS0,115200n81 selinux=0 rdshell rdcopystate" \
   	-initrd initramfs.testing
 
     if [[ $? -ne 0 ]] || ! grep -m 1 -q OK client.img; then
