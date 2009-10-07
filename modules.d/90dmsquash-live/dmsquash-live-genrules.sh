cdc74b198ebbda69f550a7d744534e41cffd7e25
Harald Hoyer <harald@redhat.com>
2009-10-07 12:49:52 +0200

    dmsquash-live: run dmsquash-live-root after udev settle

--- modules.d/90dmsquash-live/dmsquash-live-genrules.sh
+++ modules.d/90dmsquash-live/dmsquash-live-genrules.sh
@@ -4,9 +4,9 @@ if [ "${root%%:*}" = "live" ]; then
     	${root#live:/dev/} 
     printf 'SYMLINK=="%s", SYMLINK+="live"\n' \
 	${root#live:/dev/} 
-    printf 'KERNEL=="%s", RUN+="/sbin/initqueue --onetime --unique /sbin/dmsquash-live-root $env{DEVNAME}"\n' \
+    printf 'KERNEL=="%s", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root $env{DEVNAME}"\n' \
     	${root#live:/dev/} 
-    printf 'SYMLINK=="%s", RUN+="/sbin/initqueue --onetime --unique /sbin/dmsquash-live-root $env{DEVNAME}"\n' \
+    printf 'SYMLINK=="%s", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root $env{DEVNAME}"\n' \
 	${root#live:/dev/} 
 
     ) >> /etc/udev/rules.d/99-live-mount.rules
