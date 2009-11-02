fe32b77f87f33228df2f716ede22cfbe6d1048a4
David Cantrell <dcantrell@redhat.com>
2009-11-02 09:18:42 -1000

    Rename existing 95dasd handling to 95dasd_mod.
    Rename the existing rd_DASD module handler to rd_DASD_MOD.

---
+++ modules.d/95dasd_mod/parse-dasd.sh
@@ -0,0 +1,7 @@
+[ -d /etc/modprobe.d ] || mkdir /etc/modprobe.d
+
+dasd_arg=$(getarg rd_DASD_MOD=)
+if [ -n "$dasd_arg" ]; then
+	echo "options dasd_mod dasd=$dasd_arg" >> /etc/modprobe.d/dasd.conf
+fi
+unset dasd_arg
