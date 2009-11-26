57c6b805ce0a2a5bffa6ad862565b47414b41635
Harald Hoyer <harald@redhat.com>
2009-11-26 11:15:59 +0100

    rootfs-block/mount-root: remount root with /etc/fstab filesystem options

--- modules.d/95rootfs-block/mount-root.sh
+++ modules.d/95rootfs-block/mount-root.sh
@@ -1,5 +1,33 @@
 #!/bin/sh
 
+. /lib/dracut-lib.sh
+
 if [ -n "$root" -a -z "${root%%block:*}" ]; then
-    mount -t ${fstype:-auto} -o "$rflags" "${root#block:}" "$NEWROOT" && ROOTFS_MOUNTED=yes
+    mount -t ${fstype:-auto} -o "$rflags" "${root#block:}" "$NEWROOT" \
+        && ROOTFS_MOUNTED=yes
+
+    if ! getarg rd_NO_FSTAB \
+      && ! getarg rootflags \
+      && [ -f "$NEWROOT/etc/fstab" ] \
+      && ! [ -L "$NEWROOT/etc/fstab" ]; then
+        # if $NEWROOT/etc/fstab contains special mount options for 
+        # the root filesystem,
+        # remount it with the proper options
+	rootfs="auto"
+	rootopts="defaults"
+	while read dev mp fs opts rest; do 
+            if [ "$mp" = "/" ]; then
+		rootfs=$fs
+		rootopts=$opts
+		break
+            fi
+	done < "$NEWROOT/etc/fstab"
+
+	if [ "$rootopts" != "defaults" ]; then
+            umount $NEWROOT
+            info "Remounting ${root#block:} with -o $rflags,$rootopts"
+            mount -t "$rootfs" -o "$rflags","$rootopts" \
+                "${root#block:}" "$NEWROOT" 2>&1 | vinfo
+	fi
+    fi
 fi
