56f184310ad5281b4dbb5a9abba47ae9ba9e982e
Harald Hoyer <harald@redhat.com>
2010-04-16 16:44:24 +0200

    mdraid: try to start container childs manually with "mdadm -R"

--- modules.d/90mdraid/mdraid_start.sh
+++ modules.d/90mdraid/mdraid_start.sh
@@ -5,6 +5,18 @@
 info "Assembling MD RAID arrays"
 udevadm control --stop-exec-queue
 mdadm -IRs 2>&1 | vinfo
+
+# there could still be some leftover devices
+# which have had a container added
+for md in /dev/md[0-9]* /dev/md/*; do 
+	[ -b "$md" ] || continue
+	local udevinfo="$(udevadm info --query=env --name=$md)"
+	strstr "$udevinfo" "MD_UUID=" && continue
+	strstr "$udevinfo" "MD_LEVEL=container" && continue
+	strstr "$udevinfo" "DEVTYPE=partition" && continue
+	mdadm -R "$md" 2>&1 | vinfo
+done
+
 ln -s /sbin/mdraid-cleanup /pre-pivot/30-mdraid-cleanup.sh 2>/dev/null
 ln -s /sbin/mdraid-cleanup /pre-pivot/31-mdraid-cleanup.sh 2>/dev/null
 udevadm control --start-exec-queue
