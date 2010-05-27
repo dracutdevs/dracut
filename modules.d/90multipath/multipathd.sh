123e24a3b510d70a5548d39d5b90de26fd1ba7fa
Harald Hoyer <harald@redhat.com>
2010-05-27 14:34:02 +0200

    multipath: remove multipath udev rules, if no multipath.conf was found

--- modules.d/90multipath/multipathd.sh
+++ modules.d/90multipath/multipathd.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
 if [ -e /etc/multipath.conf ]; then
-        modprobe dm-multipath
-	multipathd
+    modprobe dm-multipath
+    multipathd
+else
+    rm /etc/udev/rules.d/??-multipath.rules 2>/dev/null    
 fi
 
