e0013593112eba6c986a8ed853572806a51f402d
Harald Hoyer <harald@redhat.com>
2010-07-23 11:17:19 +0200

    mknod with mode and set umask for the rest

--- modules.d/50plymouth/plymouth-pretrigger.sh
+++ modules.d/50plymouth/plymouth-pretrigger.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 
 if ! getarg rd_NO_PLYMOUTH; then
-    [ -c /dev/null ] || mknod /dev/null c 1 3
+    [ -c /dev/null ] || mknod -m 0666 /dev/null c 1 3
     # first trigger graphics subsystem
     udevadm trigger --attr-match=class=0x030000 >/dev/null 2>&1
     # first trigger graphics and tty subsystem
     udevadm trigger --subsystem-match=graphics --subsystem-match=drm --subsystem-match=tty >/dev/null 2>&1
 
     udevadm settle --timeout=30 2>&1 | vinfo
-    [ -c /dev/zero ] || mknod /dev/zero c 1 5
-    [ -c /dev/systty ] || mknod /dev/systty c 4 0
-    [ -c /dev/fb ] || mknod /dev/fb c 29 0
-    [ -c /dev/hvc0 ] || mknod /dev/hvc0 c 229 0
+    [ -c /dev/zero ] || mknod -m 0666 /dev/zero c 1 5
+    [ -c /dev/tty0 ] || mknod -m 0620 /dev/tty0 c 4 0
+    [ -e /dev/systty ] || ln -s tty0 /dev/systty
+    [ -c /dev/fb0 ] || mknod -m 0660 /dev/fb0 c 29 0
+    [ -e /dev/fb ] || ln -s fb0 /dev/fb
+    [ -c /dev/hvc0 ] || mknod -m 0600 /dev/hvc0 c 229 0
 
     info "Starting plymouth daemon"
     [ -x /bin/plymouthd ] && /bin/plymouthd --attach-to-session
