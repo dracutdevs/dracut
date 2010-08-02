606852a4a68b4aa9528d484e341520c4496b8a7a
Harald Hoyer <harald@redhat.com>
2010-08-02 09:40:55 +0200

    crypt: loop until all non-busy crypt devs closed

--- modules.d/90crypt/crypt-cleanup.sh
+++ modules.d/90crypt/crypt-cleanup.sh
@@ -2,6 +2,10 @@
 # close everything which is not busy
 rm -f /etc/udev/rules.d/70-luks.rules >/dev/null 2>&1
 
-for i in /dev/mapper/luks-*; do
-    cryptsetup luksClose $i >/dev/null 2>&1
+while true; do
+	local do_break="y"
+	for i in /dev/mapper/luks-*; do
+	    cryptsetup luksClose $i >/dev/null 2>&1 && do_break=n
+	done
+	[ "$do_break" = "y" ] && break
 done
