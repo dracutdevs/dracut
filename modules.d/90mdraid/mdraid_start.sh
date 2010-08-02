d7cc3f2ef521137b8b1f32fd45c57feb2c993efb
Harald Hoyer <harald@redhat.com>
2010-08-02 11:35:25 +0200

    mdraid: remove "local"

--- modules.d/90mdraid/mdraid_start.sh
+++ modules.d/90mdraid/mdraid_start.sh
@@ -10,12 +10,13 @@ mdadm -IRs 2>&1 | vinfo
 # which have had a container added
 for md in /dev/md[0-9]* /dev/md/*; do 
 	[ -b "$md" ] || continue
-	local udevinfo="$(udevadm info --query=env --name=$md)"
+	udevinfo="$(udevadm info --query=env --name=$md)"
 	strstr "$udevinfo" "MD_UUID=" && continue
 	strstr "$udevinfo" "MD_LEVEL=container" && continue
 	strstr "$udevinfo" "DEVTYPE=partition" && continue
 	mdadm -R "$md" 2>&1 | vinfo
 done
+unset udevinfo
 
 ln -s /sbin/mdraid-cleanup /pre-pivot/30-mdraid-cleanup.sh 2>/dev/null
 ln -s /sbin/mdraid-cleanup /pre-pivot/31-mdraid-cleanup.sh 2>/dev/null
