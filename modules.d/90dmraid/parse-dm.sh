dab1a1dc99c7cea29890db376aa2e21a3c9407e5
Harald Hoyer <harald@redhat.com>
2010-08-23 14:42:33 +0200

    dmraid: switch to rd_NO_MDIMSM, if no mdadm installed

--- modules.d/90dmraid/parse-dm.sh
+++ modules.d/90dmraid/parse-dm.sh
@@ -3,3 +3,9 @@ if getarg rd_NO_DM || getarg nodmraid; then
     info "rd_NO_DM: removing DM RAID activation"
     udevproperty rd_NO_DM=1
 fi
+
+if [ ! -x /sbin/mdadm ] || getarg rd_NO_MDIMSM || getarg noiswmd; then
+    info "rd_NO_MDIMSM: no MD RAID for imsm/isw raids"
+    udevproperty rd_NO_MDIMSM=1
+fi
+
