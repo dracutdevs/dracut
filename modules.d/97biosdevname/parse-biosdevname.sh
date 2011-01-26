07caee2662c97d302cde244a1f7b22f9a089fd3f
Harald Hoyer <harald@redhat.com>
2011-01-26 12:58:52 +0100

    add 97biosdevname dracut module

---
+++ modules.d/97biosdevname/parse-biosdevname.sh
@@ -0,0 +1,10 @@
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+if ! getargbool 1 biosdevname; then
+    info "biosdevname=0: removing biosdevname network renaming"
+    udevproperty UDEV_BIOSDEVNAME=
+    rm -f /etc/udev/rules.d/71-biosdevname.rules
+else
+    udevproperty UDEV_BIOSDEVNAME=1
+fi
+
