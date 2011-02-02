76c4613abcc402e01e6d009eb5f205220bb7d3c7
Harald Hoyer <harald@redhat.com>
2011-02-02 13:34:34 +0100

    multipath: use new "-B" parameter

--- modules.d/90multipath/multipathd.sh
+++ modules.d/90multipath/multipathd.sh
@@ -2,9 +2,9 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-if [ -e /etc/multipath.conf -a -e /etc/multipath/wwids ]; then
+if [ -e /etc/multipath.conf ]; then
     modprobe dm-multipath
-    multipathd
+    multipathd -B || multipathd
 else
     rm /etc/udev/rules.d/??-multipath.rules 2>/dev/null    
 fi
