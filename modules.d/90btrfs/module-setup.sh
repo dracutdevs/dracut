a5e33c7d6f324fb3dddc111cbad9213460408903
Will Woods <wwoods@redhat.com>
2011-03-08 18:35:15 -0500

    Use 'btrfs' command rather than 'btrfsctl', and install btrfs driver
    btrfsctl is being replaced by the btrfs command in the upstream
    tools, so change accordingly. Also, if we're using the btrfs module
    we should probably make sure the btrfs driver gets installed.

--- modules.d/90btrfs/module-setup.sh
+++ modules.d/90btrfs/module-setup.sh
@@ -3,9 +3,9 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 check() {
-    # if we don't have btrfs (btrfsctl) installed on the host system,
+    # if we don't have btrfs installed on the host system,
     # no point in trying to support it in the initramfs.
-    type -P btrfsctl >/dev/null || return 1
+    type -P btrfs >/dev/null || return 1
 
     . $dracutfunctions
     [[ $debug ]] && set -x
@@ -27,8 +27,12 @@ depends() {
     return 0
 }
 
+installkernel() {
+    instmods btrfs
+}
+
 install() {
     inst_rules "$moddir/80-btrfs.rules"
-    dracut_install btrfsctl
+    dracut_install btrfs
 }
 
