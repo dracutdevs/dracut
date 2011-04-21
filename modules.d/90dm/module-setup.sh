12b9736228f2b34c15a9cb63be79cf7b6e865b54
Harald Hoyer <harald@redhat.com>
2011-04-21 15:40:43 +0200

    dm: add 11-dm.rules for udev-168 and cleanup-db
    add OPTIONS+="db_persist" to dm devices, to persist through a
    udevadm info --cleanup-db

--- modules.d/90dm/module-setup.sh
+++ modules.d/90dm/module-setup.sh
@@ -25,5 +25,6 @@ install() {
     # Gentoo ebuild for LVM2 prior to 2.02.63-r1 doesn't install above rules
     # files, but provides the one below:
     inst_rules 64-device-mapper.rules
+    inst_rules "$moddir/11-dm.rules"
 }
 
