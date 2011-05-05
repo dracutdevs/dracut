2f95d699dc03583c90a2acfbfefdd2b55f906c59
Amadeusz Żołnowski <aidecoe@aidecoe.name>
2011-05-05 13:34:19 +0200

    crypt-lib.sh: 'command -v' preferred over 'type'

--- modules.d/90crypt/crypt-lib.sh
+++ modules.d/90crypt/crypt-lib.sh
@@ -2,7 +2,7 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+command -v getarg >/dev/null || . /lib/dracut-lib.sh
 
 # Try to mount specified device (by path, by UUID or by label) and check
 # the path with 'test'.
