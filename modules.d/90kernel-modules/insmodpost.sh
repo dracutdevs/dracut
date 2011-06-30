d083dd4c3996231b12fa6c8e7e49e18ebb1880bc
Amadeusz Żołnowski <aidecoe@aidecoe.name>
2011-06-30 22:45:24 +0200

    90kernel-modules: fixed handling rd.driver.post=

--- modules.d/90kernel-modules/insmodpost.sh
+++ modules.d/90kernel-modules/insmodpost.sh
@@ -2,11 +2,13 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-for p in $(getargs rd.driver.post rdinsmodpost=); do
+. /lib/dracut-lib.sh
+
+for modlist in $(getargs rd.driver.post rdinsmodpost=); do
     (
         IFS=,
-        for p in $i; do
-            modprobe $p
+        for m in $modlist; do
+            modprobe $m
         done
     )
 done
