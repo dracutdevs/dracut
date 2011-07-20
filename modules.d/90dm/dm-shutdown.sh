4456861f7edc8861a32bdd8ca3d3e49f602837e8
Harald Hoyer <harald@redhat.com>
2011-07-20 19:06:08 +0200

    90dm/dm-shutdown.sh: put shutdown in function

--- modules.d/90dm/dm-shutdown.sh
+++ modules.d/90dm/dm-shutdown.sh
@@ -1,2 +1,15 @@
-echo "Disassembling device-mapper devices"
-dmsetup -v remove_all
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+_do_dm_shutdown() {
+    local ret
+    info "Disassembling device-mapper devices"
+    dmsetup -v remove_all
+    ret=$?
+#info "dmsetup ls --tree"
+#dmsetup ls --tree 2>&1 | vinfo
+    return $ret
+}
+
+_do_dm_shutdown
+
