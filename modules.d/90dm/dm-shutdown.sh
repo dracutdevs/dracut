f47e0234987e7e6cfb2439e90658cb5c3b8bdef8
Harald Hoyer <harald@redhat.com>
2011-07-20 21:11:36 +0200

    shutdown: fixed check_shutdown loop

--- modules.d/90dm/dm-shutdown.sh
+++ modules.d/90dm/dm-shutdown.sh
@@ -3,13 +3,14 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 _do_dm_shutdown() {
     local ret
+    local final=$1
     info "Disassembling device-mapper devices"
     dmsetup -v remove_all
     ret=$?
-#info "dmsetup ls --tree"
-#dmsetup ls --tree 2>&1 | vinfo
+    if [ "x$final" != "x" ]; then
+        info "dmsetup ls --tree"
+        dmsetup ls --tree 2>&1 | vinfo
+    fi
     return $ret
 }
-
-_do_dm_shutdown
-
+_do_dm_shutdown $1
--- modules.d/90mdraid/md-shutdown.sh
+++ modules.d/90mdraid/md-shutdown.sh
@@ -3,12 +3,15 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 _do_md_shutdown() {
     local ret
+    local final=$1
     info "Disassembling mdraid devices."
     mdadm -v --stop --scan 
     ret=$?
-#info "cat /proc/mdstat"
-#    cat /proc/mdstat | vinfo
+    if [ "x$final" != "x" ]; then
+        info "cat /proc/mdstat"
+        cat /proc/mdstat | vinfo
+    fi
     return $ret
 }
 
-_do_md_shutdown
+_do_md_shutdown $1
