8e448cadf9fe979ac951b3ca6b5d3a5eecc150c2
Harald Hoyer <harald@redhat.com>
2011-07-20 19:06:26 +0200

    90mdraid/md-shutdown.sh: put shutdown in function

--- modules.d/90mdraid/md-shutdown.sh
+++ modules.d/90mdraid/md-shutdown.sh
@@ -1,2 +1,14 @@
-echo "Disassembling mdraid devices."
-mdadm -v --stop --scan
+#!/bin/sh
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+_do_md_shutdown() {
+    local ret
+    info "Disassembling mdraid devices."
+    mdadm -v --stop --scan 
+    ret=$?
+#info "cat /proc/mdstat"
+#    cat /proc/mdstat | vinfo
+    return $ret
+}
+
+_do_md_shutdown
