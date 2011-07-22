40ee8c1814510bc95ab0d5dffd17af28c14df182
Harald Hoyer <harald@redhat.com>
2011-07-22 11:21:24 +0200

    99base/init: try to mount /usr, if init lives on /usr

--- modules.d/99base/module-setup.sh
+++ modules.d/99base/module-setup.sh
@@ -14,7 +14,7 @@ depends() {
 install() {
     local _d
     dracut_install mount mknod mkdir modprobe pidof sleep chroot \
-        sed ls flock cp mv dmesg rm ln rmmod mkfifo umount
+        sed ls flock cp mv dmesg rm ln rmmod mkfifo umount readlink
     dracut_install -o less
     if [ ! -e "${initdir}/bin/sh" ]; then
         dracut_install bash
