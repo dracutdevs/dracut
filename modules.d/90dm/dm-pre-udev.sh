37ee12b0f305e2bf1670b14e5d7f9199b14c6585
Harald Hoyer <harald@redhat.com>
2011-08-05 13:30:57 +0200

    90dm/dm-pre-udev.sh: load dm-mirror module

--- modules.d/90dm/dm-pre-udev.sh
+++ modules.d/90dm/dm-pre-udev.sh
@@ -2,3 +2,4 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 strstr "$(cat /proc/misc)" device-mapper || modprobe dm_mod
+modprobe dm_mirror 2>/dev/null
