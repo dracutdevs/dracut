031bf1d0fafaa3fdb7096f2442e580ba05ed9fb3
Harald Hoyer <harald@redhat.com>
2011-08-11 10:48:00 +0200

    modules.d/90mdraid/65-md-incremental-imsm.rules: fix md_finished install

--- modules.d/90mdraid/md_finished.sh
+++ modules.d/90mdraid/md_finished.sh
@@ -2,9 +2,8 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 for f in $hookdir/initqueue/settled/mdcontainer_start* $hookdir/initqueue/settled/mdraid_start* $hookdir/initqueue/settled/mdadm_auto*; do
-    [ -e $f ] && return 1
+    [ -e $f ] && exit 1
 done
 
-$UDEV_QUEUE_EMPTY >/dev/null 2>&1 || return 1
-
-return 0
+$UDEV_QUEUE_EMPTY >/dev/null 2>&1 || exit 1
+exit 0
