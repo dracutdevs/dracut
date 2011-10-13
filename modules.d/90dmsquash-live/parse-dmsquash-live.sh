54ba7acba14a5db036914f2b18ff6ef569665d4e
Will Woods <wwoods@redhat.com>
2011-10-12 22:49:35 -0400

    dmsquash-live: fix log message about root/liveroot

--- modules.d/90dmsquash-live/parse-dmsquash-live.sh
+++ modules.d/90dmsquash-live/parse-dmsquash-live.sh
@@ -41,7 +41,7 @@ case "$liveroot" in
     live:/*.[Ii][Mm][Gg]|/*.[Ii][Mm][Gg])
         [ -f "${root#live:}" ] && rootok=1 ;;
 esac
-info "root was $root, liveroot is now $liveroot"
+info "liveroot was $liveroot, is now $root"
 
 # make sure that init doesn't complain
 [ -z "$root" ] && root="live"
