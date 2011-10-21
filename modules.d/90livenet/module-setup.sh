624c70d763e9a201b5450fea10ac5e32939cf2a2
Harald Hoyer <harald@redhat.com>
2011-10-21 10:15:09 +0200

    90livenet: do not install by default

--- modules.d/90livenet/module-setup.sh
+++ modules.d/90livenet/module-setup.sh
@@ -5,7 +5,7 @@ check() {
     # a live, host-only image doesn't really make a lot of sense
     [[ $hostonly ]] && return 1
     command -v wget >/dev/null || return 1
-    return 0
+    return 255
 }
 
 depends() {
