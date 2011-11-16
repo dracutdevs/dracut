dd03dea22e94de0e012e0fe4159ad22393f23936
Harald Hoyer <harald@redhat.com>
2011-11-16 10:50:21 +0100

    90mdraid: remove mdadm_auto.sh completly
    remove mdadm_auto.sh completly, because "mdadm -As --auto=yes" does not
    add new appearing devices.

--- modules.d/90mdraid/mdadm_auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
-type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
-
-info "Autoassembling MD Raid"
-mdadm -As --auto=yes 2>&1 | vinfo
