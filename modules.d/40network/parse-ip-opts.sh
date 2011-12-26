e7cbf8fd30ec39736b4f19fa2e68eba9776b8a80
Anton Blanchard <anton@samba.org>
2011-12-27 09:08:29 +1100

    ip= server-id should be server-IP
     From looking at the code it seems like server-id should really
    be named server-IP.

--- modules.d/40network/parse-ip-opts.sh
+++ modules.d/40network/parse-ip-opts.sh
@@ -7,7 +7,7 @@
 #
 #       ip=<interface>:[dhcp|on|any]
 #
-#       ip=<client-IP-number>:<server-id>:<gateway-IP-number>:<netmask>:<client-hostname>:<interface>:[dhcp|on|any|none|off]
+#       ip=<client-IP-number>:<server-IP-number>:<gateway-IP-number>:<netmask>:<client-hostname>:<interface>:[dhcp|on|any|none|off]
 #
 # When supplying more than only ip= line, <interface> is mandatory and
 # bootdev= must contain the name of the primary interface to use for
