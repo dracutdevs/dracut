edd3262efd0c0aec2ceb6d3037dc2ba912e8ade8
Harald Hoyer <harald@redhat.com>
2012-02-22 15:14:34 +0100

    40network/parse-ip-opts.sh: "local" can only be used in functions

--- modules.d/40network/parse-ip-opts.sh
+++ modules.d/40network/parse-ip-opts.sh
@@ -38,12 +38,13 @@ fi
 
 # Count ip= lines to decide whether we need bootdev= or not
 if [ -z "$NEEDBOOTDEV" ] ; then
-    local count=0
+    count=0
     for p in $(getargs ip=); do
         count=$(( $count + 1 ))
     done
     [ $count -gt 1 ] && NEEDBOOTDEV=1
 fi
+unset count
 
 # If needed, check if bootdev= contains anything usable
 if [ -n "$NEEDBOOTDEV" ] ; then
