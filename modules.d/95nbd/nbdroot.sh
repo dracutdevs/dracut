26433e9c1f974addeeab7a5829bbe9c56c212055
Harald Hoyer <harald@redhat.com>
2012-03-27 15:59:35 +0200

    95nbd/nbdroot.sh: removed bashism

--- modules.d/95nbd/nbdroot.sh
+++ modules.d/95nbd/nbdroot.sh
@@ -31,7 +31,7 @@ nbdflags=${root%%:*}
 nbdopts=${root#*:}
 
 # If nbdport not an integer, then assume name based import
-if [[ $nbdport != [0-9]* ]]; then
+if [ "${nbdport%[0-9]}" = "$nbdport" ]; then
     nbdport="-N $nbdport"
 fi
 
