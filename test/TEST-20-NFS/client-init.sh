fc87119069baf86d115f4dce86e8db8e81435dde
Harald Hoyer <harald@redhat.com>
2012-04-02 09:03:25 +0200

    TEST-20-NFS: get shell in test root for rd.shell

--- test/TEST-20-NFS/client-init.sh
+++ test/TEST-20-NFS/client-init.sh
@@ -3,7 +3,11 @@ export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 exec >/dev/console 2>&1
 export TERM=linux
 export PS1='initramfs-test:\w\$ '
+CMDLINE=$(while read line; do echo $line;done < /proc/cmdline)
+strstr() { [ "${1#*$2*}" != "$1" ]; }
+
 stty sane
+strstr "$CMDLINE" "rd.shell" && sh -i
 echo "made it to the rootfs! Powering down."
 while read dev fs fstype opts rest; do
     [ "$fstype" != "nfs" -a "$fstype" != "nfs4" ] && continue
