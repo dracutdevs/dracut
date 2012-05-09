f2d887d7d5ccb315d840192af825eba92764d9f9
Harald Hoyer <harald@redhat.com>
2012-05-09 13:13:19 +0200

    dracut.sh: install var/run and var/lock

--- dracut.sh
+++ dracut.sh
@@ -655,7 +655,7 @@ if [[ $prefix ]]; then
 fi
 
 if [[ $kernel_only != yes ]]; then
-    for d in usr/bin usr/sbin bin etc lib "$libdir" sbin tmp usr var var/log; do
+    for d in usr/bin usr/sbin bin etc lib "$libdir" sbin tmp usr var var/log var/run var/lock; do
         [[ -e "${initdir}${prefix}/$d" ]] && continue
         if [ -L "/$d" ]; then
             inst_symlink "/$d" "${prefix}/$d"
