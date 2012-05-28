8442dc45b6137494f595bb84843208b5ef0fc1d9
Juan RP <xtraeme@gmail.com>
2012-05-28 16:17:28 +0200

    dmsquash-live: do not hardcode path to mount(8) in generated hook.
    In some cases (at least in mine) mount(8) is in /usr/bin/mount and
    not in /bin/mount as dmsquash-live-root.sh expects. PATH is set to
    /usr/bin:/usr/sbin:/sbin:/bin in that script anyway.

--- modules.d/90dmsquash-live/dmsquash-live-root.sh
+++ modules.d/90dmsquash-live/dmsquash-live-root.sh
@@ -204,7 +204,7 @@ if [ -b "$BASE_LOOPDEV" ]; then
     ln -s $BASE_LOOPDEV /run/initramfs/live-baseloop
 fi
 ln -s /dev/mapper/live-rw /dev/root
-printf '/bin/mount %s /dev/mapper/live-rw %s\n' "$ROOTFLAGS" "$NEWROOT" > $hookdir/mount/01-$$-live.sh
+printf 'mount %s /dev/mapper/live-rw %s\n' "$ROOTFLAGS" "$NEWROOT" > $hookdir/mount/01-$$-live.sh
 
 need_shutdown
 
