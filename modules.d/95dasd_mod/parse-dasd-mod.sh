4d76a85df17b600553af1f58ced1dc7bea8a0e7c
Harald Hoyer <harald@redhat.com>
2012-06-05 20:23:35 +0200

    dasd_mod: change rd.dasd to dasd_mod kernel parameter

--- modules.d/95dasd_mod/parse-dasd-mod.sh
+++ modules.d/95dasd_mod/parse-dasd-mod.sh
@@ -3,41 +3,7 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 mod_args=""
 
-convert_dasd_param() {
-    local bus_id params
-    params=""
-    bus_id=$1; shift
-    while [ $# -gt 0 ]; do
-        case "$1" in
-            use_diag\=1)
-                params="$params,diag"
-                ;;
-            readonly\=1)
-                params="$params,ro"
-                ;;
-            erplog\=1)
-                params="$params,erplog"
-                ;;
-            failfast\=1)
-                params="$params,failfast"
-                ;;
-        esac
-        shift
-    done
-    params="${params#*,}"
-    if [ -n "$params" ]; then
-        echo "$bus_id($params)"
-    else
-        echo "$bus_id"
-    fi
-}
-
 for dasd_arg in $(getargs rd.dasd= rd_DASD= DASD=); do
-    OLD_IFS=$IFS
-    IFS=","
-    set -- $dasd_arg
-    IFS=$OLD_IFS
-    dasd_arg=$(convert_dasd_param "$@")
     mod_args="$mod_args,$dasd_arg"
 done
 
