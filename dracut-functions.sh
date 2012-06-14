cacaa90c63512a068720d61d671ce3ddd1fbd81d
Thomas Lange <lange@informatik.uni-koeln.de>
2012-06-14 17:42:34 +0200

    Debian multiarch support
    Hi Jon,
    
    here's the diff which works for me.  The quotation marks around $@ do
    not work for me. Instead of "$@" I must to use $_dir/$@ (or
    "$_dir"/$@) but no quotation marks around $@. Could you please review
    my patch.

--- dracut-functions.sh
+++ dracut-functions.sh
@@ -699,13 +699,13 @@ dracut_install() {
 
 # inst_libdir_file [-n <pattern>] <file> [<file>...]
 # Install a <file> located on a lib directory to the initramfs image
-# -n <pattern> install non-matching files
+# -n <pattern> install matching files
 inst_libdir_file() {
     if [[ "$1" == "-n" ]]; then
-        local _pattern=$1
+        local _pattern=$2
         shift 2
         for _dir in $libdirs; do
-            for _i in "$@"; do
+            for _i in $_dir/$@; do
                 for _f in "$_dir"/$_i; do
                     [[ "$_i" =~ $_pattern ]] || continue
                     [[ -e "$_i" ]] && dracut_install "$_i"
@@ -714,7 +714,7 @@ inst_libdir_file() {
         done
     else
         for _dir in $libdirs; do
-            for _i in "$@"; do
+            for _i in $_dir/$@; do
                 for _f in "$_dir"/$_i; do
                     [[ -e "$_f" ]] && dracut_install "$_f"
                 done
