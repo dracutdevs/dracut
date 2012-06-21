55cb17c677d6e4658b4356cf5a594acfec01366b
Harald Hoyer <harald@redhat.com>
2012-06-21 10:50:01 +0200

    systemd: remove old udev services

--- modules.d/98systemd/module-setup.sh
+++ modules.d/98systemd/module-setup.sh
@@ -85,16 +85,7 @@ install() {
         $systemdsystemunitdir/syslog.target \
         $systemdsystemunitdir/initrd-switch-root.target \
         $systemdsystemunitdir/initrd-switch-root.service \
-        $systemdsystemunitdir/umount.target \
-        $systemdsystemunitdir/udev-control.socket \
-        $systemdsystemunitdir/udev-kernel.socket \
-        $systemdsystemunitdir/udev.service \
-        $systemdsystemunitdir/udev-settle.service \
-        $systemdsystemunitdir/udev-trigger.service \
-        $systemdsystemunitdir/basic.target.wants/udev.service \
-        $systemdsystemunitdir/basic.target.wants/udev-trigger.service \
-        $systemdsystemunitdir/sockets.target.wants/udev-control.socket \
-        $systemdsystemunitdir/sockets.target.wants/udev-kernel.socket
+        $systemdsystemunitdir/umount.target
 
     for i in /etc/systemd/*.conf; do
         dracut_install "$i"
