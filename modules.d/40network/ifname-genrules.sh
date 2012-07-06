f1e7add2bf74b836bc29d20df2e4e53638e49936
Harald Hoyer <harald@redhat.com>
2012-07-06 12:57:17 +0200

    network/ifname-genrules.sh: check for multiple ifname= lines

--- modules.d/40network/ifname-genrules.sh
+++ modules.d/40network/ifname-genrules.sh
@@ -12,7 +12,23 @@ command -v parse_ifname_opts >/dev/null || . /lib/net-lib.sh
 {
     for p in $(getargs ifname=); do
         parse_ifname_opts $p
-        printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="%s", ATTR{type}=="1", NAME="%s"\n' "$ifname_mac" "$ifname_if"
-    done
 
+        if [ -f /tmp/ifname-$ifname_mac ]; then
+            read oldif < /tmp/ifname-$ifname_mac
+        fi
+        if [ -f /tmp/ifname-$ifname_if ]; then
+            read oldmac < /tmp/ifname-$ifname_if
+        fi
+        if [ -n "$oldif" -a -n "$oldmac" -a "$oldif" = "$ifname_if" -a "$oldmac" = "$ifname_mac" ]; then
+            # skip same ifname= declaration
+            continue
+        fi
+
+        [ -n "$oldif" ] && warn "Multiple interface names specified for MAC $ifname_mac: $oldif"
+        [ -n "$oldmac" ] && warn "Multiple MAC specified for $ifname_if: $oldmac"
+
+        printf 'SUBSYSTEM=="net", ACTION=="add", DRIVERS=="?*", ATTR{address}=="%s", ATTR{type}=="1", NAME="%s"\n' "$ifname_mac" "$ifname_if"
+        echo $ifname_if > /tmp/ifname-$ifname_mac
+        echo $ifname_mac > /tmp/ifname-$ifname_if
+    done
 } >> /etc/udev/rules.d/80-ifname.rules
