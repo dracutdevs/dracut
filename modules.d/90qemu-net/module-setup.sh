f1040fc4702778b38e6cc9eaf5b4c1ff24a7c26c
Harald Hoyer <harald@redhat.com>
2012-07-25 16:03:49 +0200

    qemu*/module-setup.sh: provide alternative for systemd-detect-virt

--- modules.d/90qemu-net/module-setup.sh
+++ modules.d/90qemu-net/module-setup.sh
@@ -3,10 +3,21 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 check() {
-    vm=$(systemd-detect-virt --vm &>/dev/null)
-    (($? != 0)) && return 255
-    [[ $vm = "qemu" ]] && return 0
-    [[ $vm = "kvm" ]] && return 0
+    if type -P systemd-detect-virt &>/dev/null; then
+        vm=$(systemd-detect-virt --vm &>/dev/null)
+        (($? != 0)) && return 255
+        [[ $vm = "qemu" ]] && return 0
+        [[ $vm = "kvm" ]] && return 0
+        [[ $vm = "bochs" ]] && return 0
+    fi
+
+    for i in /sys/class/dmi/id/*_vendor; do
+        [[ -f $i ]] || continue
+        read vendor < $i
+        [[  "$vendor" == "QEMU" ]] && return 0
+        [[  "$vendor" == "Bochs" ]] && return 0
+    done
+
     return 255
 }
 
