19cd47fd26d072000a49661f78fc0a7200116ef6
Harald Hoyer <harald@redhat.com>
2012-07-30 17:06:48 +0200

    plymouth/plymouth-newroot.sh: don't "exit 0" for sourced scripts

--- modules.d/50plymouth/plymouth-newroot.sh
+++ modules.d/50plymouth/plymouth-newroot.sh
@@ -2,5 +2,6 @@
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
-[ -x /bin/plymouth -a -z "$DRACUT_SYSTEMD" ] || exit 0
-/bin/plymouth --newroot=$NEWROOT
+if [ -x /bin/plymouth -a -z "$DRACUT_SYSTEMD" ]; then
+    /bin/plymouth --newroot=$NEWROOT
+fi
