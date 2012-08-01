f0a7fc5b434621033d4885ee9534867f2294524d
Harald Hoyer <harald@redhat.com>
2012-08-01 12:49:45 +0200

    watchdog: set watchdog timeout with wdctl, if available
    "wdctl -s" does not yet exist upstream. I patched
    util-linux/sys-utils/wdctl.c to set the timeout.

--- modules.d/04watchdog/watchdog.sh
+++ modules.d/04watchdog/watchdog.sh
@@ -1,7 +1,12 @@
 #!/bin/sh
 if [ -e /dev/watchdog ]; then
+	if [ ! -e /tmp/watchdog_timeout ]; then
+		wdctl -s 60 /dev/watchdog &>/dev/null
+		> /tmp/watchdog_timeout
+	fi
 	info "Triggering watchdog"
 	>/dev/watchdog
 else
 	modprobe ib700wdt
+	modprobe i6300esb
 fi
