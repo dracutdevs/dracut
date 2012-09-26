17b01c5b13a0b33ad610c2e923c323f616ff7edc
Harald Hoyer <harald@redhat.com>
2012-09-26 10:52:33 +0200

    url-lib/module-setup.sh: also install libs for https

--- modules.d/45url-lib/module-setup.sh
+++ modules.d/45url-lib/module-setup.sh
@@ -14,6 +14,12 @@ depends() {
 install() {
     inst_simple "$moddir/url-lib.sh" "/lib/url-lib.sh"
     dracut_install curl
+    # also install libs for curl https
+    inst_libdir_file "libnsspem.so*"
+    inst_libdir_file "libnsssysinit.so*"
+    inst_libdir_file "libsoftokn3.so*"
+    inst_libdir_file "libsqlite3.so*"
+
     mkdir -m 0755 -p "$initdir/etc/ssl/certs"
     if ! inst_any -t /etc/ssl/certs/ca-bundle.crt \
             /etc/ssl/certs/ca-bundle.crt \
