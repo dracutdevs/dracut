ce862de62496cb627a2bfbb7bb4226329ceb60cc
Harald Hoyer <harald@redhat.com>
2013-03-06 17:10:15 +0100

    base/dracut-lib.sh: reset /etc/profile

--- modules.d/99base/dracut-lib.sh
+++ modules.d/99base/dracut-lib.sh
@@ -908,6 +908,7 @@ _emergency_shell()
         > /.console_lock
         echo "PS1=\"$_name:\${PWD}# \"" >/etc/profile
         systemctl start dracut-emergency.service
+        rm -f /etc/profile
         rm -f /.console_lock
     else
         debug_off
