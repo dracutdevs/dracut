2673ff1932274cd32897db6b851ac050230d85b0
Harald Hoyer <harald@redhat.com>
2013-03-06 17:04:00 +0100

    rootfs-block: in systemd mode, functionality is in the dracut services

--- modules.d/95rootfs-block/module-setup.sh
+++ modules.d/95rootfs-block/module-setup.sh
@@ -23,7 +23,6 @@ check() {
             echo "root.journaldev=$journaldev" >> "${initdir}/etc/cmdline.d/95root-jurnaldev.conf"
         fi
     return 0
-
 }
 
 depends() {
@@ -32,8 +31,10 @@ depends() {
 
 install() {
     dracut_install umount
-    inst_hook cmdline 95 "$moddir/parse-block.sh"
-    inst_hook pre-udev 30 "$moddir/block-genrules.sh"
-    inst_hook mount 99 "$moddir/mount-root.sh"
+    if ! dracut_module_included "systemd"; then
+        inst_hook cmdline 95 "$moddir/parse-block.sh"
+        inst_hook pre-udev 30 "$moddir/block-genrules.sh"
+        inst_hook mount 99 "$moddir/mount-root.sh"
+    fi
 }
 
