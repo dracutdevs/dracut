7d17d6c976d705deebf3da46fd546b1fe7e1dd65
Harald Hoyer <harald@redhat.com>
2013-03-13 10:51:17 +0100

    plymouth: do not install hooks in systemd mode

--- modules.d/50plymouth/module-setup.sh
+++ modules.d/50plymouth/module-setup.sh
@@ -17,12 +17,16 @@ install() {
         . "$moddir"/plymouth-populate-initrd.sh
     else
         PLYMOUTH_POPULATE_SOURCE_FUNCTIONS="$dracutfunctions" \
-            /usr/libexec/plymouth/plymouth-populate-initrd -t $initdir
+            /usr/libexec/plymouth/plymouth-populate-initrd -t "$initdir"
     fi
 
-    inst_hook pre-pivot 90 "$moddir"/plymouth-newroot.sh
-    inst_hook pre-trigger 10 "$moddir"/plymouth-pretrigger.sh
     inst_hook emergency 50 "$moddir"/plymouth-emergency.sh
+
     dracut_install readlink
+
+    if ! dracut_module_included "systemd"; then
+        inst_hook pre-trigger 10 "$moddir"/plymouth-pretrigger.sh
+        inst_hook pre-pivot 90 "$moddir"/plymouth-newroot.sh
+    fi
 }
 
