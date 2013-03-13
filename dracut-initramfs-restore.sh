449b0e0707ee13c1417c3708ea5f4b6bc5d50216
Harald Hoyer <harald@redhat.com>
2013-03-13 13:09:13 +0100

    dracut-initramfs-restore.sh: also look for image in boot loader spec dir

--- dracut-initramfs-restore.sh
+++ dracut-initramfs-restore.sh
@@ -1,17 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
 set -e
+
+KERNEL_VERSION="$(uname -r)"
+
+[[ -f /etc/machine-id ]] && read MACHINE_ID < /etc/machine-id
+
+if [[ $MACHINE_ID ]] && [[ -d /boot/${MACHINE_ID} || -L /boot/${MACHINE_ID} ]] ; then
+    IMG="/boot/${MACHINE_ID}/${KERNEL_VERSION}/initrd"
+fi
+[[ -f $IMG ]] || IMG="/boot/initramfs-${KERNEL_VERSION}.img"
+
 cd /run/initramfs
-IMG="/boot/initramfs-$(uname -r).img"
+
 [ -f .need_shutdown -a -f "$IMG" ] || exit 1
-if zcat "$IMG"  | cpio -id >/dev/null 2>&1; then
+if zcat "$IMG"  | cpio -id --quiet >/dev/null; then
     rm .need_shutdown
-elif xzcat "$IMG"  | cpio -id >/dev/null 2>&1; then
+elif xzcat "$IMG"  | cpio -id --quiet >/dev/null; then
     rm .need_shutdown
 else
     # something failed, so we clean up
+    echo "Unpacking of $IMG to /run/initramfs failed" >&2
     rm -f /run/initramfs/shutdown
     exit 1
 fi
