9dde7a66b5c4193ba61e6642447d5ecccab467ab
Harald Hoyer <harald@redhat.com>
2013-07-19 09:33:45 +0200

    dracut-functions.sh: don't warn twice about omitted modules

--- dracut-functions.sh
+++ dracut-functions.sh
@@ -1255,7 +1255,6 @@ check_mount() {
     [[ $2 ]] || mods_checked_as_dep+=" $_mod "
 
     if [[ " $omit_dracutmodules " == *\ $_mod\ * ]]; then
-        dinfo "dracut module '$_mod' will not be installed, because it's in the list to be omitted!"
         return 1
     fi
 
