ad2565c8ff9c4309f2e5aacbd24856b615f20478
Kyle McMartin <kyle@redhat.com>
2013-07-23 22:07:05 -0400

    dracut: 90kernel-modules/module-setup.sh: add ehci-tegra.ko on arm
    kernel-tegra is now part of the base kernel package, so bits and pieces
    ended up modular, and as a result, if you boot off the internal USB, you
    drop to a dracut shell with no way of getting to root.

--- modules.d/90kernel-modules/module-setup.sh
+++ modules.d/90kernel-modules/module-setup.sh
@@ -47,7 +47,8 @@ installkernel() {
         if [[ "$(uname -p)" == arm* ]]; then
             # arm specific modules
             hostonly='' instmods sdhci_esdhc_imx mmci sdhci_tegra mvsdio omap omapdrm \
-                omap_hsmmc panel-tfp410 sdhci_dove ahci_platform pata_imx sata_mv
+                omap_hsmmc panel-tfp410 sdhci_dove ahci_platform pata_imx sata_mv \
+                ehci-tegra
         fi
 
         # install virtual machine support
