aacdba2bdb3efddd7b3b9cdb149204d86f5020b0
Harald Hoyer <harald@redhat.com>
2013-08-06 16:56:42 +0200

    dracut.sh: check the value of --kver
    https://bugzilla.redhat.com/show_bug.cgi?id=993327

--- dracut.sh
+++ dracut.sh
@@ -478,6 +478,11 @@ if [[ $regenerate_all == "yes" ]]; then
         ((ret+=$?))
     done
     exit $ret
+elif [[ $kernel ]]; then
+    if ! [[ -d /lib/modules/$kernel ]] && [[ $no_kernel != yes ]]; then
+        printf -- "Kernel version $kernel has no modules in /lib/modules/$kernel\n" >&2
+        exit 1
+    fi
 fi
 
 if ! [[ $kernel ]]; then
