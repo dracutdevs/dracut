5e264a5d3ef54a93f97c37d98202ea5299253c83
Harald Hoyer <harald@redhat.com>
2013-08-13 00:39:10 +0200

    add bash module

---
+++ modules.d/00bash/module-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
+# ex: ts=8 sw=4 sts=4 et filetype=sh
+
+check() {
+    [ -x /bin/bash ]
+}
+
+depends() {
+    return 0
+}
+
+install() {
+    # If another shell is already installed, do not use bash
+    [[ -x $initdir/bin/sh ]] && return
+
+    # Prefer bash as /bin/sh if it is available.
+    inst /bin/bash && ln -sf bash "${initdir}/bin/sh"
+}
+
