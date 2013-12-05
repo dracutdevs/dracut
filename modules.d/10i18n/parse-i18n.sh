443fb524877f28fd072f63c4dd98b74926d2513a
Harald Hoyer <harald@redhat.com>
2013-12-05 15:06:39 +0100

    10i18n/parse-i18n.sh: fixed vconsole.conf/locale.conf generation

--- modules.d/10i18n/parse-i18n.sh
+++ modules.d/10i18n/parse-i18n.sh
@@ -6,28 +6,27 @@ inst_key_val() {
     local _value
     local _file
     local _default
-    _default=$1
-    shift
-    _file=$1
-    shift
-    _value=$(getarg $@)
+    _file="$1"; shift
+    _key="$1"; shift
+    _default="$1"; shift
+    _value="$(getarg $@)"
     [ -z "${_value}" ] && _value=$_default
     if [ -n "${_value}" ]; then
-        printf '%s="%s"\n' $2 ${_value} >> $_file
+        printf '%s="%s"\n' $key ${_value} >> $_file
     fi
     unset _file
     unset _value
 }
 
-inst_key_val '' /etc/vconsole.conf rd.vconsole.keymap vconsole.keymap      KEYMAP      -d KEYTABLE
-inst_key_val '' /etc/vconsole.conf rd.vconsole.font vconsole.font        FONT        -d SYSFONT
-inst_key_val '' /etc/vconsole.conf rd.vconsole.font.map vconsole.font.map    FONT_MAP    -d CONTRANS
-inst_key_val '' /etc/vconsole.conf rd.vconsole.font.unimap vconsole.font.unimap FONT_UNIMAP -d UNIMAP
-inst_key_val 1  /etc/vconsole.conf rd.vconsole.font.unicode vconsole.font.unicode UNICODE vconsole.unicode
-inst_key_val '' /etc/vconsole.conf rd.vconsole.keymap.ext vconsole.keymap.ext  EXT_KEYMAP
+inst_key_val /etc/vconsole.conf KEYMAP      '' rd.vconsole.keymap       KEYMAP      -d KEYTABLE
+inst_key_val /etc/vconsole.conf FONT        '' rd.vconsole.font         FONT        -d SYSFONT
+inst_key_val /etc/vconsole.conf FONT_MAP    '' rd.vconsole.font.map     FONT_MAP    -d CONTRANS
+inst_key_val /etc/vconsole.conf FONT_UNIMAP '' rd.vconsole.font.unimap  FONT_UNIMAP -d UNIMAP
+inst_key_val /etc/vconsole.conf UNICODE      1 rd.vconsole.font.unicode UNICODE vconsole.unicode
+inst_key_val /etc/vconsole.conf EXT_KEYMAP  '' rd.vconsole.keymap.ext   EXT_KEYMAP
 
-inst_key_val '' /etc/locale.conf   rd.locale.LANG locale.LANG   LANG
-inst_key_val '' /etc/locale.conf   rd.locale.LC_ALL locale.LC_ALL LC_ALL
+inst_key_val /etc/locale.conf   LANG        '' rd.locale.LANG           LANG
+inst_key_val /etc/locale.conf   LC_ALL      '' rd.locale.LC_ALL         LC_ALL
 
 if [ -f /etc/locale.conf ]; then
     . /etc/locale.conf
