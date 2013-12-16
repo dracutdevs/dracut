a34d3dcaa8947c8df6a3b809e42e6868d08ba649
Harald Hoyer <harald@redhat.com>
2013-12-16 13:15:48 +0100

    test/TEST-17-LVM-THIN: add test case for lvm thin pools

---
+++ test/TEST-17-LVM-THIN/test.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+TEST_DESCRIPTION="root filesystem on LVM PV with thin pool"
+
+KVERSION=${KVERSION-$(uname -r)}
+
+# Uncomment this to debug failures
+#DEBUGFAIL="rd.break rd.shell"
+
+test_run() {
+    $testdir/run-qemu \
+	-hda $TESTDIR/root.ext2 \
+	-m 256M -smp 2 -nographic \
+	-net none -kernel /boot/vmlinuz-$KVERSION \
+	-append "root=/dev/dracut/root rw rd.auto=1 quiet rd.retry=3 rd.info console=ttyS0,115200n81 selinux=0 rd.debug  $DEBUGFAIL" \
+	-initrd $TESTDIR/initramfs.testing
+    grep -F -m 1 -q dracut-root-block-success $TESTDIR/root.ext2 || return 1
+}
+
+test_setup() {
+    # Create the blank file to use as a root filesystem
+    dd if=/dev/null of=$TESTDIR/root.ext2 bs=1M seek=80
+
+    kernel=$KVERSION
+    # Create what will eventually be our root filesystem onto an overlay
+    (
+	export initdir=$TESTDIR/overlay/source
+	. $basedir/dracut-functions.sh
+	inst_multiple sh df free ls shutdown poweroff stty cat ps ln ip route \
+	    mount dmesg ifconfig dhclient mkdir cp ping dhclient
+        for _terminfodir in /lib/terminfo /etc/terminfo /usr/share/terminfo; do
+	    [ -f ${_terminfodir}/l/linux ] && break
+	done
+	inst_multiple -o ${_terminfodir}/l/linux
+	inst "$basedir/modules.d/40network/dhclient-script.sh" "/sbin/dhclient-script"
+	inst "$basedir/modules.d/40network/ifup.sh" "/sbin/ifup"
+	inst_multiple grep
+        inst_simple /etc/os-release
+	inst ./test-init.sh /sbin/init
+	find_binary plymouth >/dev/null && inst_multiple plymouth
+	(cd "$initdir"; mkdir -p dev sys proc etc var/run tmp )
+	cp -a /etc/ld.so.conf* $initdir/etc
+	mkdir $initdir/run
+	sudo ldconfig -r "$initdir"
+    )
+
+    # second, install the files needed to make the root filesystem
+    (
+	export initdir=$TESTDIR/overlay
+	. $basedir/dracut-functions.sh
+	inst_multiple sfdisk mke2fs poweroff cp umount
+	inst_hook initqueue 01 ./create-root.sh
+        inst_hook initqueue/finished 01 ./finished-false.sh
+	inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
+    )
+
+    # create an initramfs that will create the target root filesystem.
+    # We do it this way so that we do not risk trashing the host mdraid
+    # devices, volume groups, encrypted partitions, etc.
+    $basedir/dracut.sh -l -i $TESTDIR/overlay / \
+	-m "dash lvm mdraid udev-rules base rootfs-block kernel-modules" \
+	-d "piix ide-gd_mod ata_piix ext2 sd_mod" \
+	-f $TESTDIR/initramfs.makeroot $KVERSION || return 1
+    rm -rf -- $TESTDIR/overlay
+    # Invoke KVM and/or QEMU to actually create the target filesystem.
+    $testdir/run-qemu -hda $TESTDIR/root.ext2 -m 256M -smp 2 -nographic -net none \
+	-kernel "/boot/vmlinuz-$kernel" \
+	-append "root=/dev/fakeroot rw rootfstype=ext2 quiet console=ttyS0,115200n81 selinux=0" \
+	-initrd $TESTDIR/initramfs.makeroot  || return 1
+    grep -F -m 1 -q dracut-root-block-created $TESTDIR/root.ext2 || return 1
+    (
+	export initdir=$TESTDIR/overlay
+	. $basedir/dracut-functions.sh
+	inst_multiple poweroff shutdown
+	inst_hook emergency 000 ./hard-off.sh
+	inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
+    )
+    sudo $basedir/dracut.sh -l -i $TESTDIR/overlay / \
+	-o "plymouth network" \
+	-a "debug" -I lvs \
+	-d "piix ide-gd_mod ata_piix ext2 sd_mod" \
+	-f $TESTDIR/initramfs.testing $KVERSION || return 1
+}
+
+test_cleanup() {
+    return 0
+}
+
+. $testdir/test-functions
