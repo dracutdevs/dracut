6c7c8d8a071694efd0c7e450fa78e71224a8ced4
Harald Hoyer <harald@redhat.com>
2014-01-22 17:05:22 +0100

    iscsi: more iscsiroot fixes
    handle iscsiroot with firmware more gracefully

--- modules.d/95iscsi/parse-iscsiroot.sh
+++ modules.d/95iscsi/parse-iscsiroot.sh
@@ -61,10 +61,10 @@ fi
 
 # iscsi_firmware does not need argument checking
 if [ -n "$iscsi_firmware" ] ; then
-    netroot=${netroot:-iscsi}
+    netroot=${netroot:-iscsi:}
     modprobe -q iscsi_boot_sysfs 2>/dev/null
     modprobe -q iscsi_ibft
-    initqueue --settled /sbin/iscsiroot dummy "$netroot" "$NEWROOT"
+    initqueue --onetime --timeout "/sbin/iscsiroot dummy '$netroot' '$NEWROOT'"
 fi
 
 # If it's not iscsi we don't continue
@@ -88,7 +88,7 @@ fi
 
 if [ -n "$netroot" ] && [ "$root" != "/dev/root" ] && [ "$root" != "dhcp" ]; then
     if ! getargbool 1 rd.neednet >/dev/null || ! getarg "ip="; then
-        initqueue --onetime --settled /sbin/iscsiroot dummy "$netroot" "$NEWROOT"
+        initqueue --onetime --settled "/sbin/iscsiroot dummy '$netroot' '$NEWROOT'"
     fi
 fi
 
