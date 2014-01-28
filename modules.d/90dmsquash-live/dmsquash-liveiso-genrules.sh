ba9368fa4fedda0f72d84f910d01d7da201405a3
Harald Hoyer <harald@redhat.com>
2014-01-28 18:33:36 +0100

    dmsquash-live/dmsquash-liveiso-genrules.sh: s/loop0/loop-control/
    If loop is a kernel module, /dev/loop0 will never show up as a udev
    event, if the loop module is not manually loaded somewhere.

--- modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
+++ modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
@@ -3,7 +3,7 @@
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 if [ "${root%%:*}" = "liveiso" ]; then
     {
-        printf 'KERNEL=="loop0", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root `/sbin/losetup -f --show %s`"\n' \
+        printf 'KERNEL=="loop-control", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root `/sbin/losetup -f --show %s`"\n' \
             ${root#liveiso:}
     } >> /etc/udev/rules.d/99-liveiso-mount.rules
 fi
