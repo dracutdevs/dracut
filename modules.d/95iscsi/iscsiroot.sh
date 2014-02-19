84dfea4d2a9660c7cecfafb6955d075306da2bcb
Harald Hoyer <harald@redhat.com>
2014-02-19 11:49:34 +0100

    iscsi: for iBFT read the initiator-name from the correct file
    /sys/firmware/ibft/initiator-name should be
    /sys/firmware/ibft/initiator/initiator-name
    
    https://github.com/haraldh/dracut/issues/12

--- modules.d/95iscsi/iscsiroot.sh
+++ modules.d/95iscsi/iscsiroot.sh
@@ -125,7 +125,7 @@ handle_netroot()
 
     if [ -z $iscsi_initiator ]; then
        if [ -f /sys/firmware/ibft/initiator/initiator-name ]; then
-           iscsi_initiator=$(while read line; do echo $line;done < /sys/firmware/ibft/initiator-name)
+           iscsi_initiator=$(while read line; do echo $line;done < /sys/firmware/ibft/initiator/initiator-name)
        fi
     fi
 
