1230f3bcffd8837e8846e60376ea2a88ef5ab257
Harald Hoyer <harald@redhat.com>
2014-05-12 13:53:38 +0200

    ifcfg/write-ifcfg.sh: "IPV6INIT=yes" check also for non-dhcp

--- modules.d/45ifcfg/write-ifcfg.sh
+++ modules.d/45ifcfg/write-ifcfg.sh
@@ -120,9 +120,9 @@ for netup in /tmp/net.*.did-setup ; do
         echo "ONBOOT=yes"
         echo "NETBOOT=yes"
         echo "UUID=\"$uuid\""
+        strstr "$(ip -6 addr show dev $netif)" 'inet6' && echo "IPV6INIT=yes"
         if [ -f /tmp/dhclient.$netif.lease ]; then
             [ -f /tmp/dhclient.$netif.dhcpopts ] && . /tmp/dhclient.$netif.dhcpopts
-            strstr "$(ip -6 addr show dev $netif)" 'inet6' && echo "IPV6INIT=yes"
             if [ -f /tmp/net.$netif.has_ibft_config ]; then
                 echo "BOOTPROTO=ibft"
             else
