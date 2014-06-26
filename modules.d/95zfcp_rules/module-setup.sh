624f173cbd49b7389577aac10a887f16d4f89b7a
Hannes Reinecke <hare@suse.de>
2014-06-26 13:44:05 +0200

    95zfcp_rules: print out rd.zfcp commandline parameter
    When called with '--hostonly-cmdline' the module should print
    out the configuration.
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/95zfcp_rules/module-setup.sh
+++ modules.d/95zfcp_rules/module-setup.sh
@@ -1,10 +1,40 @@
 #!/bin/bash
 
 # called by dracut
+cmdline() {
+    is_zfcp() {
+        local _dev=$1
+        local _devpath=$(cd -P /sys/dev/block/$_dev ; echo $PWD)
+        local _sdev _lun _wwpn _ccw
+
+        [ "${_devpath#*/sd}" == "$_devpath" ] && return 1
+        _sdev="${_devpath%%/block/*}"
+        [ -e ${_sdev}/fcp_lun ] || return 1
+        _lun=$(cat ${_sdev}/fcp_lun)
+        _wwpn=$(cat ${_sdev}/wwpn)
+        _ccw=$(cat ${_sdev}/hba_id)
+        echo "rd.zfcp=${_ccw},${_wwpn},${_lun}"
+        return 1
+    }
+    [[ $hostonly ]] || [[ $mount_needs ]] && {
+        for_each_host_dev_and_slaves is_zfcp
+    }
+}
+
+# called by dracut
 check() {
     local _arch=$(uname -m)
+    local _ccw
     [ "$_arch" = "s390" -o "$_arch" = "s390x" ] || return 1
-    require_binaries zfcp_disk_configure /usr/lib/udev/collect || return 1
+    require_binaries /usr/lib/udev/collect || return 1
+
+    [[ $hostonly ]] || [[ $mount_needs ]] && {
+        for _ccw in /sys/bus/ccw/devices/*/host* ; do
+            [ -d "$_ccw" ] || continue
+            found=$(($found+1));
+        done
+        [ $found -eq 0 ] && return 255
+    }
     return 0
 }
 
@@ -17,6 +47,13 @@ depends() {
 install() {
     inst_multiple /usr/lib/udev/collect
     inst_hook cmdline 30 "$moddir/parse-zfcp.sh"
+    if [[ $hostonly_cmdline == "yes" ]] ; then
+        local _zfcp
+
+        for _zfcp in $(cmdline) ; do
+            printf "%s\n" "$zfcp" >> "${initdir}/etc/cmdline.d/94zfcp.conf"
+        done
+    fi
     if [[ $hostonly ]] ; then
         inst_rules_wildcard 51-zfcp-*.rules
     fi
