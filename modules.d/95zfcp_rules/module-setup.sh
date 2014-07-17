5f923256e33893bead8233852a741a4b0036f709
Hannes Reinecke <hare@suse.de>
2014-07-17 14:29:24 +0200

    95zfcp_rules: fix typo in module_setup
    module_setup.sh has a typo preventing it from saving the correct
    dracut commandline.
    
    References: bnc#887582
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>

--- modules.d/95zfcp_rules/module-setup.sh
+++ modules.d/95zfcp_rules/module-setup.sh
@@ -52,7 +52,7 @@ install() {
         local _zfcp
 
         for _zfcp in $(cmdline) ; do
-            printf "%s\n" "$zfcp" >> "${initdir}/etc/cmdline.d/94zfcp.conf"
+            printf "%s\n" "$_zfcp" >> "${initdir}/etc/cmdline.d/94zfcp.conf"
         done
     fi
     if [[ $hostonly ]] ; then
