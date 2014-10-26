27dcc3a583e5488ee70dcd0299646cc33faf8a21
Peter Robinson <pbrobinson@gmail.com>
2014-10-26 11:48:15 +0000

    kernel-modules: Drop explicit modules that are found with block_module_filter
    The usb_storage, nvme and sdhci_acpi modules are discovered with the
    block_module_filter so there's no need to explicitly list them here.
    
    Signed-off-by: <pbrobinson@gmail.com>

--- modules.d/90kernel-modules/module-setup.sh
+++ modules.d/90kernel-modules/module-setup.sh
@@ -44,8 +44,7 @@ installkernel() {
         instmods yenta_socket scsi_dh_rdac scsi_dh_emc \
             atkbd i8042 usbhid hid-apple hid-sunplus hid-cherry hid-logitech \
             hid-logitech-dj hid-microsoft hid-lcpower firewire-ohci \
-            pcmcia usb_storage nvme hid-hyperv hv-vmbus \
-            sdhci_acpi hyperv-keyboard
+            pcmcia hid-hyperv hv-vmbus hyperv-keyboard
 
         if [[ "$(uname -p)" == arm* ]]; then
             # arm specific modules
