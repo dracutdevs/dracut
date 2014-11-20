778b3543609d8c9d32df7111229f4072d00d02f0
Harald Hoyer <harald@redhat.com>
2014-11-20 11:27:40 +0100

    multipath: add "rd.multipath=0" option

--- modules.d/90multipath/multipathd.sh
+++ modules.d/90multipath/multipathd.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-if [ -e /etc/multipath.conf ]; then
+if getargbool 1 rd.multipath -d -n rd_NO_MULTIPATH && [ -e /etc/multipath.conf ]; then
     modprobe dm-multipath
     multipathd -B || multipathd
     need_shutdown
