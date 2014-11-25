2af6708641bb896f0c117ae49906783cfd46f0a8
Harald Hoyer <harald@redhat.com>
2014-11-25 13:44:54 +0100

    dmsquash-live: do not abort, if user pressed ESC on checkisomd5
    If the user pressed ESC while checkisomd5 runs the media check, it will
    exit with "2". Previously that would mean, that the media check was not
    successful.
    
    (cherry picked from commit 370035d561baa6852bcb02161ef736a78b62b336)

--- modules.d/90dmsquash-live/dmsquash-live-root.sh
+++ modules.d/90dmsquash-live/dmsquash-live-root.sh
@@ -46,7 +46,7 @@ if [ -n "$check" ]; then
     else
         checkisomd5 --verbose $livedev
     fi
-    if [ $? -ne 0 ]; then
+    if [ $? -eq 1 ]; then
         die "CD check failed!"
         exit 1
     fi
