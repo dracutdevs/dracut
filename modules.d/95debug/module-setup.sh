94920084351349cba579ce80a89321ab6544653c
Thomas Renninger <trenn@suse.de>
2014-12-11 15:46:25 +0100

    Enhance suse.conf and debug module example with SUSE-specific settings
    SUSE is using specific settings for dracut, so add them
    to the suse.conf.example file.
    
    Add vi and find to the debug module add some help text to the suse.conf
    file when and how to use it.
    
    Signed-off-by: Thomas Renninger <trenn@suse.de>

--- modules.d/95debug/module-setup.sh
+++ modules.d/95debug/module-setup.sh
@@ -14,8 +14,7 @@ depends() {
 # called by dracut
 install() {
     inst_multiple -o ps grep more cat rm strace free showmount \
-        ping netstat rpcinfo vi scp ping6 ssh \
+        ping netstat rpcinfo vi scp ping6 ssh find vi \
         fsck fsck.ext2 fsck.ext4 fsck.ext3 fsck.ext4dev fsck.vfat e2fsck
-
 }
 
