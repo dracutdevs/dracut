7b3178d9ad9b571312131bba34fd54460a7b4a25
Hannes Reinecke <hare@suse.de>
2015-03-23 17:12:18 +0100

    95fcoe-uefi: Test for EFI firmware
    The fcoe-uefi module should test for EFI firmware when called
    in 'hostonly' mode; of no EFI firmware is found then the module
    doesn't need to be included.
    
    References: bnc#882412
    
    Signed-off-by: Hannes Reinecke <hare@suse.de>
    Signed-off-by: Thomas Renninger <trenn@suse.de>
    (cherry picked from commit 6755c208556be7a85c76e731de48a56f56e81c4c)

--- modules.d/95fcoe-uefi/module-setup.sh
+++ modules.d/95fcoe-uefi/module-setup.sh
@@ -4,6 +4,9 @@
 
 # called by dracut
 check() {
+    [[ $hostonly ]] || [[ $mount_needs ]] && {
+        [ -d /sys/firmware/efi ] || return 255
+    }
     require_binaries dcbtool fipvlan lldpad ip readlink || return 1
     return 0
 }
