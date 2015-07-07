3fa31c3a47be657a42640ba1669e0b77b8835534
Harald Hoyer <harald@redhat.com>
2015-07-07 14:31:16 +0200

    TEST-14-IMSM: remove "rd.auto"
    otherwise rd.auto auto assembles the device

--- test/TEST-14-IMSM/test.sh
+++ test/TEST-14-IMSM/test.sh
@@ -30,13 +30,13 @@ client_run() {
 test_run() {
     read MD_UUID < $TESTDIR/mduuid
     client_run rd.auto rd.md.imsm=0 || return 1
-    client_run rd.auto rd.md.uuid=$MD_UUID rd.dm=0 || return 1
+    client_run rd.md.uuid=$MD_UUID rd.dm=0 || return 1
     # This test succeeds, because the mirror parts are found without
     # assembling the mirror itsself, which is what we want
-    client_run rd.auto rd.md.uuid=$MD_UUID rd.md=0 rd.md.imsm failme && return 1
-    client_run rd.auto rd.md.uuid=$MD_UUID rd.md=0 failme && return 1
+    client_run rd.md.uuid=$MD_UUID rd.md=0 rd.md.imsm failme && return 1
+    client_run rd.md.uuid=$MD_UUID rd.md=0 failme && return 1
     # the following test hangs on newer md
-    client_run rd.auto rd.md.uuid=$MD_UUID rd.dm=0 rd.md.imsm rd.md.conf=0 || return 1
+    client_run rd.md.uuid=$MD_UUID rd.dm=0 rd.md.imsm rd.md.conf=0 || return 1
    return 0
 }
 
