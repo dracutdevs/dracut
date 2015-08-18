65977ef1d107c8741d9f2633b8618aa3aee2a49d
Harald Hoyer <harald@redhat.com>
2015-08-18 15:05:11 +0200

    test/TEST-04-FULL-SYSTEMD/create-root.sh: modprobe btrfs
    why so ever... it used to load automatically

--- test/TEST-04-FULL-SYSTEMD/create-root.sh
+++ test/TEST-04-FULL-SYSTEMD/create-root.sh
@@ -4,6 +4,7 @@ for x in 64-lvm.rules 70-mdadm.rules 99-mount-rules; do
     > "/etc/udev/rules.d/$x"
 done
 rm -f -- /etc/lvm/lvm.conf
+modprobe btrfs
 udevadm control --reload
 set -e
 # save a partition at the beginning for future flagging purposes
