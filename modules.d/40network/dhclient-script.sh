4483fff87c0a5074f3498bfd59387179c20c7ea3
Harald Hoyer <harald@redhat.com>
2015-08-18 12:41:12 +0200

    network/dhclient: exit arping immediatly, if we get an answer
    (cherry picked from commit d6bfa7052a620ebdd37f78655462120871c49ebd)

--- modules.d/40network/dhclient-script.sh
+++ modules.d/40network/dhclient-script.sh
@@ -117,7 +117,7 @@ case $reason in
             read layer2 < /sys/class/net/$netif/device/layer2
         fi
         if [ "$layer2" != "0" ]; then
-            if ! arping -q -D -c 2 -I $netif $new_ip_address ; then
+            if ! arping -f -q -D -c 2 -I $netif $new_ip_address ; then
                 warn "Duplicate address detected for $new_ip_address while doing dhcp. retrying"
                 exit 1
             fi
