71aeb43c901babce6931707c878803e3c7519897
yuwata <watanabe.yu+github@gmail.com>
2015-09-07 15:16:12 +0900

    systemd-networkd: fix typo in error message

--- modules.d/02systemd-networkd/module-setup.sh
+++ modules.d/02systemd-networkd/module-setup.sh
@@ -5,7 +5,7 @@ check() {
     [[ $mount_needs ]] && return 1
 
     if ! dracut_module_included "systemd"; then
-        derror "dracut-systemd needs systemd in the initramfs"
+        derror "systemd-networkd needs systemd in the initramfs"
         return 1
     fi
 
