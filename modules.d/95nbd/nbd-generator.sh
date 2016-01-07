07149fe6bea01026b3ae37afb51b0fa831e9f27a
Harald Hoyer <harald@redhat.com>
2016-01-07 09:56:36 +0100

    nbd: add missing generator

---
+++ modules.d/95nbd/nbd-generator.sh
@@ -0,0 +1,55 @@
+#!/bin/sh
+
+type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
+
+[ -z "$root" ] && root=$(getarg root=)
+
+[ "${root%%:*}" = "nbd" ] || exit 0
+
+GENERATOR_DIR="$2"
+[ -z "$GENERATOR_DIR" ] && exit 1
+
+[ -d "$GENERATOR_DIR" ] || mkdir "$GENERATOR_DIR"
+
+ROOTFLAGS="$(getarg rootflags)"
+
+nroot=${root#nbd:}
+nbdserver=${nroot%%:*}; nroot=${nroot#*:}
+nbdport=${nroot%%:*}; nroot=${nroot#*:}
+nbdfstype=${nroot%%:*}; nroot=${nroot#*:}
+nbdflags=${nroot%%:*}
+
+if [ "$nbdflags" = "$nbdfstype" ]; then
+    unset nbdflags
+fi
+if [ "$nbdfstype" = "$nbdport" ]; then
+    unset nbdfstype
+fi
+
+[ -n "$nbdflags" ] && ROOTFLAGS="$nbdflags"
+
+if getarg "ro"; then
+    if [ -n "$ROOTFLAGS" ]; then
+        ROOTFLAGS="$ROOTFLAGS,ro"
+    else
+        ROOTFLAGS="ro"
+    fi
+fi
+
+if [ -n "$nbdfstype" ]; then
+    ROOTFSTYPE="$nbdfstype"
+else
+    ROOTFSTYPE=$(getarg rootfstype=) || unset ROOTFSTYPE
+fi
+
+{
+    echo "[Unit]"
+    echo "Before=initrd-root-fs.target"
+    echo "[Mount]"
+    echo "Where=/sysroot"
+    echo "What=/dev/root"
+    [ -n "$ROOTFSTYPE" ] && echo "Type=${ROOTFSTYPE}"
+    [ -n "$ROOTFLAGS" ] && echo "Options=${ROOTFLAGS}"
+} > "$GENERATOR_DIR"/sysroot.mount
+
+exit 0
