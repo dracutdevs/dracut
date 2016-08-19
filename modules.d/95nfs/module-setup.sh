8c67fb91232d52d15effce75eb564d26b0a1e994
Harald Hoyer <harald@redhat.com>
2016-08-19 15:56:47 +0200

    nfs: install more kernel modules
    adds support for rpcrdma to support NFSROOT over NFSoRDMA
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1367374

--- modules.d/95nfs/module-setup.sh
+++ modules.d/95nfs/module-setup.sh
@@ -25,7 +25,7 @@ depends() {
 }
 
 installkernel() {
-    hostonly='' instmods nfs sunrpc ipv6 nfsv2 nfsv3 nfsv4 nfs_acl nfs_layout_nfsv41_files
+    hostonly='' instmods =net/sunrpc =fs/nfs ipv6 nfs_acl nfs_layout_nfsv41_files
 }
 
 install() {
