a84c391b8771e0238a0edb8b382ecbf7f2d2bfd3
Harald Hoyer <harald@redhat.com>
2016-08-22 11:04:23 +0200

    kernel-modules: add nvme kernel module

--- modules.d/90kernel-modules/module-setup.sh
+++ modules.d/90kernel-modules/module-setup.sh
@@ -21,7 +21,7 @@ installkernel() {
             yenta_socket scsi_dh_rdac scsi_dh_emc scsi_dh_alua \
             atkbd i8042 usbhid firewire-ohci pcmcia hv-vmbus \
             virtio virtio_blk virtio_ring virtio_pci virtio_scsi \
-            "=drivers/pcmcia" =ide
+            "=drivers/pcmcia" =ide nvme
 
         if [[ "$(uname -p)" == arm* ]]; then
             # arm specific modules
