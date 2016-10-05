443d167a95499b827c2d8c9c3b61f4922c59ff7e
Lukas Nykryn <lnykryn@redhat.com>
2016-10-05 12:46:39 +0200

    network: do not check "firmware boot selected flag"
    Apparently it is not set correctly on some setups
    https://bugzilla.redhat.com/show_bug.cgi?id=1380581

--- modules.d/40network/net-lib.sh
+++ modules.d/40network/net-lib.sh
@@ -241,8 +241,6 @@ ibft_to_cmdline() {
             [ -e ${iface}/flags ] && flags=$(read a < ${iface}/flags; echo $a)
             # Skip invalid interfaces
             (( $flags & 1 )) || continue
-            # Skip interfaces not used for booting
-            (( $flags & 2 )) || continue
             [ -e ${iface}/dhcp ] && dhcp=$(read a < ${iface}/dhcp; echo $a)
             [ -e ${iface}/origin ] && origin=$(read a < ${iface}/origin; echo $a)
             [ -e ${iface}/ip-addr ] && ip=$(read a < ${iface}/ip-addr; echo $a)
