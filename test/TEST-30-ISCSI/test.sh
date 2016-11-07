86f0904004540b63664b2707ad782d788b2112e4
Harald Hoyer <harald@redhat.com>
2016-11-07 13:23:39 +0100

    TEST-30-ISCSI: fixed dhcpd.conf and disabled nowait

--- test/TEST-30-ISCSI/test.sh
+++ test/TEST-30-ISCSI/test.sh
@@ -87,16 +87,16 @@ do_test_run() {
                "rd.iscsi.initiator=$initiator" \
         || return 1
 
-    run_client "netroot=iscsi target1 target2 rd.iscsi.waitnet=0" \
-	       "root=LABEL=sysroot" \
-               "ip=192.168.50.101:::255.255.255.0::ens3:off" \
-               "ip=192.168.51.101:::255.255.255.0::ens4:off" \
-	       "netroot=iscsi:192.168.51.1::::iqn.2009-06.dracut:target1" \
-               "netroot=iscsi:192.168.50.1::::iqn.2009-06.dracut:target2" \
-               "rd.iscsi.firmware" \
-               "rd.iscsi.initiator=$initiator" \
-               "rd.iscsi.waitnet=0" \
-	|| return 1
+#    run_client "netroot=iscsi target1 target2 rd.iscsi.waitnet=0" \
+#	       "root=LABEL=sysroot" \
+#               "ip=192.168.50.101:::255.255.255.0::ens3:off" \
+#               "ip=192.168.51.101:::255.255.255.0::ens4:off" \
+#	       "netroot=iscsi:192.168.51.1::::iqn.2009-06.dracut:target1" \
+#               "netroot=iscsi:192.168.50.1::::iqn.2009-06.dracut:target2" \
+#               "rd.iscsi.firmware" \
+#               "rd.iscsi.initiator=$initiator" \
+#               "rd.iscsi.waitnet=0" \
+#	|| return 1
 
     run_client "FAILME: netroot=iscsi target1 target2 rd.iscsi.waitnet=0 rd.iscsi.testroute=0" \
 	       "root=LABEL=sysroot" \
