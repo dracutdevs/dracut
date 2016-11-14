d941f0e015440a3298203619291340d14badc265
Harald Hoyer <harald@redhat.com>
2016-11-14 12:29:12 +0100

    Revert "TEST-99-RPM: use --best and install all rpms"
    This reverts commit f81806db208a55bc4a6b04eb89cabf062c79f3ef.

--- test/TEST-99-RPM/test.sh
+++ test/TEST-99-RPM/test.sh
@@ -36,7 +36,6 @@ trap '[[ -d $rootdir ]] && { umount "$rootdir/proc"; umount "$rootdir/sys"; umou
     command -v dnf >/dev/null && { dnf_or_yum="dnf"; dnf_or_yum_cmd="dnf --allowerasing"; }
     $dnf_or_yum_cmd -v --nogpgcheck --installroot "$rootdir"/ --releasever 25 --disablerepo='*' \
                 --enablerepo=fedora --enablerepo=updates \
-                --best \
                 install -y \
 	$dnf_or_yum \
 	passwd \
@@ -51,8 +50,11 @@ trap '[[ -d $rootdir ]] && { umount "$rootdir/proc"; umount "$rootdir/sys"; umou
 	mdadm \
     bash \
     iscsi-initiator-utils \
-    "$TESTDIR"/dracut-*.rpm \
+    "$TESTDIR"/dracut-[0-9]*.$(arch).rpm \
     ${NULL}
+    #"$TESTDIR"/dracut-config-rescue-[0-9]*.$(arch).rpm \
+    #"$TESTDIR"/dracut-network-[0-9]*.$(arch).rpm \
+#    ${NULL}
 
     cat >"$rootdir"/test.sh <<EOF
 #!/bin/bash
