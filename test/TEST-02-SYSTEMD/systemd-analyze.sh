c75ce027756ba846f657a847caaf4037932dcd15
Harald Hoyer <harald@redhat.com>
2017-07-28 10:15:03 +0200

    TEST-02-systemd: add systemd-analyze verify

---
+++ test/TEST-02-SYSTEMD/systemd-analyze.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cp /usr/bin/true /usr/bin/man
+
+for i in \
+    sysinit.target \
+        basic.target \
+        initrd-fs.target \
+        initrd.target \
+        initrd-switch-root.target \
+        emergency.target \
+        shutdown.target; do
+    if ! systemd-analyze verify $i; then
+        warn "systemd-analyze verify $i failed"
+        poweroff
+    fi
+done
--- test/TEST-02-SYSTEMD/test.sh
+++ test/TEST-02-SYSTEMD/test.sh
@@ -88,6 +88,7 @@ test_setup() {
         . $basedir/dracut-init.sh
         inst_multiple poweroff shutdown
         inst_hook shutdown-emergency 000 ./hard-off.sh
+        inst_hook pre-pivot 000 ./systemd-analyze.sh
         inst_hook emergency 000 ./hard-off.sh
         inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
     )
