c0371761fb7fec02a6c6f5efdea8503015a90f0e
Harald Hoyer <harald@redhat.com>
2017-07-28 09:49:17 +0200

    add systemd-analyze to debug and TEST-02-SYSTEMD

--- test/TEST-02-SYSTEMD/test.sh
+++ test/TEST-02-SYSTEMD/test.sh
@@ -4,15 +4,15 @@ TEST_DESCRIPTION="root filesystem on a ext3 filesystem"
 KVERSION="${KVERSION-$(uname -r)}"
 
 # Uncomment this to debug failures
-#DEBUGFAIL="rd.shell=1 rd.break"
+#DEBUGFAIL="rd.shell=1 rd.break=pre-mount"
 test_run() {
     $testdir/run-qemu \
-	-drive format=raw,index=0,media=disk,file=$TESTDIR/root.ext3 \
-	-m 512M  -smp 2 -nographic \
-	-net none \
+        -drive format=raw,index=0,media=disk,file=$TESTDIR/root.ext3 \
+        -m 512M  -smp 2 -nographic \
+        -net none \
         -no-reboot \
-	-append "panic=1 root=LABEL=dracut rw loglevel=77 systemd.log_level=debug systemd.log_target=console rd.retry=3 rd.info console=ttyS0,115200n81 selinux=0 rd.debug init=/sbin/init rd.shell=0 $DEBUGFAIL" \
-	-initrd $TESTDIR/initramfs.testing
+        -append "panic=1 root=LABEL=dracut rw loglevel=77 systemd.log_level=debug systemd.log_target=console rd.retry=3 rd.info console=ttyS0,115200n81 selinux=0 init=/sbin/init rd.shell=0 $DEBUGFAIL" \
+        -initrd $TESTDIR/initramfs.testing
     grep -F -m 1 -q dracut-root-block-success $TESTDIR/root.ext3 || return 1
 }
 
@@ -24,9 +24,9 @@ test_setup() {
     kernel=$KVERSION
     # Create what will eventually be our root filesystem onto an overlay
     (
-	export initdir=$TESTDIR/overlay/source
-	mkdir -p $initdir
-	. $basedir/dracut-init.sh
+        export initdir=$TESTDIR/overlay/source
+        mkdir -p $initdir
+        . $basedir/dracut-init.sh
         (
             cd "$initdir"
             mkdir -p -- dev sys proc etc var/run tmp
@@ -36,69 +36,69 @@ test_setup() {
             done
             mkdir -p -- var/lib/nfs/rpc_pipefs
         )
-	inst_multiple sh df free ls shutdown poweroff stty cat ps ln ip \
-	    mount dmesg dhclient mkdir cp ping dhclient \
-	    umount strace less setsid
-	for _terminfodir in /lib/terminfo /etc/terminfo /usr/share/terminfo; do
+        inst_multiple sh df free ls shutdown poweroff stty cat ps ln ip \
+            mount dmesg dhclient mkdir cp ping dhclient \
+            umount strace less setsid systemd-analyze
+        for _terminfodir in /lib/terminfo /etc/terminfo /usr/share/terminfo; do
             [ -f ${_terminfodir}/l/linux ] && break
-	done
-	inst_multiple -o ${_terminfodir}/l/linux
-	inst "$basedir/modules.d/40network/dhclient-script.sh" "/sbin/dhclient-script"
-	inst "$basedir/modules.d/40network/ifup.sh" "/sbin/ifup"
-	inst_multiple grep
+        done
+        inst_multiple -o ${_terminfodir}/l/linux
+        inst "$basedir/modules.d/40network/dhclient-script.sh" "/sbin/dhclient-script"
+        inst "$basedir/modules.d/40network/ifup.sh" "/sbin/ifup"
+        inst_multiple grep
         inst_simple /etc/os-release
-	inst ./test-init.sh /sbin/init
-	find_binary plymouth >/dev/null && inst_multiple plymouth
-	cp -a /etc/ld.so.conf* $initdir/etc
-	sudo ldconfig -r "$initdir"
+        inst ./test-init.sh /sbin/init
+        find_binary plymouth >/dev/null && inst_multiple plymouth
+        cp -a /etc/ld.so.conf* $initdir/etc
+        sudo ldconfig -r "$initdir"
     )
 
     # second, install the files needed to make the root filesystem
     (
-	export initdir=$TESTDIR/overlay
-	. $basedir/dracut-init.sh
-	inst_multiple sfdisk mkfs.ext3 poweroff cp umount
-	inst_hook initqueue 01 ./create-root.sh
+        export initdir=$TESTDIR/overlay
+        . $basedir/dracut-init.sh
+        inst_multiple sfdisk mkfs.ext3 poweroff cp umount
+        inst_hook initqueue 01 ./create-root.sh
         inst_hook initqueue/finished 01 ./finished-false.sh
-	inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
+        inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
     )
 
     # create an initramfs that will create the target root filesystem.
     # We do it this way so that we do not risk trashing the host mdraid
     # devices, volume groups, encrypted partitions, etc.
     $basedir/dracut.sh -l -i $TESTDIR/overlay / \
-	-m "dash udev-rules base rootfs-block fs-lib kernel-modules" \
-	-d "piix ide-gd_mod ata_piix ext3 sd_mod" \
+        -m "dash udev-rules base rootfs-block fs-lib kernel-modules" \
+        -d "piix ide-gd_mod ata_piix ext3 sd_mod" \
         --nomdadmconf \
         --no-hostonly-cmdline -N \
-	-f $TESTDIR/initramfs.makeroot $KVERSION || return 1
+        -f $TESTDIR/initramfs.makeroot $KVERSION || return 1
     rm -rf -- $TESTDIR/overlay
     # Invoke KVM and/or QEMU to actually create the target filesystem.
 
     $testdir/run-qemu \
-	-drive format=raw,index=0,media=disk,file=$TESTDIR/root.ext3 \
-	-m 512M  -smp 2 -nographic -net none \
-	-append "root=/dev/fakeroot rw rootfstype=ext3 quiet console=ttyS0,115200n81 selinux=0" \
-	-initrd $TESTDIR/initramfs.makeroot  || return 1
+        -drive format=raw,index=0,media=disk,file=$TESTDIR/root.ext3 \
+        -m 512M  -smp 2 -nographic -net none \
+        -append "root=/dev/fakeroot rw rootfstype=ext3 quiet console=ttyS0,115200n81 selinux=0" \
+        -initrd $TESTDIR/initramfs.makeroot  || return 1
     grep -F -m 1 -q dracut-root-block-created $TESTDIR/root.ext3 || return 1
 
 
     (
-	export initdir=$TESTDIR/overlay
-	. $basedir/dracut-init.sh
-	inst_multiple poweroff shutdown
-	inst_hook shutdown-emergency 000 ./hard-off.sh
+        export initdir=$TESTDIR/overlay
+        . $basedir/dracut-init.sh
+        inst_multiple poweroff shutdown
+        inst_hook shutdown-emergency 000 ./hard-off.sh
         inst_hook emergency 000 ./hard-off.sh
-	inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
+        inst_simple ./99-idesymlinks.rules /etc/udev/rules.d/99-idesymlinks.rules
     )
     sudo $basedir/dracut.sh -l -i $TESTDIR/overlay / \
-	-a "debug systemd" \
-	-o "network kernel-network-modules" \
-	-d "piix ide-gd_mod ata_piix ext3 sd_mod" \
+        -a "debug systemd" \
+        -o "network kernel-network-modules" \
+        -d "piix ide-gd_mod ata_piix ext3 sd_mod" \
         --no-hostonly-cmdline -N \
-	-f $TESTDIR/initramfs.testing $KVERSION || return 1
+        -f $TESTDIR/initramfs.testing $KVERSION || return 1
 
-#	-o "plymouth network md dmraid multipath fips caps crypt btrfs resume dmsquash-live dm"
+#       -o "plymouth network md dmraid multipath fips caps crypt btrfs resume dmsquash-live dm"
 }
 
 test_cleanup() {
