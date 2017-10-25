21789cbb0a6d103176a2e10c9dd456cdcdcb4b94
Harald Hoyer <harald@redhat.com>
2017-10-25 12:53:47 +0200

    make qemu and qemu-net a default module in non-hostonly mode

--- modules.d/90qemu-net/module-setup.sh
+++ modules.d/90qemu-net/module-setup.sh
@@ -2,22 +2,25 @@
 
 # called by dracut
 check() {
-    if type -P systemd-detect-virt >/dev/null 2>&1; then
-        vm=$(systemd-detect-virt --vm >/dev/null 2>&1)
-        (($? != 0)) && return 255
-        [[ $vm = "qemu" ]] && return 0
-        [[ $vm = "kvm" ]] && return 0
-        [[ $vm = "bochs" ]] && return 0
+    if [[ $hostonly ]] || [[ $mount_needs ]]; then
+        if type -P systemd-detect-virt >/dev/null 2>&1; then
+            vm=$(systemd-detect-virt --vm >/dev/null 2>&1)
+            (($? != 0)) && return 255
+            [[ $vm = "qemu" ]] && return 0
+            [[ $vm = "kvm" ]] && return 0
+            [[ $vm = "bochs" ]] && return 0
+        fi
+
+        for i in /sys/class/dmi/id/*_vendor; do
+            [[ -f $i ]] || continue
+            read vendor < $i
+            [[  "$vendor" == "QEMU" ]] && return 0
+            [[  "$vendor" == "Bochs" ]] && return 0
+        done
+
+        return 255
     fi
-
-    for i in /sys/class/dmi/id/*_vendor; do
-        [[ -f $i ]] || continue
-        read vendor < $i
-        [[  "$vendor" == "QEMU" ]] && return 0
-        [[  "$vendor" == "Bochs" ]] && return 0
-    done
-
-    return 255
+    return 0
 }
 
 # called by dracut
--- modules.d/90qemu/module-setup.sh
+++ modules.d/90qemu/module-setup.sh
@@ -2,28 +2,33 @@
 
 # called by dracut
 check() {
-    if type -P systemd-detect-virt >/dev/null 2>&1; then
-        vm=$(systemd-detect-virt --vm 2>/dev/null)
-        (($? != 0)) && return 255
-        [[ $vm = "qemu" ]] && return 0
-        [[ $vm = "kvm" ]] && return 0
-        [[ $vm = "bochs" ]] && return 0
+    if [[ $hostonly ]] || [[ $mount_needs ]]; then
+        if type -P systemd-detect-virt >/dev/null 2>&1; then
+            vm=$(systemd-detect-virt --vm 2>/dev/null)
+            (($? != 0)) && return 255
+            [[ $vm = "qemu" ]] && return 0
+            [[ $vm = "kvm" ]] && return 0
+            [[ $vm = "bochs" ]] && return 0
+        fi
+
+        for i in /sys/class/dmi/id/*_vendor; do
+            [[ -f $i ]] || continue
+            read vendor < $i
+            [[ "$vendor" == "QEMU" ]] && return 0
+            [[ "$vendor" == "Red Hat" ]] && return 0
+            [[ "$vendor" == "Bochs" ]] && return 0
+        done
+
+        return 255
     fi
 
-    for i in /sys/class/dmi/id/*_vendor; do
-        [[ -f $i ]] || continue
-        read vendor < $i
-        [[ "$vendor" == "QEMU" ]] && return 0
-        [[ "$vendor" == "Red Hat" ]] && return 0
-        [[ "$vendor" == "Bochs" ]] && return 0
-    done
-    return 255
+    return 0
 }
 
 # called by dracut
 installkernel() {
-        # qemu specific modules
-        hostonly='' instmods \
+    # qemu specific modules
+    hostonly='' instmods \
             ata_piix ata_generic pata_acpi cdrom sr_mod ahci \
             virtio_blk virtio virtio_ring virtio_pci \
             virtio_scsi virtio_console virtio_rng \
