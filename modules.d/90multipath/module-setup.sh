af97814e42c3942005f7201f9afdeee363111796
Lukas Nykryn <lnykryn@redhat.com>
2018-07-26 14:43:37 +0200

    Revert "add 90multipath-hostonly module"
    This reverts commit 6ef3173e86c2be99b504f7c7a789dea5c0072f64.
    
    Cherry-picked from: 35e86ac117acbfd699f371f163cdda9db0ebc047
    Resolves: #1599592

--- modules.d/90multipath/module-setup.sh
+++ modules.d/90multipath/module-setup.sh
@@ -9,6 +9,17 @@ is_mpath() {
     return 1
 }
 
+majmin_to_mpath_dev() {
+    local _dev
+    for i in /dev/mapper/*; do
+        [[ $i == /dev/mapper/control ]] && continue
+        _dev=$(get_maj_min $i)
+        if [ "$_dev" = "$1" ]; then
+            echo $i
+            return
+        fi
+    done
+}
 
 check() {
     local _rootdev
@@ -70,6 +81,16 @@ installkernel() {
 
 install() {
     local _f _allow
+    add_hostonly_mpath_conf() {
+        is_mpath $1 && {
+            local _dev
+
+            _dev=$(majmin_to_mpath_dev $1)
+            [ -z "$_dev" ] && return
+            strstr "$_allow" "$_dev" && return
+            _allow="$_allow --allow $_dev"
+        }
+    }
 
     inst_multiple -o  \
         dmsetup \
@@ -83,6 +104,11 @@ install() {
         /etc/xdrdevices.conf \
         /etc/multipath.conf \
         /etc/multipath/*
+
+    [[ $hostonly ]] && [[ $hostonly_mode = "strict" ]] && {
+        for_each_host_dev_and_slaves_all add_hostonly_mpath_conf
+        [ -n "$_allow" ] && mpathconf $_allow --outfile ${initdir}/etc/multipath.conf
+    }
 
     inst $(command -v partx) /sbin/partx
 
