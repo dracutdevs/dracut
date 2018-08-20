157f8ee92bcc89d2cf487fdab138e5b7a35736b3
Kairui Song <kasong@redhat.com>
2018-08-20 18:40:05 +0800

    kernel-modules: add nfit
    To support pmem devices, nfit module is required

--- modules.d/90kernel-modules/module-setup.sh
+++ modules.d/90kernel-modules/module-setup.sh
@@ -41,7 +41,7 @@ installkernel() {
             yenta_socket scsi_dh_rdac scsi_dh_emc scsi_dh_alua \
             atkbd i8042 usbhid firewire-ohci pcmcia hv-vmbus \
             virtio virtio_blk virtio_ring virtio_pci virtio_scsi \
-            "=drivers/pcmcia" =ide nvme vmd
+            "=drivers/pcmcia" =ide nvme vmd nfit
 
         if [[ "$(uname -m)" == arm* || "$(uname -m)" == aarch64 ]]; then
             # arm/aarch64 specific modules
