bc8fb9dccc9212e0172b7f2670769b778efe3ad2
Norbert Lange <norbert.lange@andritz.com>
2018-12-12 17:21:12 +0100

    remove superfluous quotes in parameter expansion
    as this breaks busybox hush shell.
    offending functions can be found with
    grep -r '${[^}]*"[^}]*}'

--- modules.d/95iscsi/mount-lun.sh
+++ modules.d/95iscsi/mount-lun.sh
@@ -2,7 +2,7 @@
 if [ -z $iscsi_lun ]; then
     iscsi_lun=0
 fi
-NEWROOT=${NEWROOT:-"/sysroot"}
+NEWROOT=${NEWROOT:-/sysroot}
 
 for disk in /dev/disk/by-path/*-iscsi-*-$iscsi_lun; do
     if mount -t ${fstype:-auto} -o "$rflags" $disk $NEWROOT; then
