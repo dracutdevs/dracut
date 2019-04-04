2bcc83e849415d548150015129e7e0712b78fac1
Jiri Konecny <jkonecny@redhat.com>
2019-04-04 15:31:18 +0200

    Support only fips=1 not fips
    It looks like only the 'fips=1' is really supported and it is working
    correctly. So instead of failing on 'fips' not clear way give user
    information why we are failing.

--- modules.d/01fips/fips-boot.sh
+++ modules.d/01fips/fips-boot.sh
@@ -2,6 +2,8 @@
 
 if ! fipsmode=$(getarg fips) || [ "$fipsmode" = "0" ]; then
     rm -f -- /etc/modprobe.d/fips.conf >/dev/null 2>&1
+elif [ -z "$fipsmode" ]; then
+    die "FIPS mode have to be enabled by 'fips=1' not just 'fips'"
 elif getarg boot= >/dev/null; then
     . /sbin/fips.sh
     if mount_boot; then
