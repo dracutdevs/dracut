110d26933fa8465b0eb96a7cbf95be88b08971e3
Daniel Molkentin <dmolkentin@suse.com>
2019-05-14 14:17:14 +0200

    Consollidate the qemu virtualization detection
    Eventually, we could add a full virt detection routine.
    This would be useful e.g. to determine inclusion of
    ucode for hostonly setups.

--- modules.d/90qemu-net/module-setup.sh
+++ modules.d/90qemu-net/module-setup.sh
@@ -3,21 +3,7 @@
 # called by dracut
 check() {
     if [[ $hostonly ]] || [[ $mount_needs ]]; then
-        if type -P systemd-detect-virt >/dev/null 2>&1; then
-            vm=$(systemd-detect-virt --vm >/dev/null 2>&1)
-            (($? != 0)) && return 255
-            [[ $vm = "qemu" ]] && return 0
-            [[ $vm = "kvm" ]] && return 0
-            [[ $vm = "bochs" ]] && return 0
-        fi
-
-        for i in /sys/class/dmi/id/*_vendor; do
-            [[ -f $i ]] || continue
-            read vendor < $i
-            [[  "$vendor" == "QEMU" ]] && return 0
-            [[  "$vendor" == "Bochs" ]] && return 0
-        done
-
+        is_qemu_virtualized && return 0
         return 255
     fi
     return 0
