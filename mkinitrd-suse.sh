62f939b8577d5e7f85f218acb5f327e0339ae1ab
Andreas Schwab <schwab@suse.de>
2019-07-08 10:24:58 +0200

    Add support for riscv64

--- mkinitrd-suse.sh
+++ mkinitrd-suse.sh
@@ -191,7 +191,7 @@ default_kernel_images() {
         arm*)
             regex='[uz]Image'
             ;;
-        aarch64)
+        aarch64|riscv64)
             regex='Image'
             ;;
         *)  regex='vmlinu.'
