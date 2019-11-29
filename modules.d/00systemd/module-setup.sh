346e57c1b6af4e2cfdbaae64941f2f3b8454a944
Lukas Nykryn <lnykryn@redhat.com>
2019-11-29 13:29:23 +0100

    always include sg module
    RHEL-only
    Resolves:#1579763

--- modules.d/00systemd/module-setup.sh
+++ modules.d/00systemd/module-setup.sh
@@ -18,7 +18,7 @@ depends() {
 }
 
 installkernel() {
-    hostonly='' instmods autofs4 ipv6 algif_hash hmac sha256
+    hostonly='' instmods autofs4 ipv6 algif_hash hmac sha256 sg
     instmods -s efivarfs
 }
 
