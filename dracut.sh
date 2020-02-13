cdc9cd0e41615d5f5f5af7887991a311cf5c4c7a
Renaud Métrich <rmetrich@redhat.com>
2020-02-13 11:37:13 +0100

    dracut.sh: added help for --regenerate-all

--- dracut.sh
+++ dracut.sh
@@ -184,8 +184,8 @@ Creates initial ramdisk images for preloading modules
   --mount "[DEV] [MP] [FSTYPE] [FSOPTS]"
                         Mount device [DEV] on mountpoint [MP] with filesystem
                         [FSTYPE] and options [FSOPTS] in the initramfs
-  --mount "[MP]"	Same as above, but [DEV], [FSTYPE] and [FSOPTS] are
-			determined by looking at the current mounts.
+  --mount "[MP]"        Same as above, but [DEV], [FSTYPE] and [FSOPTS] are
+                        determined by looking at the current mounts.
   --add-device "[DEV]"  Bring up [DEV] in initramfs
   -i, --include [SOURCE] [TARGET]
                         Include the files in the SOURCE directory into the
@@ -236,7 +236,9 @@ Creates initial ramdisk images for preloading modules
   --uefi                Create an UEFI executable with the kernel cmdline and
                         kernel combined
   --uefi-stub [FILE]    Use the UEFI stub [FILE] to create an UEFI executable
-  --kernel-image [FILE] location of the kernel image
+  --kernel-image [FILE] Location of the kernel image
+  --regenerate-all      Regenerate all initramfs images at the default location
+                        for the kernel versions found on the system
 
 If [LIST] has multiple arguments, then you have to put these in quotes.
 
