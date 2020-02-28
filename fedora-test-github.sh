55a974bae699367f56fd78e4f90070da278d5013
Harald Hoyer <harald@redhat.com>
2020-02-28 12:52:03 +0100

    fedora-test.sh / fedora-test-github.sh: don't build the documentation
    don't build the documentation, if running a test

--- fedora-test-github.sh
+++ fedora-test-github.sh
@@ -14,11 +14,12 @@ NCPU=$(getconf _NPROCESSORS_ONLN)
 if ! [[ $TESTS ]]; then
     make -j$NCPU all syncheck rpm logtee
 else
-    make -j$NCPU all logtee
+    make -j$NCPU enable_documentation=no all logtee
 
     cd test
 
     time LOGTEE_TIMEOUT_MS=590000 make \
+         enable_documentation=no \
          KVERSION=$(rpm -qa kernel --qf '%{VERSION}-%{RELEASE}.%{ARCH}\n' | sort -rn | head -1) \
          DRACUT_NO_XATTR=1 \
          TEST_RUN_ID=$RUN_ID \
