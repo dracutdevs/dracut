3067edf07449e1dbc2dae0776da9426274b34cba
Beniamino Galvani <bgalvani@redhat.com>
2020-06-24 13:06:05 +0200

    network-manager: move connection generation to a lib file
    Move the connection generation code to a library file so that it can
    be reused from other places.
    
    (cherry picked from commit 6e1e87cd2567801b10b2b4f716436c48688408bf)
    
    Resolves: #1847518

--- modules.d/35network-manager/nm-config.sh
+++ modules.d/35network-manager/nm-config.sh
@@ -1,18 +1,9 @@
 #!/bin/sh
 
+type nm_generate_connections >/dev/null 2>&1 || . /lib/nm-lib.sh
+
 if [ -n "$netroot" ] || [ -e /tmp/net.ifaces ]; then
     echo rd.neednet >> /etc/cmdline.d/35-neednet.conf
 fi
 
-/usr/libexec/nm-initrd-generator -- $(getcmdline)
-
-if getargbool 0 rd.neednet; then
-  for i in /usr/lib/NetworkManager/system-connections/* \
-           /run/NetworkManager/system-connections/* \
-           /etc/NetworkManager/system-connections/* \
-           /etc/sysconfig/network-scripts/ifcfg-*; do
-    [ -f "$i" ] || continue
-    echo '[ -f /tmp/nm.done ]' >$hookdir/initqueue/finished/nm.sh
-    break
-  done
-fi
+nm_generate_connections
