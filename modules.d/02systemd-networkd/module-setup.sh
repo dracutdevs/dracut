dcdeec420022d1eefd9dc19b5e3234f57e00c9ad
Jóhann B. Guðmundsson <johannbg@gmail.com>
2020-08-14 22:55:49 +0000

    New-ish sd-networkd module

--- modules.d/02systemd-networkd/module-setup.sh
+++ modules.d/02systemd-networkd/module-setup.sh
@@ -9,7 +9,7 @@ check() {
         return 1
     fi
 
-    return 255
+    return 0
 }
 
 # called by dracut
@@ -23,6 +23,8 @@ installkernel() {
 
 # called by dracut
 install() {
+    local _mods
+
     inst_multiple -o \
         $systemdutildir/systemd-networkd \
         $systemdutildir/systemd-networkd-wait-online \
@@ -32,40 +34,26 @@ install() {
         $systemdutildir/network/99-default.link \
         networkctl ip
 
-        #hostnamectl timedatectl
-        # $systemdutildir/systemd-timesyncd \
-        # $systemdutildir/systemd-timedated \
-        # $systemdutildir/systemd-hostnamed \
-        # $systemdutildir/systemd-resolvd \
-        # $systemdutildir/systemd-resolve-host \
-        # $systemdsystemunitdir/systemd-resolved.service \
-        # $systemdsystemunitdir/systemd-hostnamed.service \
-        # $systemdsystemunitdir/systemd-timesyncd.service \
-        # $systemdsystemunitdir/systemd-timedated.service \
-        # $systemdsystemunitdir/time-sync.target \
-        # /etc/systemd/resolved.conf \
-
-
-    # inst_dir /var/lib/systemd/clock
+    if [[ $hostonly ]]; then
+        inst_multiple -H -o \
+           /etc/systemd/network.conf \
+           /etc/systemd/network/* \
+           ${NULL}
+    fi
 
     grep '^systemd-network:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
     grep '^systemd-network:' $dracutsysrootdir/etc/group >> "$initdir/etc/group"
-    # grep '^systemd-timesync:' $dracutsysrootdir/etc/passwd 2>/dev/null >> "$initdir/etc/passwd"
-    # grep '^systemd-timesync:' $dracutsysrootdir/etc/group >> "$initdir/etc/group"
 
     _arch=${DRACUT_ARCH:-$(uname -m)}
     inst_libdir_file {"tls/$_arch/",tls/,"$_arch/",}"libnss_dns.so.*" \
                      {"tls/$_arch/",tls/,"$_arch/",}"libnss_mdns4_minimal.so.*" \
-                     {"tls/$_arch/",tls/,"$_arch/",}"libnss_myhostname.so.*" \
-                     {"tls/$_arch/",tls/,"$_arch/",}"libnss_resolve.so.*"
 
     for i in \
-        systemd-networkd-wait-online.service \
-        systemd-networkd.service \
-        systemd-networkd.socket
-#       systemd-timesyncd.service
-    do
-        systemctl -q --root "$initdir" enable "$i"
-    done
-}
+         systemd-networkd-wait-online.service \
+         systemd-networkd.service \
+         systemd-networkd.socket
+     do
+         systemctl -q --root "$initdir" enable "$i"
+     done   
 
+}
