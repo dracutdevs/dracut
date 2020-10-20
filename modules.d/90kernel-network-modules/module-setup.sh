5489a1ca688826074fa4f40d7a1fad4d497938cc
Kairui Song <kasong@redhat.com>
2020-10-20 17:33:08 +0800

    90kernel-network-modules: Don't install iscsi related module
    These modules are installed in 95iscsi, no need to repeat here.

--- modules.d/90kernel-network-modules/module-setup.sh
+++ modules.d/90kernel-network-modules/module-setup.sh
@@ -41,7 +41,6 @@ installkernel() {
         =drivers/net/team \
         =drivers/net/ethernet \
         ecb arc4 bridge stp llc ipv6 bonding 8021q ipvlan macvlan af_packet virtio_net xennet
-    hostonly="" instmods iscsi_ibft crc32c iscsi_boot_sysfs
 }
 
 # called by dracut
