d9a8d63a03e826b7df77eac8c3c181c5271bfb4d
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 15:51:58 +0000

    refactor: use /usr prefix

--- modules.d/01fips/module-setup.sh
+++ modules.d/01fips/module-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/bash
 
 # called by dracut
 check() {
@@ -65,7 +65,7 @@ install() {
     inst_hook pre-mount 01 "$moddir/fips-boot.sh"
     inst_hook pre-pivot 01 "$moddir/fips-noboot.sh"
     inst_hook pre-udev 01 "$moddir/fips-load-crypto.sh"
-    inst_script "$moddir/fips.sh" /sbin/fips.sh
+    inst_script "$moddir/fips.sh" /usr/sbin/fips.sh
 
     inst_multiple sha512hmac rmmod insmod mount uname umount
 
