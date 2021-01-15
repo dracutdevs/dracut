73b4c1142682a29a7c4e6208534775fae563fb7f
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 15:50:44 +0000

    refactor: use /usr prefix

--- modules.d/01fips/fips-noboot.sh
+++ modules.d/01fips/fips-noboot.sh
@@ -1,11 +1,11 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 if ! fipsmode=$(getarg fips) || [ "$fipsmode" = "0" ]; then
     rm -f -- /etc/modprobe.d/fips.conf >/dev/null 2>&1
 elif [ -z "$fipsmode" ]; then
     die "FIPS mode have to be enabled by 'fips=1' not just 'fips'"
 elif ! [ -f /tmp/fipsdone ]; then
-    . /sbin/fips.sh
+    . /usr/sbin/fips.sh
     mount_boot
     do_fips || die "FIPS integrity test failed"
 fi
