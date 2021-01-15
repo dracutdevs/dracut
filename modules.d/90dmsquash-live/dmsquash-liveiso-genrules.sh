3cf0ba6d0c4b409a9305ba4d4c3f297d120f1c4b
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 18:54:59 +0000

    refactor: use /usr prefix

--- modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
+++ modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
@@ -1,8 +1,8 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 if [ "${root%%:*}" = "liveiso" ]; then
     {
-        printf 'KERNEL=="loop-control", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root `/sbin/losetup -f --show %s`"\n' \
+        printf 'KERNEL=="loop-control", RUN+="/usr/sbin/initqueue --settled --onetime --unique /usr/sbin/dmsquash-live-root `/usr/sbin/losetup -f --show %s`"\n' \
             ${root#liveiso:}
     } >> /etc/udev/rules.d/99-liveiso-mount.rules
 fi
