42e96c4ca598f33ca5615af0f79f60ec523a6f6f
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 21:02:17 +0000

    refactor: use /usr prefix

--- modules.d/95dasd_mod/parse-dasd-mod.sh
+++ modules.d/95dasd_mod/parse-dasd-mod.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/usr/bin/sh
 mod_args=""
 
 for dasd_arg in $(getargs rd.dasd= -d rd_DASD= DASD=); do
@@ -7,12 +7,12 @@ done
 
 mod_args="${mod_args#*,}"
 
-if [ -x /sbin/dasd_cio_free -a -n "$mod_args" ]; then
+if [ -x /usr/sbin/dasd_cio_free -a -n "$mod_args" ]; then
     [ -d /etc/modprobe.d ] || mkdir -m 0755 -p /etc/modprobe.d
     echo "options dasd_mod dasd=$mod_args" >> /etc/modprobe.d/dasd_mod.conf
 fi
 
 unset dasd_arg
-if [ -x /sbin/dasd_cio_free ] ; then
+if [ -x /usr/sbin/dasd_cio_free ] ; then
     dasd_cio_free
 fi
