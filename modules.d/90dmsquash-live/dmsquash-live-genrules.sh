5e31d9fbf107ee4676835479234bb56283eafde5
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 18:54:02 +0000

    refactor: use /usr prefix

--- modules.d/90dmsquash-live/dmsquash-live-genrules.sh
+++ modules.d/90dmsquash-live/dmsquash-live-genrules.sh
@@ -1,18 +1,18 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 case "$root" in
   live:/dev/*)
     {
-        printf 'KERNEL=="%s", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root %s"\n' \
+        printf 'KERNEL=="%s", RUN+="/usr/sbin/initqueue --settled --onetime --unique /usr/sbin/dmsquash-live-root %s"\n' \
             "${root#live:/dev/}" "${root#live:}"
-        printf 'SYMLINK=="%s", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root %s"\n' \
+        printf 'SYMLINK=="%s", RUN+="/usr/sbin/initqueue --settled --onetime --unique /usr/sbin/dmsquash-live-root %s"\n' \
             "${root#live:/dev/}" "${root#live:}"
     } >> /etc/udev/rules.d/99-live-squash.rules
     wait_for_dev -n "${root#live:}"
   ;;
   live:*)
     if [ -f "${root#live:}" ]; then
-        /sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root "${root#live:}"
+        /usr/sbin/initqueue --settled --onetime --unique /usr/sbin/dmsquash-live-root "${root#live:}"
     fi
   ;;
 esac
