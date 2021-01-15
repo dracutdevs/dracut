b7864bcbbb2abba8413e79d540a7fc3b21ed097b
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 12:06:27 +0000

    refactor: use /usr prefix

--- test/TEST-10-RAID/cryptroot-ask.sh
+++ test/TEST-10-RAID/cryptroot-ask.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 [ -b "/dev/mapper/$2" ] && exit 0
 printf test >/keyfile
-/sbin/cryptsetup luksOpen "$1" "$2" </keyfile
+/usr/sbin/cryptsetup luksOpen "$1" "$2" </keyfile
