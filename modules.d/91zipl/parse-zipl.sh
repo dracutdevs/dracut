63b688b0df8dbcdca89f43a58dbaa2430b515770
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 20:58:05 +0000

    refactor: use /usr prefix

--- modules.d/91zipl/parse-zipl.sh
+++ modules.d/91zipl/parse-zipl.sh
@@ -1,6 +1,4 @@
-#!/bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
+#!/usr/bin/sh
 
 zipl_arg=$(getarg rd.zipl)
 
@@ -31,7 +29,7 @@ if [ -n "$zipl_arg" ] ; then
     esac
     if [ "$zipl_env" ] ; then
         {
-            printf 'ACTION=="add|change", SUBSYSTEM=="block", %s=="%s", ENV{SYSTEMD_READY}!="0", RUN+="/sbin/initqueue --settled --onetime --unique --name install_zipl_cmdline /sbin/install_zipl_cmdline.sh %s"\n' \
+            printf 'ACTION=="add|change", SUBSYSTEM=="block", %s=="%s", ENV{SYSTEMD_READY}!="0", RUN+="/usr/sbin/initqueue --settled --onetime --unique --name install_zipl_cmdline /usr/sbin/install_zipl_cmdline.sh %s"\n' \
                 ${zipl_env} ${zipl_val} ${zipl_arg}
             echo "[ -f /tmp/install.zipl.cmdline-done ]" >$hookdir/initqueue/finished/wait-zipl-conf.sh
         } >> /etc/udev/rules.d/99zipl-conf.rules
