8f9eca564f39de15d08ccf6dad07e9c099c6def2
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 21:04:32 +0000

    refactor: use /usr prefix

--- modules.d/95dcssblk/parse-dcssblk.sh
+++ modules.d/95dcssblk/parse-dcssblk.sh
@@ -1,6 +1,5 @@
-#!/bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
+#!/usr/bin/sh
+
 dcssblk_arg=$(getarg rd.dcssblk=)
 if [ $? = 0 ];then
 	info "Loading dcssblk segments=$dcssblk_arg"
