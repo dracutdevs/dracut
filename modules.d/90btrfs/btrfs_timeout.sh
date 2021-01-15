4b96abc0d835638613e31b6f360a8dcffaddf9cc
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-01-15 20:00:40 +0000

    refactor: use /usr prefix

--- modules.d/90btrfs/btrfs_timeout.sh
+++ modules.d/90btrfs/btrfs_timeout.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
+#!/usr/bin/sh
 
 type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 info "Scanning for all btrfs devices"
-/sbin/btrfs device scan >/dev/null 2>&1
+/usr/sbin/btrfs device scan >/dev/null 2>&1
