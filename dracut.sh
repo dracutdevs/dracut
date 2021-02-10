def68db1104f5b5f0f090f1575299678dd674eb3
Kairui Song <kasong@redhat.com>
2021-02-11 00:39:17 +0800

    fix(squash): post install should be the last step before stripping
    Ensure dracut squash module doesn't effect other steps, and stripping
    can cover the new binaries installed by it.

--- dracut.sh
+++ dracut.sh
@@ -2018,14 +2018,6 @@ if [[ $hostonly_cmdline == "yes" ]] ; then
     fi
 fi
 
-if dracut_module_included "squash"; then
-    readonly squash_dir="$initdir/squash/root"
-    readonly squash_img="$initdir/squash/root.img"
-
-    dinfo "*** Install squash loader ***"
-    DRACUT_SQUASH_POST_INST=1 module_install "squash"
-fi
-
 if [[ $kernel_only != yes ]]; then
     # libpthread workaround: pthread_cancel wants to dlopen libgcc_s.so
     for _dir in $libdirs; do
@@ -2060,6 +2052,14 @@ if [[ $kernel_only != yes ]]; then
             derror "ldconfig might need uid=0 (root) for chroot()"
         fi
     fi
+fi
+
+if dracut_module_included "squash"; then
+    readonly squash_dir="$initdir/squash/root"
+    readonly squash_img="$initdir/squash/root.img"
+
+    dinfo "*** Install squash loader ***"
+    DRACUT_SQUASH_POST_INST=1 module_install "squash"
 fi
 
 if [[ $do_strip = yes ]] && ! [[ $DRACUT_FIPS_MODE ]]; then
