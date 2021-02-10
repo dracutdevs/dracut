34c73b339baa025dfd8916379c4d191be34a8af5
Lukas Nykryn <lnykryn@redhat.com>
2021-02-10 10:27:27 +0100

    fix(network-manager): rework how NM is started in debug mode
    Instead of running directly NM with --debug, create a drop-in in
    nm-config.sh. This will make sure, that we can get the debug output
    regardless the way how NM will be started.

--- modules.d/35network-manager/nm-config.sh
+++ modules.d/35network-manager/nm-config.sh
@@ -6,4 +6,12 @@ if [ -n "$netroot" ] || [ -e /tmp/net.ifaces ]; then
     echo rd.neednet >> /etc/cmdline.d/35-neednet.conf
 fi
 
+if getargbool 0 rd.debug -d -y rdinitdebug -d -y rdnetdebug; then
+    mkdir -m 0755 -p /run/NetworkManager/conf.d
+    (
+        echo '[logging]'
+        echo 'level=TRACE'
+    ) > /run/NetworkManager/conf.d/initrd-logging.conf
+fi
+
 nm_generate_connections
