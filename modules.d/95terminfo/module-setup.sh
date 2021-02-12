4a5dcb93ad045a9c4c856e7cb9a29fa708cea8bc
Harald Hoyer <harald@redhat.com>
2021-02-12 13:26:13 +0100

    fix: shellcheck for modules.d/95terminfo/module-setup.sh

--- modules.d/95terminfo/module-setup.sh
+++ modules.d/95terminfo/module-setup.sh
@@ -5,10 +5,10 @@ install() {
     local _terminfodir
     # terminfo bits make things work better if you fall into interactive mode
     for _terminfodir in /lib/terminfo /etc/terminfo /usr/share/terminfo; do
-        [ -f $dracutsysrootdir${_terminfodir}/l/linux ] && break
+        [[ -f $dracutsysrootdir${_terminfodir}/l/linux ]] && break
     done
 
-    if [ -d $dracutsysrootdir${_terminfodir} ]; then
+    if [[ -d $dracutsysrootdir${_terminfodir} ]]; then
         for i in "l/linux" "v/vt100" "v/vt102" "v/vt220"; do
             inst_dir "$_terminfodir/${i%/*}"
             $DRACUT_CP -L -t "${initdir}/${_terminfodir}/${i%/*}" "$dracutsysrootdir$_terminfodir/$i"
