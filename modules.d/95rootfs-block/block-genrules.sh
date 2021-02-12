a614e5f96a53c18ef85f71159dd0bed690e7c733
Harald Hoyer <harald@redhat.com>
2021-02-12 13:26:13 +0100

    fix: shellcheck for modules.d/95rootfs-block/block-genrules.sh

--- modules.d/95rootfs-block/block-genrules.sh
+++ modules.d/95rootfs-block/block-genrules.sh
@@ -1,15 +1,18 @@
 #!/bin/sh
 
+# shellcheck disable=SC2154
 if [ "${root%%:*}" = "block" ]; then
     {
         printf 'KERNEL=="%s", SYMLINK+="root"\n' \
-            ${root#block:/dev/}
+            "${root#block:/dev/}"
         printf 'SYMLINK=="%s", SYMLINK+="root"\n' \
-            ${root#block:/dev/}
+            "${root#block:/dev/}"
     } >> /etc/udev/rules.d/99-root.rules
 
+    # shellcheck disable=SC2016
+    # shellcheck disable=SC2154
     printf '[ -e "%s" ] && { ln -s "%s" /dev/root 2>/dev/null; rm "$job"; }\n' \
-        "${root#block:}" "${root#block:}" > $hookdir/initqueue/settled/blocksymlink.sh
+        "${root#block:}" "${root#block:}" > "$hookdir"/initqueue/settled/blocksymlink.sh
 
     wait_for_dev "${root#block:}"
 fi
