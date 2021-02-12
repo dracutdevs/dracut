54afa830883de2bcdb4ef731755cc39736388d93
Harald Hoyer <harald@redhat.com>
2021-02-12 13:26:12 +0100

    fix: shellcheck for modules.d/95cifs/cifsroot.sh

--- modules.d/95cifs/cifsroot.sh
+++ modules.d/95cifs/cifsroot.sh
@@ -7,18 +7,18 @@ type getarg >/dev/null 2>&1 || . /lib/dracut-lib.sh
 
 # root is in the form root=cifs://user:pass@[server]/[folder] either from
 # cmdline or dhcp root-path
-netif="$1"
+#netif="$1"
 root="$2"
 NEWROOT="$3"
 
-cifs_to_var $root
-echo server: $server
-echo path: $path
-echo options: $options
+cifs_to_var "$root"
 
+# shellcheck disable=SC2154
 mount.cifs "//$server/$path" "$NEWROOT" -o "$options" && { [ -e /dev/root ] || ln -s null /dev/root ; }
 
 # inject new exit_if_exists
-echo 'settle_exit_if_exists="--exit-if-exists=/dev/root"; rm -f -- "$job"' > $hookdir/initqueue/cifs.sh
+# shellcheck disable=SC2016
+# shellcheck disable=SC2154
+echo 'settle_exit_if_exists="--exit-if-exists=/dev/root"; rm -f -- "$job"' > "$hookdir"/initqueue/cifs.sh
 # force udevsettle to break
-> $hookdir/initqueue/work
+: > "$hookdir"/initqueue/work
