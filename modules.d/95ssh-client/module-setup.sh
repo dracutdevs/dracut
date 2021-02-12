a1b3c13e3ae1295d70847043bbc70fbb34c35787
Harald Hoyer <harald@redhat.com>
2021-02-12 13:26:13 +0100

    fix: shellcheck for modules.d/95ssh-client/module-setup.sh

--- modules.d/95ssh-client/module-setup.sh
+++ modules.d/95ssh-client/module-setup.sh
@@ -10,7 +10,7 @@ check() {
     require_binaries ssh scp  || return 1
 
     if [[ $sshkey ]]; then
-        [ ! -f $dracutsysrootdir$sshkey ] && {
+        [[ ! -f $dracutsysrootdir$sshkey ]] && {
             derror "ssh key: $sshkey is not found!"
             return 1
         }
@@ -27,14 +27,14 @@ depends() {
 
 inst_sshenv()
 {
-    if [ -d $dracutsysrootdir/root/.ssh ]; then
+    if [[ -d $dracutsysrootdir/root/.ssh ]]; then
         inst_dir /root/.ssh
-        chmod 700 ${initdir}/root/.ssh
+        chmod 700 "${initdir}"/root/.ssh
     fi
 
     # Copy over ssh key and knowhosts if needed
     [[ $sshkey ]] && {
-        inst_simple $sshkey
+        inst_simple "$sshkey"
         [[ -f $dracutsysrootdir/root/.ssh/known_hosts ]] && inst_simple /root/.ssh/known_hosts
         [[ -f $dracutsysrootdir/etc/ssh/ssh_known_hosts ]] && inst_simple /etc/ssh/ssh_known_hosts
     }
@@ -43,8 +43,8 @@ inst_sshenv()
     [[ -f $dracutsysrootdir/root/.ssh/config ]] && inst_simple /root/.ssh/config
     if [[ -f $dracutsysrootdir/etc/ssh/ssh_config ]]; then
         inst_simple /etc/ssh/ssh_config
-        sed -i -e 's/\(^[[:space:]]*\)ProxyCommand/\1# ProxyCommand/' ${initdir}/etc/ssh/ssh_config
-        while read key val || [ -n "$key" ]; do
+        sed -i -e 's/\(^[[:space:]]*\)ProxyCommand/\1# ProxyCommand/' "${initdir}"/etc/ssh/ssh_config
+        while read -r key val || [ -n "$key" ]; do
             if [[ $key == "GlobalKnownHostsFile" ]]; then
                 inst_simple "$val"
             # Copy customized UserKnowHostsFile
@@ -55,7 +55,7 @@ inst_sshenv()
                 fi
                 inst_simple "$val"
             fi
-        done < $dracutsysrootdir/etc/ssh/ssh_config
+        done < "$dracutsysrootdir"/etc/ssh/ssh_config
     fi
 
     return 0
@@ -70,7 +70,7 @@ install() {
 
     _nsslibs=$(
         sed -e 's/#.*//; s/^[^:]*://; s/\[[^]]*\]//' \
-            $dracutsysrootdir/etc/nsswitch.conf \
+            "$dracutsysrootdir"/etc/nsswitch.conf \
         |  tr -s '[:space:]' '\n' | sort -u | tr -s '[:space:]' '|')
     _nsslibs=${_nsslibs#|}
     _nsslibs=${_nsslibs%|}
