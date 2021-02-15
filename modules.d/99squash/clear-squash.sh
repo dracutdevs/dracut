bdd194bb8c0427921b87b4dce16c05e18e0c2fb0
Kairui Song <kasong@redhat.com>
2021-02-15 23:05:08 +0800

    refactor(squash): don't record mount points in text file
    The squasn mount points are recorded in text file so later clean up
    script can umount them, this is not needed, the mount points are fixed
    so just detect the umount by hardcoded pattern.

--- modules.d/99squash/clear-squash.sh
+++ modules.d/99squash/clear-squash.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SQUASH_MNT_REC=/squash/mounts
-
-mapfile -t SQUASH_MNTS < $SQUASH_MNT_REC
-
-umount --lazy -- "${SQUASH_MNTS[@]}"
+mnt="/squash/root"
+for dir in jsquash/root/*; do
+	mnt="$mnt ${dir#$SQUASH_MNT}"
+done
+umount --lazy -- $mnt
