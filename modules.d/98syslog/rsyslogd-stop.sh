90840f2c84656c92d3b4e86cdcb54009fbc7556b
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:43 +0100

    fix: shellcheck for modules.d/98syslog/rsyslogd-stop.sh

--- modules.d/98syslog/rsyslogd-stop.sh
+++ modules.d/98syslog/rsyslogd-stop.sh
@@ -6,8 +6,8 @@ type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 if [ -f /var/run/syslogd.pid ]; then
     read pid < /var/run/syslogd.pid
-    kill $pid
-    kill -0 $pid && kill -9 $pid
+    kill "$pid"
+    kill -0 "$pid" && kill -9 "$pid"
 else
     warn "rsyslogd-stop: Could not find a pid for rsyslogd. Won't kill it."
 fi
