8bdfed5bcf8e883f4ac4f8693580be30d888156b
Harald Hoyer <harald@redhat.com>
2021-03-12 15:07:24 +0100

    fix: shellcheck for test/TEST 12-RAID-DEG/create-root.sh

--- test/TEST-12-RAID-DEG/create-root.sh
+++ test/TEST-12-RAID-DEG/create-root.sh
@@ -39,10 +39,10 @@ udevadm settle
 mdadm --detail --export /dev/md0 | grep -F MD_UUID > /tmp/mduuid
 . /tmp/mduuid
 udevadm settle
-eval $(udevadm info --query=env --name=/dev/md0 | while read line || [ -n "$line" ]; do [ "$line" != "${line#*ID_FS_UUID*}" ] && echo $line; done)
+eval $(udevadm info --query=env --name=/dev/md0 | while read line || [ -n "$line" ]; do [ "$line" != "${line#*ID_FS_UUID*}" ] && echo "$line"; done)
 {
     echo "dracut-root-block-created"
-    echo MD_UUID=$MD_UUID
+    echo MD_UUID="$MD_UUID"
     echo "ID_FS_UUID=$ID_FS_UUID"
 } | dd oflag=direct,dsync of=/dev/sda
 sync
