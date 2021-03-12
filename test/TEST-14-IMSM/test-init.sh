07f85c87208d162bc0da5720e98ca8c73a894747
Harald Hoyer <harald@redhat.com>
2021-03-12 15:07:25 +0100

    fix: shellcheck for test/TEST 14-IMSM/test-init.sh

--- test/TEST-14-IMSM/test-init.sh
+++ test/TEST-14-IMSM/test-init.sh
@@ -50,7 +50,7 @@ getarg() {
                 shift
                 ;;
             -y)
-                if _dogetarg $2 > /dev/null; then
+                if _dogetarg "$2" > /dev/null; then
                     if [ "$_deprecated" = "1" ]; then
                         [ -n "$_newoption" ] && warn "Kernel command line option '$2' is deprecated, use '$_newoption' instead." || warn "Option '$2' is deprecated."
                     fi
@@ -61,7 +61,7 @@ getarg() {
                 shift 2
                 ;;
             -n)
-                if _dogetarg $2 > /dev/null; then
+                if _dogetarg "$2" > /dev/null; then
                     echo 0
                     if [ "$_deprecated" = "1" ]; then
                         [ -n "$_newoption" ] && warn "Kernel command line option '$2' is deprecated, use '$_newoption=0' instead." || warn "Option '$2' is deprecated."
@@ -75,7 +75,7 @@ getarg() {
                 if [ -z "$_newoption" ]; then
                     _newoption="$1"
                 fi
-                if _dogetarg $1; then
+                if _dogetarg "$1"; then
                     if [ "$_deprecated" = "1" ]; then
                         [ -n "$_newoption" ] && warn "Kernel command line option '$1' is deprecated, use '$_newoption' instead." || warn "Option '$1' is deprecated."
                     fi
@@ -98,15 +98,15 @@ getargbool() {
     _b=$(getarg "$@")
     [ $? -ne 0 -a -z "$_b" ] && _b="$_default"
     if [ -n "$_b" ]; then
-        [ $_b = "0" ] && return 1
-        [ $_b = "no" ] && return 1
-        [ $_b = "off" ] && return 1
+        [ "$_b" = "0" ] && return 1
+        [ "$_b" = "no" ] && return 1
+        [ "$_b" = "off" ] && return 1
     fi
     return 0
 }
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 strstr() { [ "${1##*"$2"*}" != "$1" ]; }
-CMDLINE=$(while read line || [ -n "$line" ]; do echo $line; done < /proc/cmdline)
+CMDLINE=$(while read line || [ -n "$line" ]; do echo "$line"; done < /proc/cmdline)
 plymouth --quit
 exec > /dev/console 2>&1
 echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/sda
