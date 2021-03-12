69f7a11f73df59bfea9d4a9c001f8f7c10bfe1bc
Harald Hoyer <harald@redhat.com>
2021-03-12 15:07:26 +0100

    fix: shellcheck for test/TEST 40-NBD/create-encrypted-root.sh

--- test/TEST-40-NBD/create-encrypted-root.sh
+++ test/TEST-40-NBD/create-encrypted-root.sh
@@ -26,7 +26,7 @@ udevadm settle
 cryptsetup luksClose /dev/mapper/dracut_crypt_test
 udevadm settle
 sleep 1
-eval $(udevadm info --query=env --name=/dev/sda | while read line || [ -n "$line" ]; do [ "$line" != "${line#*ID_FS_UUID*}" ] && echo $line; done)
+eval $(udevadm info --query=env --name=/dev/sda | while read line || [ -n "$line" ]; do [ "$line" != "${line#*ID_FS_UUID*}" ] && echo "$line"; done)
 {
     echo "dracut-root-block-created"
     echo "ID_FS_UUID=$ID_FS_UUID"
