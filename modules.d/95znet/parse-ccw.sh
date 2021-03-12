8bdddc1406546865ed35d61c9b5027b2a56bec28
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:41 +0100

    fix: shellcheck for modules.d/95znet/parse-ccw.sh

--- modules.d/95znet/parse-ccw.sh
+++ modules.d/95znet/parse-ccw.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for ccw_arg in $(getargs rd.ccw -d 'rd_CCW=') $(getargs rd.znet -d 'rd_ZNET='); do
-    echo $ccw_arg >> /etc/ccw.conf
+    echo "$ccw_arg" >> /etc/ccw.conf
 done
 
 for ifname in $(getargs rd.znet_ifname); do
