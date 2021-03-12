6b58618a5f862b58a0fc1855a0841da44f645d93
Harald Hoyer <harald@redhat.com>
2021-03-12 14:50:15 +0100

    fix: shellcheck for modules.d/01fips/fips-load-crypto.sh

--- modules.d/01fips/fips-load-crypto.sh
+++ modules.d/01fips/fips-load-crypto.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
 
-if ! fipsmode=$(getarg fips) || [ $fipsmode = "0" ]; then
+type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
+
+if ! fipsmode=$(getarg fips) || [ "$fipsmode" = "0" ]; then
     rm -f -- /etc/modprobe.d/fips.conf > /dev/null 2>&1
+elif [ -z "$fipsmode" ]; then
+    die "FIPS mode have to be enabled by 'fips=1' not just 'fips'"
 else
     . /sbin/fips.sh
     fips_load_crypto || die "FIPS integrity test failed"
