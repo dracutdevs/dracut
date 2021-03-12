3e0aa50fa0c10a5bb0101a28e656a23c230cc231
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:31 +0100

    fix: shellcheck for modules.d/05busybox/module-setup.sh

--- modules.d/05busybox/module-setup.sh
+++ modules.d/05busybox/module-setup.sh
@@ -17,7 +17,7 @@ install() {
     local _i _path _busybox
     local _progs=()
     _busybox=$(find_binary busybox)
-    inst $_busybox /usr/bin/busybox
+    inst "$_busybox" /usr/bin/busybox
     for _i in $($_busybox --list); do
         [[ ${_i} == busybox ]] && continue
         _progs+=("${_i}")
@@ -26,6 +26,6 @@ install() {
     for _i in "${_progs[@]}"; do
         _path=$(find_binary "$_i")
         [ -z "$_path" ] && continue
-        ln_r /usr/bin/busybox $_path
+        ln_r /usr/bin/busybox "$_path"
     done
 }
