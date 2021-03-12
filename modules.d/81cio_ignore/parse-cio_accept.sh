cfd36d16b085fd73affe0fa2e879d8916528f0e9
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:34 +0100

    fix: shellcheck for modules.d/81cio_ignore/parse-cio_accept.sh

--- modules.d/81cio_ignore/parse-cio_accept.sh
+++ modules.d/81cio_ignore/parse-cio_accept.sh
@@ -5,17 +5,17 @@
 CIO_IGNORE=$(getarg cio_ignore)
 CIO_ACCEPT=$(getarg rd.cio_accept)
 
-if [ -z $CIO_IGNORE ]; then
+if [ -z "$CIO_IGNORE" ]; then
     info "cio_ignored disabled on commandline"
     return
 fi
 if [ -n "$CIO_ACCEPT" ]; then
     OLDIFS="$IFS"
     IFS=,
-    set -- $CIO_ACCEPT
+    set -- "$CIO_ACCEPT"
     while (($# > 0)); do
         info "Enabling device $1"
-        cio_ignore --remove $1
+        cio_ignore --remove "$1"
         shift
     done
     IFS="$OLDIFS"
