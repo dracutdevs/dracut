22651b3f0fab58f6818cb80ad3cc8beefba4833d
Harald Hoyer <harald@redhat.com>
2021-03-12 14:50:16 +0100

    fix: shellcheck for modules.d/04watchdog/watchdog.sh

--- modules.d/04watchdog/watchdog.sh
+++ modules.d/04watchdog/watchdog.sh
@@ -2,10 +2,10 @@
 if [ -e /dev/watchdog ]; then
     if [ ! -e /tmp/watchdog_timeout ]; then
         wdctl -s 60 /dev/watchdog > /dev/null 2>&1
-        > /tmp/watchdog_timeout
+        : > /tmp/watchdog_timeout
     fi
     info "Triggering watchdog"
-    > /dev/watchdog
+    : > /dev/watchdog
 else
     modprobe ib700wdt
     modprobe i6300esb
