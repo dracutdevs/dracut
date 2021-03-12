44d3176b63106895b03e7cb35d578bd48f9d325d
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:38 +0100

    fix: shellcheck for modules.d/95fstab-sys/mount-sys.sh

--- modules.d/95fstab-sys/mount-sys.sh
+++ modules.d/95fstab-sys/mount-sys.sh
@@ -9,19 +9,19 @@ fstab_mount() {
     info "Mounting from $1"
     while read _dev _mp _fs _opts _dump _pass _rest || [ -n "$_dev" ]; do
         [ -z "${_dev%%#*}" ] && continue # Skip comment lines
-        ismounted $_mp && continue       # Skip mounted filesystem
+        ismounted "$_mp" && continue       # Skip mounted filesystem
         if [ "$_pass" -gt 0 ] && ! strstr "$_opts" _netdev; then
             fsck_single "$_dev" "$_fs" "$_opts"
         fi
         _fs=$(det_fs "$_dev" "$_fs")
         info "Mounting $_dev"
         if [ -d "$NEWROOT/$_mp" ]; then
-            mount -v -t $_fs -o $_opts $_dev "$NEWROOT/$_mp" 2>&1 | vinfo
+            mount -v -t "$_fs" -o "$_opts" "$_dev" "$NEWROOT/$_mp" 2>&1 | vinfo
         else
             [ -d "$_mp" ] || mkdir -p "$_mp"
-            mount -v -t $_fs -o $_opts $_dev $_mp 2>&1 | vinfo
+            mount -v -t "$_fs" -o "$_opts" "$_dev" "$_mp" 2>&1 | vinfo
         fi
-    done < $1
+    done < "$1"
     return 0
 }
 
@@ -32,8 +32,8 @@ if [ -z "$DRACUT_SYSTEMD" ]; then
 fi
 
 # prefer $NEWROOT/etc/fstab.sys over local /etc/fstab.sys
-if [ -f $NEWROOT/etc/fstab.sys ]; then
-    fstab_mount $NEWROOT/etc/fstab.sys
+if [ -f "$NEWROOT"/etc/fstab.sys ]; then
+    fstab_mount "$NEWROOT"/etc/fstab.sys
 elif [ -f /etc/fstab.sys ]; then
     fstab_mount /etc/fstab.sys
 fi
