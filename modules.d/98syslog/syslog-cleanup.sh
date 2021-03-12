34723d574bcaf19cc5a2283abd8f55849d5f3d0d
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:43 +0100

    fix: shellcheck for modules.d/98syslog/syslog-cleanup.sh

--- modules.d/98syslog/syslog-cleanup.sh
+++ modules.d/98syslog/syslog-cleanup.sh
@@ -7,7 +7,7 @@ type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 if [ -f /tmp/syslog.server ]; then
     read syslogtype < /tmp/syslog.type
     if command -v "${syslogtype}-stop" > /dev/null; then
-        ${syslogtype}-stop
+        "${syslogtype}"-stop
     else
         warn "syslog-cleanup: Could not find script to stop syslog of type \"$syslogtype\". Syslog will not be stopped."
     fi
