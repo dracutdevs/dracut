6252c33cdc1809595e212ed3b58ccdea113c0115
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:35 +0100

    fix: shellcheck for modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh

--- modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
+++ modules.d/90dmsquash-live/dmsquash-liveiso-genrules.sh
@@ -3,6 +3,6 @@
 if [ "${root%%:*}" = "liveiso" ]; then
     {
         printf 'KERNEL=="loop-control", RUN+="/sbin/initqueue --settled --onetime --unique /sbin/dmsquash-live-root `/sbin/losetup -f --show %s`"\n' \
-            ${root#liveiso:}
+            "${root#liveiso:}"
     } >> /etc/udev/rules.d/99-liveiso-mount.rules
 fi
