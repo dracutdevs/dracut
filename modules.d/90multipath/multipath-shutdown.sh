8ad71ed93796618888f01a41724ccb4c22a8cbf9
Harald Hoyer <harald@redhat.com>
2021-03-12 15:05:36 +0100

    fix: shellcheck for modules.d/90multipath/multipath-shutdown.sh

--- modules.d/90multipath/multipath-shutdown.sh
+++ modules.d/90multipath/multipath-shutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in $(multipath -l -v1); do
-    if ! dmsetup table $i | sed -n '/.*queue_if_no_path.*/q1'; then
-        dmsetup message $i 0 fail_if_no_path
+    if ! dmsetup table "$i" | sed -n '/.*queue_if_no_path.*/q1'; then
+        dmsetup message "$i" 0 fail_if_no_path
     fi
 done
