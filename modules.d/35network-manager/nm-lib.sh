d888318728a2296622f9e9a4ff626f937ff422c9
Harald Hoyer <harald@redhat.com>
2021-03-15 11:44:51 +0100

    fix: shellcheck for modules.d/35network-manager/nm-lib.sh

--- modules.d/35network-manager/nm-lib.sh
+++ modules.d/35network-manager/nm-lib.sh
@@ -4,7 +4,7 @@ type getcmdline > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 nm_generate_connections() {
     rm -f /run/NetworkManager/system-connections/*
-    /usr/libexec/nm-initrd-generator -- $(getcmdline)
+    /usr/libexec/nm-initrd-generator -- "$(getcmdline)"
 
     if getargbool 0 rd.neednet; then
         for i in /usr/lib/NetworkManager/system-connections/* \
