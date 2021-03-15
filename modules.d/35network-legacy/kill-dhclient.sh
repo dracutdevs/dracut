b7688e3fe36ac38f860bb4508731892c7250175e
Harald Hoyer <harald@redhat.com>
2021-03-15 11:44:50 +0100

    fix: shellcheck for modules.d/35network-legacy/kill-dhclient.sh

--- modules.d/35network-legacy/kill-dhclient.sh
+++ modules.d/35network-legacy/kill-dhclient.sh
@@ -2,7 +2,7 @@
 
 for f in /tmp/dhclient.*.pid; do
     [ -e "$f" ] || continue
-    read PID < "$f"
+    read -r PID < "$f"
     kill "$PID" > /dev/null 2>&1
 done
 
@@ -10,6 +10,6 @@ sleep 0.1
 
 for f in /tmp/dhclient.*.pid; do
     [ -e "$f" ] || continue
-    read PID < "$f"
+    read -r PID < "$f"
     kill -9 "$PID" > /dev/null 2>&1
 done
