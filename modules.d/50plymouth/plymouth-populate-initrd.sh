b77a8de4b89dc2a51857ef77f6e76b435a48d7c0
Harald Hoyer <harald@redhat.com>
2021-03-15 11:44:52 +0100

    fix: shellcheck for modules.d/50plymouth/plymouth-populate-initrd.sh

--- modules.d/50plymouth/plymouth-populate-initrd.sh
+++ modules.d/50plymouth/plymouth-populate-initrd.sh
@@ -8,6 +8,7 @@ inst_multiple plymouthd plymouth \
 
 test -e "${PLYMOUTH_LOGO_FILE}" && inst_simple "${PLYMOUTH_LOGO_FILE}"
 
+# shellcheck disable=SC2174
 mkdir -m 0755 -p "${initdir}/usr/share/plymouth"
 
 inst_libdir_file "plymouth/text.so" "plymouth/details.so"
@@ -27,13 +28,14 @@ if [[ $hostonly ]]; then
     if [[ -L $dracutsysrootdir/usr/share/plymouth/themes/default.plymouth ]]; then
         inst /usr/share/plymouth/themes/default.plymouth
         # Install plugin for this theme
-        PLYMOUTH_PLUGIN=$(grep "^ModuleName=" "$dracutsysrootdir"/usr/share/plymouth/themes/default.plymouth | while read a b c || [ -n "$b" ]; do echo "$b"; done)
+        PLYMOUTH_PLUGIN=$(grep "^ModuleName=" "$dracutsysrootdir"/usr/share/plymouth/themes/default.plymouth | while read -r _ b _ || [ -n "$b" ]; do echo "$b"; done)
         inst_libdir_file "plymouth/${PLYMOUTH_PLUGIN}.so"
     fi
 else
     for x in "$dracutsysrootdir"/usr/share/plymouth/themes/{text,details}/*; do
         [[ -f $x ]] || continue
         THEME_DIR=$(dirname "${x#$dracutsysrootdir}")
+        # shellcheck disable=SC2174
         mkdir -m 0755 -p "${initdir}/$THEME_DIR"
         inst_multiple "${x#$dracutsysrootdir}"
     done
