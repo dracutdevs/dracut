bbf510a7f9fa43cb95bedf1d3e4c0e10ee8bf9e0
Harald Hoyer <harald@redhat.com>
2021-03-18 15:15:13 +0100

    fix: shellcheck for test/TEST 01-BASIC/test-init.sh

--- test/TEST-01-BASIC/test-init.sh
+++ test/TEST-01-BASIC/test-init.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-> /dev/watchdog
+# shellcheck disable=SC2015
+
+: > /dev/watchdog
 getcmdline() {
     while read -r _line || [ -n "$_line" ]; do
         printf "%s" "$_line"
@@ -96,8 +98,7 @@ getargbool() {
     local _default
     _default="$1"
     shift
-    _b=$(getarg "$@")
-    [ $? -ne 0 -a -z "$_b" ] && _b="$_default"
+    _b=$(getarg "$@") || _b="$_default"
     if [ -n "$_b" ]; then
         [ "$_b" = "0" ] && return 1
         [ "$_b" = "no" ] && return 1
@@ -107,7 +108,7 @@ getargbool() {
 }
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 strstr() { [ "${1##*"$2"*}" != "$1" ]; }
-CMDLINE=$(while read line || [ -n "$line" ]; do echo "$line"; done < /proc/cmdline)
+CMDLINE=$(while read -r line || [ -n "$line" ]; do echo "$line"; done < /proc/cmdline)
 plymouth --quit
 exec > /dev/console 2>&1
 echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/sdb
