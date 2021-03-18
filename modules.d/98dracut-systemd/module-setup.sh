5e95d2b853497a2effcae575e7187270db71c3ad
Harald Hoyer <harald@redhat.com>
2021-03-18 15:15:10 +0100

    fix: shellcheck for modules.d/98dracut-systemd/module-setup.sh

--- modules.d/98dracut-systemd/module-setup.sh
+++ modules.d/98dracut-systemd/module-setup.sh
@@ -19,7 +19,6 @@ installkernel() {
 
 # called by dracut
 install() {
-    local _mods
     inst_script "$moddir/dracut-emergency.sh" /bin/dracut-emergency
     inst_simple "$moddir/emergency.service" "${systemdsystemunitdir}"/emergency.service
     inst_simple "$moddir/dracut-emergency.service" "${systemdsystemunitdir}"/dracut-emergency.service
