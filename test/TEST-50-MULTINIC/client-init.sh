fe37f9c2715ae4b6c88631d551bd4f9b32f6d5ca
Harald Hoyer <harald@redhat.com>
2021-03-18 15:15:18 +0100

    fix: shellcheck for test/TEST 50-MULTINIC/client-init.sh

--- test/TEST-50-MULTINIC/client-init.sh
+++ test/TEST-50-MULTINIC/client-init.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+# shellcheck disable=SC2015
+
 getcmdline() {
     while read -r _line || [ -n "$_line" ]; do
         printf "%s" "$_line"
@@ -95,8 +97,7 @@ getargbool() {
     local _default
     _default="$1"
     shift
-    _b=$(getarg "$@")
-    [ $? -ne 0 -a -z "$_b" ] && _b="$_default"
+    _b=$(getarg "$@") || _b="$_default"
     if [ -n "$_b" ]; then
         [ "$_b" = "0" ] && return 1
         [ "$_b" = "no" ] && return 1
@@ -110,7 +111,7 @@ set -x
 export PATH=/sbin:/bin:/usr/sbin:/usr/bin
 strstr() { [ "${1##*"$2"*}" != "$1" ]; }
 strglobin() { [ -n "$1" -a -z "${1##*$2*}" ]; }
-CMDLINE=$(while read line || [ -n "$line" ]; do echo "$line"; done < /proc/cmdline)
+CMDLINE=$(while read -r line || [ -n "$line" ]; do echo "$line"; done < /proc/cmdline)
 export TERM=linux
 export PS1='initramfs-test:\w\$ '
 stty sane
