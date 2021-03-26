9dd4d42265e8412f4a1c3f25a0b5219dec931d74
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:25 +0100

    fix(dasd): shellcheck for modules.d/95dasd

--- modules.d/95dasd/parse-dasd.sh
+++ modules.d/95dasd/parse-dasd.sh
@@ -3,6 +3,7 @@ for dasd_arg in $(getargs rd.dasd= -d rd_DASD= DASD=); do
     (
         local OLDIFS="$IFS"
         IFS=","
+        # shellcheck disable=SC2086
         set -- $dasd_arg
         IFS="$OLDIFS"
         echo "$@" | normalize_dasd_arg >> /etc/dasd.conf
