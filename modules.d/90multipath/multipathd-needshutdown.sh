37f87f9883d8033f887715b38003174050958c56
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:22 +0100

    fix(multipath): shellcheck for modules.d/90multipath

--- modules.d/90multipath/multipathd-needshutdown.sh
+++ modules.d/90multipath/multipathd-needshutdown.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 for i in $(multipath -l -v1); do
-    if $(dmsetup table $i | sed -n '/.*queue_if_no_path.*/q1'); then
+    if dmsetup table "$i" | sed -n '/.*queue_if_no_path.*/q1'; then
         need_shutdown
         break
     fi
--- modules.d/90multipath/multipathd-stop.sh
+++ modules.d/90multipath/multipathd-stop.sh
@@ -4,7 +4,7 @@ if [ -e /etc/multipath.conf ]; then
     HARD=""
     while pidof multipathd > /dev/null 2>&1; do
         for pid in $(pidof multipathd); do
-            kill $HARD $pid > /dev/null 2>&1
+            kill "$HARD" "$pid" > /dev/null 2>&1
         done
         HARD="-9"
     done
