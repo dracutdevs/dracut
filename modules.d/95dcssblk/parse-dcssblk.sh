f732667d141cce2f8e076c9e802d6de97f6afa58
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:25 +0100

    fix(dcssblk): shellcheck for modules.d/95dcssblk

--- modules.d/95dcssblk/parse-dcssblk.sh
+++ modules.d/95dcssblk/parse-dcssblk.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
-# -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
-# ex: ts=8 sw=4 sts=4 et filetype=sh
-dcssblk_arg=$(getarg rd.dcssblk=)
-if [ $? = 0 ]; then
+
+if dcssblk_arg=$(getarg rd.dcssblk=); then
     info "Loading dcssblk segments=$dcssblk_arg"
-    modprobe dcssblk segments=$dcssblk_arg
+    modprobe dcssblk segments="$dcssblk_arg"
 fi
