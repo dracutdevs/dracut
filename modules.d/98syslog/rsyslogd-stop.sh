30dccccba22ef84ad585f8f413a74272b03e01ed
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:33 +0100

    fix(syslog): shellcheck for modules.d/98syslog

--- modules.d/98syslog/rsyslogd-stop.sh
+++ modules.d/98syslog/rsyslogd-stop.sh
@@ -5,9 +5,9 @@ type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 # Kills rsyslogd
 
 if [ -f /var/run/syslogd.pid ]; then
-    read pid < /var/run/syslogd.pid
-    kill $pid
-    kill -0 $pid && kill -9 $pid
+    read -r pid < /var/run/syslogd.pid
+    kill "$pid"
+    kill -0 "$pid" && kill -9 "$pid"
 else
     warn "rsyslogd-stop: Could not find a pid for rsyslogd. Won't kill it."
 fi
--- modules.d/98syslog/syslog-cleanup.sh
+++ modules.d/98syslog/syslog-cleanup.sh
@@ -5,9 +5,9 @@
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 if [ -f /tmp/syslog.server ]; then
-    read syslogtype < /tmp/syslog.type
+    read -r syslogtype < /tmp/syslog.type
     if command -v "${syslogtype}-stop" > /dev/null; then
-        ${syslogtype}-stop
+        "${syslogtype}"-stop
     else
         warn "syslog-cleanup: Could not find script to stop syslog of type \"$syslogtype\". Syslog will not be stopped."
     fi
