d8d0b03cb419e77f569b948b254949ddb975c102
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:27 +0100

    fix(lunmask): shellcheck for modules.d/95lunmask

--- modules.d/95lunmask/parse-lunmask.sh
+++ modules.d/95lunmask/parse-lunmask.sh
@@ -7,17 +7,16 @@ create_udev_rule() {
     local tgtid=$2
     local lun=$3
     local _rule=/etc/udev/rules.d/51-${transport}-lunmask-${tgtid}.rules
-    local _cu_type _dev_type
 
-    [ -e ${_rule} ] && return 0
+    [ -e "${_rule}" ] && return 0
 
     if [ ! -f "$_rule" ]; then
         if [ "$transport" = "fc" ]; then
-            cat > $_rule << EOF
+            cat > "$_rule" << EOF
 ACTION=="add", SUBSYSTEM=="fc_remote_ports", ATTR{port_name}=="$tgtid", PROGRAM="fc_transport_scan_lun.sh $lun"
 EOF
         elif [ "$transport" = "sas" ]; then
-            cat > $_rule << EOF
+            cat > "$_rule" << EOF
 ACTION=="add", SUBSYSTEM=="sas_device", ATTR{sas_address}=="$tgtid", PROGRAM="sas_transport_scan_lun.sh $lun"
 EOF
         fi
@@ -25,16 +24,11 @@ EOF
 }
 
 for lunmask_arg in $(getargs rd.lunmask); do
-    (
-        local OLDIFS="$IFS"
-        local IFS=","
-        set $lunmask_arg
-        IFS="$OLDIFS"
-        if [ -d /sys/module/scsi_mod ]; then
-            printf "manual" > /sys/module/scsi_mod/parameters/scan
-        elif [ ! -f /etc/modprobe.d/95lunmask.conf ]; then
-            echo "options scsi_mod scan=manual" > /etc/modprobe.d/95lunmask.conf
-        fi
-        create_udev_rule $1 $2 $3
-    )
+    IFS="," read -r -a _args <<< "$lunmask_arg"
+    if [ -d /sys/module/scsi_mod ]; then
+        printf "manual" > /sys/module/scsi_mod/parameters/scan
+    elif [ ! -f /etc/modprobe.d/95lunmask.conf ]; then
+        echo "options scsi_mod scan=manual" > /etc/modprobe.d/95lunmask.conf
+    fi
+    create_udev_rule "${_args[@]}"
 done
--- modules.d/95lunmask/sas_transport_scan_lun.sh
+++ modules.d/95lunmask/sas_transport_scan_lun.sh
@@ -8,7 +8,7 @@
 #    PROGRAM="sas_transport_lun_scan lun"
 #
 
-[ -z $DEVPATH ] && exit 1
+[ -z "$DEVPATH" ] && exit 1
 
 if [ -n "$1" ]; then
     LUN=$1
@@ -19,8 +19,8 @@ ID=${DEVPATH##*/end_device-}
 HOST=${ID%%:*}
 CHANNEL=${ID%%-*}
 CHANNEL=${CHANNEL#*:}
-if [ -f /sys$DEVPATH/scsi_target_id ]; then
-    TARGET=$(cat /sys$DEVPATH/scsi_target_id)
+if [ -f /sys"$DEVPATH"/scsi_target_id ]; then
+    TARGET=$(cat /sys"$DEVPATH"/scsi_target_id)
 fi
 [ -z "$TARGET" ] && exit 1
-echo 0 $TARGET $LUN > /sys/class/scsi_host/host$HOST/scan
+echo 0 "$TARGET" $LUN > /sys/class/scsi_host/host"$HOST"/scan
