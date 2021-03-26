a201be999ed59e8b26ca19580b43923d5c0af47d
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:27 +0100

    fix(nbd): shellcheck for modules.d/95nbd

--- modules.d/95nbd/module-setup.sh
+++ modules.d/95nbd/module-setup.sh
@@ -33,7 +33,7 @@ install() {
     inst_hook cmdline 90 "$moddir/parse-nbdroot.sh"
     inst_script "$moddir/nbdroot.sh" "/sbin/nbdroot"
     if dracut_module_included "systemd-initrd"; then
-        inst_script "$moddir/nbd-generator.sh" $systemdutildir/system-generators/dracut-nbd-generator
+        inst_script "$moddir/nbd-generator.sh" "$systemdutildir"/system-generators/dracut-nbd-generator
     fi
     dracut_need_initqueue
 }
