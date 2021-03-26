31c09174208e7f4910e1d1af64f624c8a2450056
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:24 +0100

    fix(zipl): shellcheck for modules.d/91zipl

--- modules.d/91zipl/install_zipl_cmdline.sh
+++ modules.d/91zipl/install_zipl_cmdline.sh
@@ -5,16 +5,15 @@ MNT=/boot/zipl
 
 if [ -z "$DEV" ]; then
     echo "No IPL device given"
-    > /tmp/install.zipl.cmdline-done
+    : > /tmp/install.zipl.cmdline-done
     exit 1
 fi
 
 [ -d ${MNT} ] || mkdir -p ${MNT}
 
-mount -o ro ${DEV} ${MNT}
-if [ "$?" != "0" ]; then
+if ! mount -o ro "${DEV}" ${MNT}; then
     echo "Failed to mount ${MNT}"
-    > /tmp/install.zipl.cmdline-done
+    : > /tmp/install.zipl.cmdline-done
     exit 1
 fi
 
@@ -23,9 +22,9 @@ if [ -f ${MNT}/dracut-cmdline.conf ]; then
 fi
 
 if [ -f ${MNT}/active_devices.txt ]; then
-    while read dev etc; do
+    while read -r dev _ || [[ $dev ]]; do
         [ "$dev" = "#" -o "$dev" = "" ] && continue
-        cio_ignore -r $dev
+        cio_ignore -r "$dev"
     done < ${MNT}/active_devices.txt
 fi
 
@@ -35,6 +34,6 @@ if [ -f /etc/cmdline.d/99zipl.conf ]; then
     systemctl restart dracut-cmdline.service
     systemctl restart systemd-udev-trigger.service
 fi
-> /tmp/install.zipl.cmdline-done
+: > /tmp/install.zipl.cmdline-done
 
 exit 0
