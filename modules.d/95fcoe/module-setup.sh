1c8a343df57d852b30ac3ff7a248f2bb4ebfc7ff
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:26 +0100

    fix(fcoe): shellcheck for modules.d/95fcoe

--- modules.d/95fcoe/module-setup.sh
+++ modules.d/95fcoe/module-setup.sh
@@ -3,14 +3,14 @@
 # called by dracut
 check() {
     is_fcoe() {
-        block_is_fcoe $1 || return 1
+        block_is_fcoe "$1" || return 1
     }
 
     [[ $hostonly ]] || [[ $mount_needs ]] && {
         for_each_host_dev_and_slaves is_fcoe || return 255
     }
 
-    require_binaries dcbtool fipvlan lldpad ip readlink fcoemon fcoeadm || return 1
+    require_binaries dcbtool fipvlan lldpad ip readlink fcoemon fcoeadm tr || return 1
     return 0
 }
 
@@ -28,12 +28,12 @@ installkernel() {
 get_vlan_parent() {
     local link=$1
 
-    [ -d $link ] || return
-    read iflink < $link/iflink
+    [ -d "$link" ] || return
+    read -r iflink < "$link"/iflink
     for if in /sys/class/net/*; do
-        read idx < $if/ifindex
-        if [ $idx -eq $iflink ]; then
-            echo ${if##*/}
+        read -r idx < "$if"/ifindex
+        if [ "$idx" -eq "$iflink" ]; then
+            echo "${if##*/}"
         fi
     done
 }
@@ -42,27 +42,27 @@ get_vlan_parent() {
 cmdline() {
     {
         for c in /sys/bus/fcoe/devices/ctlr_*; do
-            [ -L $c ] || continue
-            read enabled < $c/enabled
-            read mode < $c/mode
-            [ $enabled -eq 0 ] && continue
-            if [ $mode = "VN2VN" ]; then
+            [ -L "$c" ] || continue
+            read -r enabled < "$c"/enabled
+            read -r mode < "$c"/mode
+            [ "$enabled" -eq 0 ] && continue
+            if [ "$mode" = "VN2VN" ]; then
                 mode="vn2vn"
             else
                 mode="fabric"
             fi
             d=$(
-                cd -P $c
-                echo $PWD
+                cd -P "$c" || exit
+                echo "$PWD"
             )
             i=${d%/*}
             ifname=${i##*/}
-            read mac < ${i}/address
-            s=$(dcbtool gc ${i##*/} dcb 2> /dev/null | sed -n 's/^DCB State:\t*\(.*\)/\1/p')
+            read -r mac < "${i}"/address
+            s=$(dcbtool gc "${i##*/}" dcb 2> /dev/null | sed -n 's/^DCB State:\t*\(.*\)/\1/p')
             if [ -z "$s" ]; then
-                p=$(get_vlan_parent ${i})
+                p=$(get_vlan_parent "${i}")
                 if [ "$p" ]; then
-                    s=$(dcbtool gc ${p} dcb 2> /dev/null | sed -n 's/^DCB State:\t*\(.*\)/\1/p')
+                    s=$(dcbtool gc "${p}" dcb 2> /dev/null | sed -n 's/^DCB State:\t*\(.*\)/\1/p')
                     ifname=${p##*/}
                 fi
             fi
@@ -78,11 +78,14 @@ cmdline() {
             # DCB_REQUIRED in "/etc/fcoe/cfg-xxx" is expected to set to "no".
             #
             # Force "nodcb" if there's any DCB_REQUIRED="no"(child or vlan parent).
-            grep -q '^[[:blank:]]*DCB_REQUIRED="no"' /etc/fcoe/cfg-${i##*/} &> /dev/null
-            [ $? -eq 0 ] && dcb="nodcb"
+            if grep -q '^[[:blank:]]*DCB_REQUIRED="no"' /etc/fcoe/cfg-"${i##*/}" &> /dev/null; then
+                dcb="nodcb"
+            fi
+
             if [ "$p" ]; then
-                grep -q '^[[:blank:]]*DCB_REQUIRED="no"' /etc/fcoe/cfg-${p} &> /dev/null
-                [ $? -eq 0 ] && dcb="nodcb"
+                if grep -q '^[[:blank:]]*DCB_REQUIRED="no"' /etc/fcoe/cfg-"${p}" &> /dev/null; then
+                    dcb="nodcb"
+                fi
             fi
 
             echo "ifname=${ifname}:${mac}"
@@ -93,7 +96,7 @@ cmdline() {
 
 # called by dracut
 install() {
-    inst_multiple ip dcbtool fipvlan lldpad readlink lldptool fcoemon fcoeadm
+    inst_multiple ip dcbtool fipvlan lldpad readlink lldptool fcoemon fcoeadm tr
     if [[ -e $dracutsysrootdir/etc/hba.conf ]]; then
         inst_libdir_file 'libhbalinux.so*'
         inst_simple "/etc/hba.conf"
@@ -103,14 +106,14 @@ install() {
     mkdir -m 0755 -p "$initdir/etc/fcoe"
 
     if [[ $hostonly_cmdline == "yes" ]]; then
-        local _fcoeconf=$(cmdline)
+        local _fcoeconf
+        _fcoeconf=$(cmdline)
         [[ $_fcoeconf ]] && printf "%s\n" "$_fcoeconf" >> "${initdir}/etc/cmdline.d/95fcoe.conf"
     fi
     inst_multiple "/etc/fcoe/cfg-*"
 
     inst "$moddir/fcoe-up.sh" "/sbin/fcoe-up"
     inst "$moddir/fcoe-edd.sh" "/sbin/fcoe-edd"
-    inst "$moddir/fcoe-genrules.sh" "/sbin/fcoe-genrules.sh"
     inst_hook pre-trigger 03 "$moddir/lldpad.sh"
     inst_hook cmdline 99 "$moddir/parse-fcoe.sh"
     inst_hook cleanup 90 "$moddir/cleanup-fcoe.sh"
