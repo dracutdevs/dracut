abc9879c9d1392cedb1dbca3ba9718fd36dad87d
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:18 +0100

    fix(cio_ignore): shellcheck for modules.d/81cio_ignore

--- modules.d/81cio_ignore/module-setup.sh
+++ modules.d/81cio_ignore/module-setup.sh
@@ -14,7 +14,7 @@ cmdline() {
     local cio_accept
 
     if [ -e /boot/zipl/active_devices.txt ]; then
-        while read dev etc; do
+        while read -r dev _; do
             [ "$dev" = "#" -o "$dev" = "" ] && continue
             if [ -z "$cio_accept" ]; then
                 cio_accept="$dev"
@@ -31,7 +31,8 @@ cmdline() {
 # called by dracut
 install() {
     if [[ $hostonly_cmdline == "yes" ]]; then
-        local _cio_accept=$(cmdline)
+        local _cio_accept
+        _cio_accept=$(cmdline)
         [[ $_cio_accept ]] && printf "%s\n" "$_cio_accept" >> "${initdir}/etc/cmdline.d/01cio_accept.conf"
     fi
 
