7f36619559f29897f16ec3f63ac95fcf17caa4d9
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:28 +0100

    fix(qeth_rules): shellcheck for modules.d/95qeth_rules

--- modules.d/95qeth_rules/module-setup.sh
+++ modules.d/95qeth_rules/module-setup.sh
@@ -10,8 +10,8 @@ check() {
 
     [[ $hostonly ]] && {
         for i in /sys/devices/qeth/*/online; do
-            read _online < $i
-            [ $_online -eq 1 ] && return 0
+            read -r _online < "$i"
+            [ "$_online" -eq 1 ] && return 0
         done
     }
     return 255
@@ -25,7 +25,7 @@ installkernel() {
 # called by dracut
 install() {
     ccwid() {
-        qeth_path=$(readlink -e -q $1/device)
+        qeth_path=$(readlink -e -q "$1"/device)
         basename "$qeth_path"
     }
 
@@ -44,15 +44,15 @@ install() {
         # not readable in qeth interfaces
         # that have just been assembled, ignore
         # read error and assume no carrier
-        read carrier 2> /dev/null < "$1/carrier"
+        read -r carrier 2> /dev/null < "$1/carrier"
         [ "$carrier" -eq 1 ] && return 0
         return 1
     }
 
     for dev in /sys/class/net/*; do
-        has_carrier $dev || continue
-        id=$(ccwid $dev)
-        [ -n "$id" ] && inst_rules_qeth $id
+        has_carrier "$dev" || continue
+        id=$(ccwid "$dev")
+        [ -n "$id" ] && inst_rules_qeth "$id"
     done
 
     inst_simple /usr/lib/udev/collect
