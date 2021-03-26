58ab677405b787eeb5194c02046faf590b2ecc97
Harald Hoyer <harald@redhat.com>
2021-03-26 10:29:20 +0100

    fix(kernel-modules): shellcheck for modules.d/90kernel-modules

--- modules.d/90kernel-modules/insmodpost.sh
+++ modules.d/90kernel-modules/insmodpost.sh
@@ -6,7 +6,7 @@ for modlist in $(getargs rd.driver.post -d rdinsmodpost=); do
     (
         IFS=,
         for m in $modlist; do
-            modprobe $m
+            modprobe "$m"
         done
     )
 done
