c2ecc4d131876383b47820a2e8d1a6f8a11716d9
Harald Hoyer <harald@redhat.com>
2021-03-30 15:02:31 +0200

    fix(memstrack): correct dependencies
    memstrack does not need to depend on `bash` but does on `systemd`.

--- modules.d/99memstrack/memstrack-report.sh
+++ modules.d/99memstrack/memstrack-report.sh
@@ -1,15 +1,15 @@
-#!/bin/bash
+#!/bin/sh
 . /lib/dracut-lib.sh
 
 if ! [ "$DEBUG_MEM_LEVEL" -ge 4 ]; then
     return 0
 fi
 
-if type -P systemctl > /dev/null; then
+if command -v systemctl > /dev/null; then
     systemctl stop memstrack.service
 else
     pkill --signal INT '[m]emstrack'
-    while [[ $(pgrep '[m]emstrack') ]]; do
+    while pgrep -c '[m]emstrack' > /dev/null; do
         sleep 1
     done
 fi
