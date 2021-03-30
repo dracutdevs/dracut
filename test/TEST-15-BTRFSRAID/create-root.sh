5f66d400e461216275d0fde2e9d8f2650ed8ad5b
Harald Hoyer <harald@redhat.com>
2021-03-30 12:17:35 +0200

    fix(TEST BTRFSRAID): shellcheck

--- test/TEST-15-BTRFSRAID/create-root.sh
+++ test/TEST-15-BTRFSRAID/create-root.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # don't let udev and this script step on eachother's toes
 for x in 64-lvm.rules 70-mdadm.rules 99-mount-rules; do
-    > "/etc/udev/rules.d/$x"
+    : > "/etc/udev/rules.d/$x"
 done
 modprobe btrfs
 udevadm control --reload
