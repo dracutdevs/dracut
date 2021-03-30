06ba6aa3100177b2c077ac4953eb3ad0728a1eea
Harald Hoyer <harald@redhat.com>
2021-03-30 12:19:52 +0200

    fix(TEST NBD): shellcheck

--- test/TEST-40-NBD/create-server-root.sh
+++ test/TEST-40-NBD/create-server-root.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # don't let udev and this script step on eachother's toes
 for x in 64-lvm.rules 70-mdadm.rules 99-mount-rules; do
-    > "/etc/udev/rules.d/$x"
+    : > "/etc/udev/rules.d/$x"
 done
 rm -f -- /etc/lvm/lvm.conf
 udevadm control --reload
