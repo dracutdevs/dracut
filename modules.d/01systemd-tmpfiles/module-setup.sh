a7f7bd1427dd997aba48a2f2f4b7386df032e27b
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-05-05 09:26:26 +0000

    feat(systemd-tmpfiles): introducing the systemd-tmpfiles module
    Introducing the systemd-tmpfiles module

---
+++ modules.d/01systemd-tmpfiles/module-setup.sh
@@ -0,0 +1,71 @@
+#!/bin/bash
+# This file is part of dracut.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Prerequisite check(s) for module.
+check() {
+
+    # If the binary(s) requirements are not fulfilled the module can't be installed.
+    require_binaries systemd-tmpfiles || return 1
+
+    # Return 255 to only include the module, if another module requires it.
+    return 255
+
+}
+
+# Module dependency requirements.
+depends() {
+
+    # Return 0 to include the dependent module(s) in the initramfs.
+    return 0
+
+}
+
+# Install the required file(s) and directories for the module in the initramfs.
+install() {
+
+    # Excluding "$tmpfilesdir/home.conf", sets up /home /srv
+    # Excluding "$tmpfilesdir/portables.conf", belongs in seperated portables module
+    # Excluding "$tmpfilesdir/journal-nocow.conf", requires spesific btrfs setup
+    # Excluding "$tmpfilesdir/legacy.conf", belongs in seperated legacy module
+    # Excluding "$tmpfilesdir/systemd-nologin.conf", belongs in seperated pam module
+    # Excluding "$tmpfilesdir/systemd-nspawn.conf", belongs in seperated machined module
+    # Excluding "$tmpfilesdir/systemd-pstore.conf", belongs in seperated pstore module
+    # Excluding "$tmpfilesdir/x11.conf", belongs in seperated x11 module
+
+
+    inst_multiple -o \
+        /usr/lib/group \
+        /usr/lib/passwd \
+        "$tmpfilesdir/etc.conf" \
+        "$tmpfilesdir/static-nodes-permissions.conf" \
+        "$tmpfilesdir/systemd-tmp.conf" \
+        "$tmpfilesdir/systemd.conf" \
+        "$tmpfilesdir/var.conf" \
+        "$systemdsystemunitdir"/systemd-tmpfiles-clean.service \
+        "$systemdsystemunitdir/systemd-tmpfiles-clean.service.d/*.conf" \
+        "$systemdsystemunitdir"/systemd-tmpfiles-setup.service \
+        "$systemdsystemunitdir/systemd-tmpfiles-setup.service.d/*.conf" \
+        "$systemdsystemunitdir"/systemd-tmpfiles-setup-dev.service \
+        "$systemdsystemunitdir/systemd-tmpfiles-setup-dev.service.d/*.conf" \
+        "$systemdsystemunitdir"/systemd-tmpfiles-clean.timer \
+        "$systemdsystemunitdir"/timers.target.wants/systemd-tmpfiles-clean.timer \
+        "$systemdsystemunitdir"/sysinit.target.wants/systemd-tmpfiles-setup-dev.service \
+        "$systemdsystemunitdir"/sysinit.target.wants/systemd-tmpfiles-setup.service \
+        systemd-tmpfiles
+
+    # Install the hosts local user configurations if enabled.
+    if [[ $hostonly ]]; then
+        inst_multiple -H -o \
+            /etc/group \
+            /etc/passwd \
+            "$tmpfilesconfdir/*.conf" \
+            "$systemdsystemconfdir"/systemd-tmpfiles-clean.service \
+            "$systemdsystemconfdir/systemd-tmpfiles-clean.service.d/*.conf" \
+            "$systemdsystemconfdir"/systemd-tmpfiles-setup.service \
+            "$systemdsystemconfdir/systemd-tmpfiles-setup.service.d/*.conf" \
+            "$systemdsystemconfdir"/systemd-tmpfiles-setup-dev.service \
+            "$systemdsystemconfdir/systemd-tmpfiles-setup-dev.service.d/*.conf"
+    fi
+
+}
