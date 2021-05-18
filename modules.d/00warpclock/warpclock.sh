7d205598c6a500b58b4d328e824d0446276f7ced
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-05-18 10:34:46 +0000

    fix(warpclock): minor cleanups
    - Removing shell mode lines
    - Remove uncessary file requirements
    - Remove hard coded path to binary
    - Add missing license header
    - Document sections
    - refactor install section

--- modules.d/00warpclock/warpclock.sh
+++ modules.d/00warpclock/warpclock.sh
@@ -1,4 +1,9 @@
 #!/bin/sh
+# This file is part of dracut warpclock module.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Set the kernel's timezone and reset the system time
+# if adjtime is set to LOCAL.
 
 if test -e /etc/adjtime; then
     while read -r line; do
