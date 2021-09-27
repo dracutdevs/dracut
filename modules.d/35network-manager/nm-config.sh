e07b7ad0e7f5dbb8024336f3075610b3b74ffb2e
Beniamino Galvani <bgalvani@redhat.com>
2021-09-27 13:08:34 +0200

    fix(network-manager): show output on console only with rd.debug enabled
    The module should show the output on console only when initrd debugging is
    enabled.

--- modules.d/35network-manager/nm-config.sh
+++ modules.d/35network-manager/nm-config.sh
@@ -16,6 +16,15 @@ if getargbool 0 rd.debug -d -y rdinitdebug -d -y rdnetdebug; then
         echo '[logging]'
         echo 'level=TRACE'
     ) > /run/NetworkManager/conf.d/initrd-logging.conf
+
+    if [ -n "$DRACUT_SYSTEMD" ]; then
+        mkdir -p /run/systemd/system/nm-initrd.service.d
+        cat << EOF > /run/systemd/system/nm-initrd.service.d/tty-output.conf
+[Service]
+StandardOutput=tty
+EOF
+        systemctl --no-block daemon-reload
+    fi
 fi
 
 nm_generate_connections
