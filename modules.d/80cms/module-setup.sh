35e4069b1215651439b4350cbe084c424e6e96f9
Laszlo Gombos <laszlo.gombos@gmail.com>
2021-11-19 15:32:27 -0500

    fix(dmsquash-live-ntfs): fuse3 no longer requires ulockmgr_server
    fuse3 no longer includes ulockmgr_server.
    https://github.com/libfuse/libfuse/blob/master/ChangeLog.rst#libfuse-300-2016-12-08

--- modules.d/80cms/module-setup.sh
+++ modules.d/80cms/module-setup.sh
@@ -27,7 +27,7 @@ install() {
     inst_script "$moddir/cmsifup.sh" /sbin/cmsifup
     # shellcheck disable=SC2046
     inst_multiple /etc/cmsfs-fuse/filetypes.conf /etc/udev/rules.d/99-fuse.rules /etc/fuse.conf \
-        cmsfs-fuse fusermount ulockmgr_server bash insmod rmmod cat normalize_dasd_arg sed \
+        cmsfs-fuse fusermount bash insmod rmmod cat normalize_dasd_arg sed \
         $(rpm -ql s390utils-base) awk getopt
 
     inst_libdir_file "gconv/*"
