d287ab5e55d4a8db09170e9634a7d899983e12cb
Henrik Gombos <henrik99999@gmail.com>
2021-11-25 18:08:57 -0500

    fix(img-lib): install rmdir
    rmdir is used in img-lib module.

--- modules.d/99img-lib/module-setup.sh
+++ modules.d/99img-lib/module-setup.sh
@@ -14,7 +14,7 @@ depends() {
 
 # called by dracut
 install() {
-    inst_multiple tar gzip dd echo tr
+    inst_multiple tar gzip dd echo tr rmdir
     # TODO: make this conditional on a cmdline flag / config option
     inst_multiple -o cpio xz bzip2 zstd
     inst_simple "$moddir/img-lib.sh" "/lib/img-lib.sh"
