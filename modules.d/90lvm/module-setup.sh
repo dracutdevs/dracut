50e74668434d935db649b5690dc2158b0f87d91c
David Teigland <teigland@redhat.com>
2021-12-08 14:11:13 -0600

    revert(lvm): remove 69-dm-lvm-metad.rules
    This udev rule runs pvscan to autoactivate VGs, which dracut
    does not want to do, and previously disabled by editing the
    rule file and commenting out lines.
    
    This also stops /dev/disk/by-id/lvm-pv-uuid-* symlinks from
    being created in the initrd.

--- modules.d/90lvm/module-setup.sh
+++ modules.d/90lvm/module-setup.sh
@@ -78,20 +78,7 @@ install() {
         unset LVM_SUPPRESS_FD_WARNINGS
     fi
 
-    inst_rules 11-dm-lvm.rules 69-dm-lvm-metad.rules
-
-    # Do not run lvmetad update via pvscan in udev rule  - lvmetad is not running yet in dracut!
-    if [[ -f ${initdir}/lib/udev/rules.d/69-dm-lvm-metad.rules ]]; then
-        if grep -q SYSTEMD_WANTS "${initdir}"/lib/udev/rules.d/69-dm-lvm-metad.rules; then
-            sed -i -e 's/^ENV{SYSTEMD_ALIAS}=.*/# No LVM pvscan in dracut - lvmetad is not running yet/' \
-                "${initdir}"/lib/udev/rules.d/69-dm-lvm-metad.rules
-            sed -i -e 's/^ENV{ID_MODEL}=.*//' "${initdir}"/lib/udev/rules.d/69-dm-lvm-metad.rules
-            sed -i -e 's/^ENV{SYSTEMD_WANTS}+\?=.*//' "${initdir}"/lib/udev/rules.d/69-dm-lvm-metad.rules
-        else
-            sed -i -e 's/.*lvm pvscan.*/# No LVM pvscan for in dracut - lvmetad is not running yet/' \
-                "${initdir}"/lib/udev/rules.d/69-dm-lvm-metad.rules
-        fi
-    fi
+    inst_rules 11-dm-lvm.rules
 
     # Gentoo ebuild for LVM2 prior to 2.02.63-r1 doesn't install above rules
     # files, but provides the one below:
