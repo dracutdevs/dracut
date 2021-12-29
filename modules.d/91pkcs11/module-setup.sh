83ea8cf001a49356cf7814b3c08bdd1c4b4f2763
Jóhann B. Guðmundsson <johannbg@gmail.com>
2021-12-29 13:03:16 +0000

    feat(pkcs11): introducing the pkcs11 module
    This module allows unlocking of an encrypted filesystem using pkcs11.

---
+++ modules.d/91pkcs11/module-setup.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+# This file is part of dracut.
+# SPDX-License-Identifier: GPL-2.0-or-later
+
+# Prerequisite check(s) for module.
+check() {
+
+    # Return 255 to only include the module, if another module requires it.
+    return 255
+
+}
+
+# Module dependency requirements.
+depends() {
+
+    # This module has external dependency on other module(s).
+    echo systemd-udevd
+    # Return 0 to include the dependent module(s) in the initramfs.
+    return 0
+
+}
+
+# Install the required file(s) and directories for the module in the initramfs.
+install() {
+
+    # Install library file(s)
+    _arch=${DRACUT_ARCH:-$(uname -m)}
+    inst_libdir_file \
+        {"tls/$_arch/",tls/,"$_arch/",}"libtasn1.so.*" \
+        {"tls/$_arch/",tls/,"$_arch/",}"libffi.so.*" \
+        {"tls/$_arch/",tls/,"$_arch/",}"libp11-kit.so.*" \
+        {"tls/$_arch/",tls/,"$_arch/",}"libcryptsetup.so.*" \
+        {"tls/$_arch/",tls/,"$_arch/",}"/cryptsetup/libcryptsetup-token-systemd-pkcs11.so.*"
+
+}
