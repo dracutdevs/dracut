4eb7232b74f7f92b10e67f7720618115a4adef83
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2022-01-10 11:46:41 +0100

    fix(network): wrong test of wicked unit
    The test for the wicked service is never met because it
    does not have execute permission.

--- modules.d/40network/module-setup.sh
+++ modules.d/40network/module-setup.sh
@@ -17,7 +17,7 @@ depends() {
     done
 
     if [ -z "$network_handler" ]; then
-        if [[ -x $dracutsysrootdir$systemdsystemunitdir/wicked.service ]]; then
+        if [[ -e $dracutsysrootdir$systemdsystemunitdir/wicked.service ]]; then
             network_handler="network-wicked"
         elif [[ -x $dracutsysrootdir/usr/libexec/nm-initrd-generator ]] || [[ -x $dracutsysrootdir/usr/lib/nm-initrd-generator ]]; then
             network_handler="network-manager"
