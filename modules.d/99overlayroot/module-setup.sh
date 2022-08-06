bfb47ca0a4bf39632d66ed891fa370d94628a65f
TylerHelt0 <wesleyrobhelton@gmail.com>
2022-08-06 12:28:40 -0500

    Remove license

--- modules.d/99overlayroot/module-setup.sh
+++ modules.d/99overlayroot/module-setup.sh
@@ -2,7 +2,7 @@
 
 check() {
     require_binaries "chmod mount mkdir"
-    if [ ! $(cmd /proc/filesystems|grep overlay|cut -f2) ]; then 
+    if [ ! $(cat /proc/filesystems|grep overlay|cut -f2) ]; then 
         echo "Overlay filesystem support is not available on this kernel."
         echo "Overlayroot not installed."
         return 1
