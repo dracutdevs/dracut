7b3f7823449f7aac9a009ad8dc3bfe5b2caa9b9f
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 23:37:52 +0200

    refactor(95resume): cleanup
    test -a is OB XSI

--- modules.d/95resume/resume.sh
+++ modules.d/95resume/resume.sh
@@ -2,7 +2,7 @@
 
 PATH=/usr/sbin:/usr/bin:/sbin:/bin
 
-[ -s /.resume -a -b "$resume" ] && {
+[ -s /.resume ] && [ -b "$resume" ] && {
     # First try user level resume; it offers splash etc
     case "$splash" in
         quiet)
@@ -14,5 +14,6 @@ PATH=/usr/sbin:/usr/bin:/sbin:/bin
     esac
     [ -x "$(command -v resume)" ] && command resume "$a_splash" "$resume"
 
-    (readlink -fn "$resume" > /sys/power/resume) > /.resume
+    readlink -fn "$resume" > /sys/power/resume
+    : > /.resume
 }
