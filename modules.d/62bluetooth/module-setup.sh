98735f9960419a9bb9a32deeeeea17d9cd119819
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 18:08:58 +0200

    refactor(62bluetooth): debashify
    shellcheck -s {,k,da}sh clean

--- modules.d/62bluetooth/module-setup.sh
+++ modules.d/62bluetooth/module-setup.sh
@@ -7,7 +7,7 @@ check() {
     # If the binary(s) requirements are not fulfilled the module can't be installed
     require_any_binary /usr/lib/bluetooth/bluetoothd /usr/libexec/bluetooth/bluetoothd || return 1
 
-    if [[ $hostonly ]]; then
+    if [ -n "$hostonly" ]; then
         # Include by default if a Peripheral (0x500) is found of minor class:
         #  * Keyboard (0x40)
         #  * Keyboard/pointing (0xC0)
@@ -64,7 +64,7 @@ install() {
         /usr/libexec/bluetooth/bluetoothd \
         /usr/lib/bluetooth/bluetoothd
 
-    if [[ $hostonly ]]; then
+    if [ -n "$hostonly" ]; then
         var_lib_files=("$dracutsysrootdir"/var/lib/bluetooth/**)
 
         inst_multiple -o \
