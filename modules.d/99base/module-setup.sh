e189d28b3c686c70826c6acf73a627de131477ed
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 22:56:13 +0200

    refactor(99base): clean up setup

--- modules.d/99base/module-setup.sh
+++ modules.d/99base/module-setup.sh
@@ -24,18 +24,18 @@ install() {
     ln -s dracut-util "${initdir}/usr/bin/dracut-getarg"
     ln -s dracut-util "${initdir}/usr/bin/dracut-getargs"
 
-    if [ ! -e "${initdir}/bin/sh" ]; then
+    if ! [ -e "${initdir}/bin/sh" ]; then
         inst_multiple bash
-        (ln -s bash "${initdir}/bin/sh" || :)
+        ln -s bash "${initdir}/bin/sh"
     fi
 
     # add common users in /etc/passwd, it will be used by nfs/ssh currently
     # use password for hostonly images to facilitate secure sulogin in emergency console
-    [[ $hostonly ]] && pwshadow='x'
+    [ -n "$hostonly" ] && pwshadow='x'
     grep '^root:' "$initdir/etc/passwd" 2> /dev/null || echo "root:$pwshadow:0:0::/root:/bin/sh" >> "$initdir/etc/passwd"
     grep '^nobody:' "$dracutsysrootdir"/etc/passwd >> "$initdir/etc/passwd"
 
-    [[ $hostonly ]] && grep '^root:' "$dracutsysrootdir"/etc/shadow >> "$initdir/etc/shadow"
+    [ -n "$hostonly" ] && grep '^root:' "$dracutsysrootdir"/etc/shadow >> "$initdir/etc/shadow"
 
     # install our scripts and hooks
     inst_script "$moddir/init.sh" "/init"
@@ -59,28 +59,25 @@ install() {
         inst_multiple -o "$systemdutildir"/systemd-timestamp
     fi
 
-    if [[ $realinitpath ]]; then
-        for i in $realinitpath; do
-            echo "rd.distroinit=$i"
-        done > "${initdir}/etc/cmdline.d/distroinit.conf"
-    fi
+    # shellcheck disable=SC2086
+    [ -n "$realinitpath" ] && printf "rd.distroinit=%s\n" $realinitpath > "${initdir}/etc/cmdline.d/distroinit.conf"
 
     ln -fs /proc/self/mounts "$initdir/etc/mtab"
-    if [[ $ro_mnt == yes ]]; then
-        echo ro >> "${initdir}/etc/cmdline.d/base.conf"
-    fi
+    [ "$ro_mnt" = yes ] && echo ro >> "${initdir}/etc/cmdline.d/base.conf"
 
     [ -e "${initdir}/usr/lib" ] || mkdir -m 0755 -p "${initdir}"/usr/lib
 
-    local VERSION=""
-    local PRETTY_NAME=""
+    local VERSION
+    local PRETTY_NAME
+    VERSION=""
+    PRETTY_NAME=""
     # Derive an os-release file from the host, if it exists
-    if [[ -e $dracutsysrootdir/etc/os-release ]]; then
+    if [ -e "$dracutsysrootdir"/etc/os-release ]; then
         # shellcheck disable=SC1090
         . "$dracutsysrootdir"/etc/os-release
-        grep -hE -ve '^VERSION=' -ve '^PRETTY_NAME' "$dracutsysrootdir"/etc/os-release > "${initdir}"/usr/lib/initrd-release
-        [[ -n ${VERSION} ]] && VERSION+=" "
-        [[ -n ${PRETTY_NAME} ]] && PRETTY_NAME+=" "
+        grep -hEv -e '^VERSION=' -e '^PRETTY_NAME' "$dracutsysrootdir"/etc/os-release > "${initdir}"/usr/lib/initrd-release
+        [ -n "${VERSION}" ] && VERSION="$VERSION "
+        [ -n "${PRETTY_NAME}" ] && PRETTY_NAME="$PRETTY_NAME "
     else
         # Fall back to synthesizing one, since dracut is presently used
         # on non-systemd systems as well.
@@ -91,8 +88,8 @@ install() {
             echo 'ANSI_COLOR="0;34"'
         } > "${initdir}"/usr/lib/initrd-release
     fi
-    VERSION+="dracut-$DRACUT_VERSION"
-    PRETTY_NAME+="dracut-$DRACUT_VERSION (Initramfs)"
+    VERSION="${VERSION}dracut-$DRACUT_VERSION"
+    PRETTY_NAME="${PRETTY_NAME}dracut-$DRACUT_VERSION (Initramfs)"
     {
         echo "VERSION=\"$VERSION\""
         echo "PRETTY_NAME=\"$PRETTY_NAME\""
@@ -107,11 +104,10 @@ install() {
     ln -sf initrd-release "$initdir"/etc/os-release
 
     ## save host_devs which we need bring up
-    if [[ $hostonly_cmdline == "yes" ]]; then
-        if [[ -n ${host_devs[*]} ]]; then
-            dracut_need_initqueue
-        fi
-        if [[ -f $initdir/lib/dracut/need-initqueue ]] || ! dracut_module_included "systemd"; then
+    if [ "$hostonly_cmdline" = "yes" ]; then
+        [ -n "${host_devs[*]}" ] && dracut_need_initqueue
+
+        if [ -f "$initdir"/lib/dracut/need-initqueue ] || ! dracut_module_included "systemd"; then
             (
                 if dracut_module_included "systemd"; then
                     export DRACUT_SYSTEMD=1
@@ -124,21 +120,20 @@ install() {
 
                 for _dev in "${host_devs[@]}"; do
                     for _dev2 in "${root_devs[@]}"; do
-                        [[ $_dev == "$_dev2" ]] && continue 2
+                        [ "$_dev" = "$_dev2" ] && continue 2
                     done
 
                     # We only actually wait for real devs - swap is only needed
                     # for resume and udev rules generated when parsing resume=
                     # argument take care of the waiting for us
                     for _dev2 in "${swap_devs[@]}"; do
-                        [[ $_dev == "$_dev2" ]] && continue 2
+                        [ "$_dev" = "$_dev2" ] && continue 2
                     done
 
                     _pdev=$(get_persistent_dev "$_dev")
 
                     case "$_pdev" in
                         /dev/?*) wait_for_dev "$_pdev" 0 ;;
-                        *) ;;
                     esac
                 done
             )
