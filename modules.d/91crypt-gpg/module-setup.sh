4a24aeec0136756893708a621fd2c9d538f25ef4
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 23:56:36 +0200

    refactor(91crypt-gpg): debashify, cleanup
    shellcheck -s {,k,da}sh clean

--- modules.d/91crypt-gpg/module-setup.sh
+++ modules.d/91crypt-gpg/module-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # GPG support is optional
 # called by dracut
@@ -30,12 +30,12 @@ install() {
         inst_multiple gpg-agent
         inst_multiple gpg-connect-agent
         inst_multiple -o /usr/libexec/scdaemon /usr/lib/gnupg/scdaemon
-        cp "$dracutsysrootdir$(sc_public_key)" "${initdir}/root/"
+        cp "$dracutsysrootdir/etc/dracut.conf.d/crypt-public-key.gpg" "${initdir}/root/"
     fi
 }
 
-sc_public_key() {
-    echo -n "/etc/dracut.conf.d/crypt-public-key.gpg"
+sc_requested() {
+    [ -f "$dracutsysrootdir/etc/dracut.conf.d/crypt-public-key.gpg" ]
 }
 
 # CCID Smartcard support requires GnuPG >= 2.1 with scdaemon and libusb
@@ -43,29 +43,21 @@ sc_supported() {
     local gpgMajor
     local gpgMinor
     local scdaemon
-    gpgMajor="$(gpg --version | sed -n 1p | sed -n -r -e 's|.* ([0-9]*).*|\1|p')"
-    gpgMinor="$(gpg --version | sed -n 1p | sed -n -r -e 's|.* [0-9]*\.([0-9]*).*|\1|p')"
+    gpgMajor="$(gpg --version | sed -nr '1s|.* ([0-9]*).*|\1|p')"
+    gpgMinor="$(gpg --version | sed -nr '1s|.* [0-9]*\.([0-9]*).*|\1|p')"
 
-    if [[ -x "$dracutsysrootdir"/usr/libexec/scdaemon ]]; then
+    if [ -x "$dracutsysrootdir"/usr/libexec/scdaemon ]; then
         scdaemon=/usr/libexec/scdaemon
-    elif [[ -x "$dracutsysrootdir"/usr/lib/gnupg/scdaemon ]]; then
+    elif [ -x "$dracutsysrootdir"/usr/lib/gnupg/scdaemon ]; then
         scdaemon=/usr/lib/gnupg/scdaemon
     else
         return 1
     fi
 
-    if [[ ${gpgMajor} -gt 2 || ${gpgMajor} -eq 2 && ${gpgMinor} -ge 1 ]] \
+    if { [ "${gpgMajor}" -gt 2 ] || { [ "${gpgMajor}" -eq 2 ] && [ "${gpgMinor}" -ge 1 ]; }; } \
         && require_binaries gpg-agent \
         && require_binaries gpg-connect-agent \
-        && ($DRACUT_LDD "${dracutsysrootdir}${scdaemon}" | grep libusb > /dev/null); then
-        return 0
-    else
-        return 1
-    fi
-}
-
-sc_requested() {
-    if [ -f "$dracutsysrootdir$(sc_public_key)" ]; then
+        && $DRACUT_LDD "${dracutsysrootdir}${scdaemon}" | grep -q libusb; then
         return 0
     else
         return 1
