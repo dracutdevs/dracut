8bc9a4509f323bf07f3e60dbb3b95fe1e00d01e3
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 21:55:53 +0200

    refactor(95lunmask): debashify, fix
    shellcheck -s {,k,da}sh clean; SC2168: local is only valid in functions

--- modules.d/95lunmask/module-setup.sh
+++ modules.d/95lunmask/module-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # -*- mode: shell-script; indent-tabs-mode: nil; sh-basic-offset: 4; -*-
 # ex: ts=8 sw=4 sts=4 et filetype=sh
 
@@ -7,12 +7,9 @@ cmdline() {
     get_lunmask() {
         local _dev=$1
         local _devpath _sdev _lun _rport _end_device _classdev _wwpn _sas_address
-        _devpath=$(
-            cd -P /sys/dev/block/"$_dev" || exit
-            echo "$PWD"
-        )
+        _devpath=$(cd -P /sys/dev/block/"$_dev" && echo "$PWD")
 
-        [ "${_devpath#*/sd}" == "$_devpath" ] && return 1
+        [ "${_devpath#*/sd}" = "$_devpath" ] && return 1
         _sdev="${_devpath%%/block/*}"
         _lun="${_sdev##*:}"
         # Check for FibreChannel
@@ -21,7 +18,7 @@ cmdline() {
             _rport="${_rport%%/*}"
             _classdev="/sys/class/fc_remote_ports/rport-${_rport}"
             [ -d "$_classdev" ] || return 1
-            _wwpn=$(cat "${_classdev}"/port_name)
+            read -r _wwpn < "${_classdev}"/port_name
             echo "rd.lunmask=fc,${_wwpn},${_lun}"
             return 0
         fi
@@ -37,14 +34,14 @@ cmdline() {
         fi
         return 1
     }
-    [[ $hostonly ]] || [[ $mount_needs ]] && {
+    [ -n "$hostonly" ] || [ -n "$mount_needs" ] && {
         for_each_host_dev_and_slaves_all get_lunmask
     } | sort | uniq
 }
 
 # called by dracut
 check() {
-    [[ $hostonly ]] || [[ $mount_needs ]] && {
+    [ -n "$hostonly" ] || [ -n "$mount_needs" ] && {
         [ -w /sys/module/scsi_mod/parameters/scan ] || return 255
         scan_type=$(cat /sys/module/scsi_mod/parameters/scan)
         [ "$scan_type" = "manual" ] && return 0
@@ -63,7 +60,7 @@ install() {
     inst_script "$moddir/fc_transport_scan_lun.sh" /usr/lib/udev/fc_transport_scan_lun.sh
     inst_script "$moddir/sas_transport_scan_lun.sh" /usr/lib/udev/sas_transport_scan_lun.sh
     inst_hook cmdline 30 "$moddir/parse-lunmask.sh"
-    if [[ $hostonly_cmdline == "yes" ]]; then
+    if [ "$hostonly_cmdline" = "yes" ]; then
         local _lunmask
 
         for _lunmask in $(cmdline); do
--- modules.d/95lunmask/parse-lunmask.sh
+++ modules.d/95lunmask/parse-lunmask.sh
@@ -10,31 +10,21 @@ create_udev_rule() {
 
     [ -e "${_rule}" ] && return 0
 
-    if [ ! -f "$_rule" ]; then
+    if ! [ -f "$_rule" ]; then
         if [ "$transport" = "fc" ]; then
-            cat > "$_rule" << EOF
-ACTION=="add", SUBSYSTEM=="fc_remote_ports", ATTR{port_name}=="$tgtid", PROGRAM="fc_transport_scan_lun.sh $lun"
-EOF
+            printf 'ACTION=="add", SUBSYSTEM=="fc_remote_ports", ATTR{port_name}=="%s", PROGRAM="fc_transport_scan_lun.sh %s"\n' "$tgtid" "$lun" > "$_rule"
         elif [ "$transport" = "sas" ]; then
-            cat > "$_rule" << EOF
-ACTION=="add", SUBSYSTEM=="sas_device", ATTR{sas_address}=="$tgtid", PROGRAM="sas_transport_scan_lun.sh $lun"
-EOF
+            printf 'ACTION=="add", SUBSYSTEM=="sas_device", ATTR{sas_address}=="%s", PROGRAM="sas_transport_scan_lun.sh %s"\n' "$tgtid" "$lun" > "$_rule"
         fi
     fi
 }
 
 for lunmask_arg in $(getargs rd.lunmask); do
-    (
-        local OLDIFS="$IFS"
-        local IFS=","
-        # shellcheck disable=SC2086
-        set $lunmask_arg
-        IFS="$OLDIFS"
-        if [ -d /sys/module/scsi_mod ]; then
-            printf "manual" > /sys/module/scsi_mod/parameters/scan
-        elif [ ! -f /etc/modprobe.d/95lunmask.conf ]; then
-            echo "options scsi_mod scan=manual" > /etc/modprobe.d/95lunmask.conf
-        fi
-        create_udev_rule "$1" "$2" "$3"
-    )
+    if [ -d /sys/module/scsi_mod ]; then
+        printf "manual" > /sys/module/scsi_mod/parameters/scan
+    elif ! [ -f /etc/modprobe.d/95lunmask.conf ]; then
+        echo "options scsi_mod scan=manual" > /etc/modprobe.d/95lunmask.conf
+    fi
+    IFS=","
+    create_udev_rule "$lunmask_arg"
 done
