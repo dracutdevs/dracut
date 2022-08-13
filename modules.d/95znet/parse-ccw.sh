2f77507e93467c4afac1475cf4b41b05b6aca455
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-14 00:07:04 +0200

    refactor(95znet): debashify
    shellcheck -s {,k,da}sh clean

--- modules.d/95znet/parse-ccw.sh
+++ modules.d/95znet/parse-ccw.sh
@@ -1,23 +1,19 @@
-#!/bin/bash
+#!/bin/sh
 
 for ccw_arg in $(getargs rd.ccw -d 'rd_CCW=') $(getargs rd.znet -d 'rd_ZNET='); do
     echo "$ccw_arg" >> /etc/ccw.conf
 done
 
-for ifname in $(getargs rd.znet_ifname); do
-    IFS=: read -r ifname_if ifname_subchannels _rest <<< "$ifname"
+getargs rd.znet_ifname | while IFS=: read -r ifname_if ifname_subchannels _rest; do
     if [ -z "$ifname_if" ] || [ -z "$ifname_subchannels" ] || [ -n "$_rest" ]; then
         warn "Invalid arguments for rd.znet_ifname="
     else
         {
-            ifname_subchannels=${ifname_subchannels//,/|}
-
             echo 'ACTION!="add|change", GOTO="ccw_ifname_end"'
             echo 'ATTR{type}!="1", GOTO="ccw_ifname_end"'
             echo 'SUBSYSTEM!="net", GOTO="ccw_ifname_end"'
-            echo "SUBSYSTEMS==\"ccwgroup\", KERNELS==\"$ifname_subchannels\", DRIVERS==\"?*\" NAME=\"$ifname_if\""
+            echo "SUBSYSTEMS==\"ccwgroup\", KERNELS==\"$(echo "$ifname_subchannels" | tr , \|)\", DRIVERS==\"?*\" NAME=\"$ifname_if\""
             echo 'LABEL="ccw_ifname_end"'
-
         } > /etc/udev/rules.d/81-ccw-ifname.rules
     fi
 done
