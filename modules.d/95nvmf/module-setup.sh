995fcc1c145b727f5a254b8801e930b4f22185af
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-14 00:03:33 +0200

    refactor(95nvmf): debashify
    shellcheck -s {,k,da}sh clean

--- modules.d/95nvmf/module-setup.sh
+++ modules.d/95nvmf/module-setup.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 # called by dracut
 check() {
@@ -10,7 +10,7 @@ check() {
         local _dev=$1
         local trtype
 
-        [[ -L "/sys/dev/block/$_dev" ]] || return 0
+        [ -L "/sys/dev/block/$_dev" ] || return 0
         cd -P "/sys/dev/block/$_dev" || return 0
         if [ -f partition ]; then
             cd ..
@@ -18,19 +18,19 @@ check() {
         for d in device/nvme*; do
             [ -L "$d" ] || continue
             if readlink "$d" | grep -q nvme-fabrics; then
-                trtype=$(cat "$d"/transport)
+                read -r trtype < "$d"/transport
                 break
             fi
         done
-        [[ $trtype == "fc" ]] || [[ $trtype == "tcp" ]] || [[ $trtype == "rdma" ]]
+        [ "$trtype" = "fc" ] || [ "$trtype" = "tcp" ] || [ "$trtype" = "rdma" ]
     }
 
-    [[ $hostonly ]] || [[ $mount_needs ]] && {
-        pushd . > /dev/null
+    [ -n "$hostonly" ] || [ -n "$mount_needs" ] && {
+        cd .
         for_each_host_dev_and_slaves is_nvmf
         local _is_nvmf=$?
-        popd > /dev/null || exit
-        [[ $_is_nvmf == 0 ]] || return 255
+        cd - || exit
+        [ "$_is_nvmf" -eq 0 ] || return 255
         if [ ! -f /sys/class/fc/fc_udev_device/nvme_discovery ]; then
             if [ ! -f /etc/nvme/discovery.conf ]; then
                 echo "No discovery arguments present"
@@ -43,7 +43,7 @@ check() {
 
 # called by dracut
 depends() {
-    echo bash rootfs-block network
+    echo rootfs-block network
     return 0
 }
 
@@ -62,7 +62,7 @@ cmdline() {
         local _dev=$1
         local trtype
 
-        [[ -L "/sys/dev/block/$_dev" ]] || return 0
+        [ -L "/sys/dev/block/$_dev" ] || return 0
         cd -P "/sys/dev/block/$_dev" || return 0
         if [ -f partition ]; then
             cd ..
@@ -70,7 +70,7 @@ cmdline() {
         for d in device/nvme*; do
             [ -L "$d" ] || continue
             if readlink "$d" | grep -q nvme-fabrics; then
-                trtype=$(cat "$d"/transport)
+                read -r trtype < "$d"/transport
                 break
             fi
         done
@@ -78,32 +78,32 @@ cmdline() {
         [ -z "$trtype" ] && return 0
         nvme list-subsys "${PWD##*/}" | while read -r _ _ trtype traddr host_traddr _; do
             [ "$trtype" != "${trtype#NQN}" ] && continue
-            echo -n " rd.nvmf.discover=$trtype,${traddr#traddr=},${host_traddr#host_traddr=}"
+            printf " rd.nvmf.discover=%s" "$trtype,${traddr#traddr=},${host_traddr#host_traddr=}"
         done
     }
 
     if [ -f /etc/nvme/hostnqn ]; then
-        _hostnqn=$(cat /etc/nvme/hostnqn)
-        echo -n " rd.nvmf.hostnqn=${_hostnqn}"
+        read -r _hostnqn < /etc/nvme/hostnqn
+        printf " rd.nvmf.hostnqn=%s" "${_hostnqn}"
     fi
     if [ -f /etc/nvme/hostid ]; then
-        _hostid=$(cat /etc/nvme/hostid)
-        echo -n " rd.nvmf.hostid=${_hostid}"
+        read -r _hostid < /etc/nvme/hostid
+        printf " rd.nvmf.hostid=%s" "${_hostid}"
     fi
 
-    [[ $hostonly ]] || [[ $mount_needs ]] && {
-        pushd . > /dev/null
+    [ -n "$hostonly" ] || [ -n "$mount_needs" ] && {
+        cd .
         for_each_host_dev_and_slaves gen_nvmf_cmdline
-        popd > /dev/null || exit
+        cd - || exit
     }
 }
 
 # called by dracut
 install() {
-    if [[ $hostonly_cmdline == "yes" ]]; then
+    if [ "$hostonly_cmdline" = "yes" ]; then
         local _nvmf_args
         _nvmf_args=$(cmdline)
-        [[ "$_nvmf_args" ]] && printf "%s" "$_nvmf_args" >> "${initdir}/etc/cmdline.d/95nvmf-args.conf"
+        [ -n "$_nvmf_args" ] && printf "%s" "$_nvmf_args" >> "${initdir}/etc/cmdline.d/95nvmf-args.conf"
     fi
     inst_simple "/etc/nvme/hostnqn"
     inst_simple "/etc/nvme/hostid"
