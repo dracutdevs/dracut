59d14635045d707c5403237d6e9b3420686ef878
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 18:06:46 +0200

    refactor(45url-lib): debashify, simplify
    shellcheck -s {,k,da}sh clean

--- modules.d/45url-lib/module-setup.sh
+++ modules.d/45url-lib/module-setup.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
-# module-setup for url-lib
+#!/bin/sh
 
 # called by dracut
 check() {
@@ -15,11 +14,11 @@ depends() {
 
 # called by dracut
 install() {
-    local _dir _crt _crts _found _lib _nssckbi _p11roots _p11root
+    local _dir _crt _crts _found _lib _nssckbi
     inst_simple "$moddir/url-lib.sh" "/lib/url-lib.sh"
     inst_multiple -o ctorrent
     inst_multiple curl sed
-    if curl --version | grep -qi '\bNSS\b'; then
+    if curl --version | grep -qwiF 'NSS'; then
         # also install libs for curl https
         inst_libdir_file "libnsspem.so*"
         inst_libdir_file "libnsssysinit.so*"
@@ -28,27 +27,21 @@ install() {
     fi
 
     for _dir in $libdirs; do
-        [[ -d $dracutsysrootdir$_dir ]] || continue
+        [ -d "$dracutsysrootdir$_dir" ] || continue
         for _lib in "$dracutsysrootdir$_dir"/libcurl.so.* "$dracutsysrootdir$_dir"/libcrypto.so.*; do
-            [[ -e $_lib ]] || continue
-            if ! [[ $_nssckbi ]]; then
-                read -r -d '' _nssckbi < <(grep -F --binary-files=text -z libnssckbi "$_lib")
-            fi
-            read -r -d '' _crt < <(grep -E --binary-files=text -z "\.(pem|crt)" "$_lib" | sed 's/\x0//g')
-            [[ $_crt ]] || continue
-            [[ $_crt == /*/* ]] || continue
-            if [[ -e $_crt ]]; then
+            [ -e "$_lib" ] || continue
+            [ -z "$_nssckbi" ] && grep -Fqaz libnssckbi "$_lib" && _nssckbi=1
+            _crt="$(grep -Eazm1 "\.(pem|crt)" "$_lib" | tr -d '\0')"
+            [ "${_crt#/*/}" != "$_crt" ] || continue
+            if [ -e "$_crt" ]; then
                 _crts="$_crts $_crt"
                 _found=1
             fi
         done
     done
-    if [[ $_found ]] && [[ -n $_crts ]]; then
+    if [ -n "$_found" ]; then
         for _crt in $_crts; do
-            if ! inst "${_crt#$dracutsysrootdir}"; then
-                dwarn "Couldn't install '$_crt' SSL CA cert bundle; HTTPS might not work."
-                continue
-            fi
+            inst "${_crt#"$dracutsysrootdir"}" || dwarn "Couldn't install '$_crt' SSL CA cert bundle; HTTPS might not work."
         done
     fi
     # If we found no cert bundle files referenced in libcurl but we
@@ -56,33 +49,32 @@ install() {
     # If its truly NSS libnssckbi, it includes its own trust bundle,
     # but if it's really p11-kit-trust.so, we need to find the dirs
     # where it will look for a trust bundle and install them too.
-    if ! [[ $_found ]] && [[ $_nssckbi ]]; then
-        _found=1
-        inst_libdir_file "libnssckbi.so*" || _found=
+    if [ -z "$_found" ] && [ -n "$_nssckbi" ]; then
+        inst_libdir_file "libnssckbi.so*" && _found=1 || _found=
         for _dir in $libdirs; do
-            [[ -e $dracutsysrootdir$_dir/libnssckbi.so ]] || continue
+            [ -e "$dracutsysrootdir$_dir"/libnssckbi.so ] || continue
             # this looks for directory-ish strings in the file
-            grep -z -o --binary-files=text '/[[:alpha:]][[:print:]]*' "${dracutsysrootdir}${_dir}"/libnssckbi.so \
-                | while read -r -d '' _p11roots || [[ $_p11roots ]]; do
-                    IFS=":" read -r -a _p11roots <<< "$_p11roots"
-                    # the string can be a :-separated list of dirs
-                    for _p11root in "${_p11roots[@]}"; do
-                        # check if it's actually a directory (there are
-                        # several false positives in the results)
-                        [[ -d "$dracutsysrootdir$_p11root" ]] || continue
-                        # check if it has some specific subdirs that all
-                        # p11-kit trust dirs have
-                        [[ -d "$dracutsysrootdir${_p11root}/anchors" ]] || continue
-                        [[ -d "$dracutsysrootdir${_p11root}/blacklist" ]] || continue
-                        # so now we know it's really a p11-kit trust dir;
-                        # install everything in it
-                        mkdir -p -- "${initdir}/${_p11root}"
-                        if ! $DRACUT_CP -L -t "${initdir}/${_p11root}" "${dracutsysrootdir}${_p11root}"/*; then
-                            dwarn "Couldn't install from p11-kit trust dir '${_p11root#$dracutsysrootdir}'; HTTPS might not work."
-                        fi
-                    done
+            grep -zoa '/[[:alpha:]][[:print:]]*' "$dracutsysrootdir$_dir"/libnssckbi.so \
+                | tr '\0' '\n' | while read -r _p11roots || [ -n "$_p11roots" ]; do
+                IFS=:
+                # the string can be a :-separated list of dirs
+                for _p11root in $_p11roots; do
+                    # check if it's actually a directory (there are
+                    # several false positives in the results)
+                    [ -d "$dracutsysrootdir$_p11root" ] || continue
+                    # check if it has some specific subdirs that all
+                    # p11-kit trust dirs have
+                    [ -d "$dracutsysrootdir$_p11root/anchors" ] || continue
+                    [ -d "$dracutsysrootdir$_p11root/blacklist" ] || continue
+                    # so now we know it's really a p11-kit trust dir;
+                    # install everything in it
+                    mkdir -p -- "${initdir}/${_p11root}"
+                    if ! $DRACUT_CP -L -t "${initdir}/${_p11root}" "$dracutsysrootdir$_p11root"/*; then
+                        dwarn "Couldn't install from p11-kit trust dir '${_p11root#"$dracutsysrootdir"}'; HTTPS might not work."
+                    fi
                 done
+            done
         done
     fi
-    [[ $_found ]] || dwarn "Couldn't find SSL CA cert bundle or libnssckbi.so; HTTPS won't work."
+    [ -n "$_found" ] || dwarn "Couldn't find SSL CA cert bundle or libnssckbi.so; HTTPS won't work."
 }
