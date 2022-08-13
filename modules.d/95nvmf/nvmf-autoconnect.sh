139d38bf561d167c65f7f9fca534d9bcebc7687b
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-14 00:03:33 +0200

    refactor(95nvmf): debashify
    shellcheck -s {,k,da}sh clean

--- modules.d/95nvmf/nvmf-autoconnect.sh
+++ modules.d/95nvmf/nvmf-autoconnect.sh
@@ -1,5 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
-[ -f /sys/class/fc/fc_udev_device/nvme_discovery ] || exit 1
+[ -f /sys/class/fc/fc_udev_device/nvme_discovery ] || exit
 echo add > /sys/class/fc/fc_udev_device/nvme_discovery
-exit 0
