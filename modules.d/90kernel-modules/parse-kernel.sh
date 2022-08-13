6fc4a96d17b3e87e428fecac0b84ebb24f1fffdf
наб <nabijaczleweli@nabijaczleweli.xyz>
2022-08-13 19:43:16 +0200

    refactor(90kernel-modules): mostly debashify, simplify
    For my modules.dep, the shell filter was 140ms, grep takes 2
    
    shellcheck -s {,k,da}sh clean, except for the host filesystem array

--- modules.d/90kernel-modules/parse-kernel.sh
+++ modules.d/90kernel-modules/parse-kernel.sh
@@ -5,32 +5,27 @@ if [ -d /usr/lib/modprobe.d ]; then
     _modprobe_d=/usr/lib/modprobe.d
 elif [ -d /lib/modprobe.d ]; then
     _modprobe_d=/lib/modprobe.d
-elif [ ! -d $_modprobe_d ]; then
-    mkdir -p $_modprobe_d
+elif ! [ -d "$_modprobe_d" ]; then
+    mkdir -p "$_modprobe_d"
 fi
 
-for i in $(getargs rd.driver.pre -d rdloaddriver=); do
-    (
-        IFS=,
-        for p in $i; do
-            modprobe "$p" 2>&1 | vinfo
-        done
-    )
-done
+IFS="$IFS,"
+# shellcheck disable=SC2046
+modprobe -a $(getargs rd.driver.pre -d rdloaddriver=) 2>&1 | vinfo
+IFS="${IFS%,}"
 
 [ -d /etc/modprobe.d ] || mkdir -p /etc/modprobe.d
 
 for i in $(getargs rd.driver.blacklist -d rdblacklist=); do
     (
         IFS=,
-        for p in $i; do
-            echo "blacklist $p" >> $_modprobe_d/initramfsblacklist.conf
-        done
+        # shellcheck disable=SC2086
+        printf "blacklist %s\n" $i >> "$_modprobe_d"/initramfsblacklist.conf
     )
 done
 
 for p in $(getargs rd.driver.post -d rdinsmodpost=); do
-    echo "blacklist $p" >> $_modprobe_d/initramfsblacklist.conf
+    echo "blacklist $p" >> "$_modprobe_d"/initramfsblacklist.conf
     _do_insmodpost=1
 done
 
