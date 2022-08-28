d28cfca1befe52dde5e35b7acf0e4be952c7af39
Laszlo Gombos <laszlo.gombos@gmail.com>
2022-08-28 13:35:36 +0000

    chore(base): do not output root entry while installing base
    Silence the printing out root entry of passwd file
    while including base module.
    
    dracut: *** Including module: base ***
    root:x:0:0:root:/root:/bin/bash
    dracut: *** Including module: fs-lib ***

--- modules.d/99base/module-setup.sh
+++ modules.d/99base/module-setup.sh
@@ -32,7 +32,7 @@ install() {
     # add common users in /etc/passwd, it will be used by nfs/ssh currently
     # use password for hostonly images to facilitate secure sulogin in emergency console
     [[ $hostonly ]] && pwshadow='x'
-    grep '^root:' "$initdir/etc/passwd" 2> /dev/null || echo "root:$pwshadow:0:0::/root:/bin/sh" >> "$initdir/etc/passwd"
+    grep '^root:' "$initdir/etc/passwd" > /dev/null 2>&1 || echo "root:$pwshadow:0:0::/root:/bin/sh" >> "$initdir/etc/passwd"
     grep '^nobody:' "$dracutsysrootdir"/etc/passwd >> "$initdir/etc/passwd"
 
     [[ $hostonly ]] && grep '^root:' "$dracutsysrootdir"/etc/shadow >> "$initdir/etc/shadow"
