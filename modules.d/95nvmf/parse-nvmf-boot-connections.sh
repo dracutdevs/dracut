26fa360354bfec20ea1ea807c18d4d1b3cf72528
Martin Wilck <mwilck@suse.com>
2022-09-17 00:51:46 +0200

    fix(nvmf): make sure "rd.nvmf.discover=fc,auto" takes precedence
    The command line may contain several rd.nvmf.discover options.
    The "fc,auto" option should take precedence.

--- modules.d/95nvmf/parse-nvmf-boot-connections.sh
+++ modules.d/95nvmf/parse-nvmf-boot-connections.sh
@@ -92,19 +92,21 @@ if [ -n "$nvmf_hostid" ]; then
     echo "$nvmf_hostid" > /etc/nvme/hostid
 fi
 
+NVMF_FC_AUTO=
 for d in $(getargs rd.nvmf.discover -d nvmf.discover=); do
-    parse_nvmf_discover "$d" || break
+    parse_nvmf_discover "$d" || {
+        NVMF_FC_AUTO=1
+        break
+    }
 done
 
 # Host NQN and host id are mandatory for NVMe-oF
 [ -f "/etc/nvme/hostnqn" ] || exit 0
 [ -f "/etc/nvme/hostid" ] || exit 0
 
-if [ -f "/etc/nvme/discovery.conf" ]; then
-    /sbin/initqueue --settled --onetime --unique --name nvme-discover /usr/sbin/nvme connect-all
+# If no nvme command line arguments present, try autodiscovery
+if [ $NVMF_FC_AUTO ] || [ ! -f "/etc/nvme/discovery.conf" ]; then
+    /sbin/initqueue --settled --onetime --unique --name nvme-fc-autoconnect /sbin/nvmf-autoconnect.sh
 else
-    # No nvme command line arguments present, try autodiscovery
-    if [ "$trtype" = "fc" ]; then
-        /sbin/initqueue --settled --onetime --unique --name nvme-fc-autoconnect /sbin/nvmf-autoconnect.sh
-    fi
+    /sbin/initqueue --settled --onetime --unique --name nvme-discover /usr/sbin/nvme connect-all
 fi
