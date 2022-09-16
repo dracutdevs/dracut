f58e1d54e95a2b8878720ab8fb118bc1b886c60f
Martin Wilck <mwilck@suse.com>
2022-09-16 21:36:52 +0200

    feat(nvmf): add code for parsing the NBFT
    Add code to parse the Nvme-oF Boot Firmware Table (NBFT) according
    to the NVM Express Boot Specification 1.0 [1]. The implementation in
    dracut follows a similar general approach as iBFT support in the
    iscsi module.
    
    NBFT support requires two steps:
    
    (1) Setting up the network and routing according to the
        HFI ("Host Fabric Interface") records in the NBFT,
    (2) Establishing the actual NVMe-oF connection.
    
    (1) is accomplished by reading the NBFT using JSON output from
    the "nvme nbft show" command, and transforming it into command
    line options ("ip=", "rd.neednet", etc.) understood by dracut's
    network module and its backends. The resulting network setup code
    is backend-agnostic. It has been tested with the "network-legacy"
    and "network-manager" network backend modules. The network setup
    code supports IPv4 and IPv6 with static, RA, or DHCP configurations,
    802.1q VLANs, and simple routing / gateway setup.
    
    (2) is done using the "nvme connect-all" command [2] in the netroot handler,
    which is invoked by networking backends when an interface gets fully
    configured. This patch adds support for "netboot=nbft". The "nbftroot"
    handler calls nvmf-autoconnect.sh, which contains the actual connect
    logic. nvmf-autoconnect.sh itself is preserved, because there are
    other NVMe-oF setups like NVMe over FC which don't depend on the
    network.
    
    The various ways to configure NVMe-oF are prioritized like this:
    
     1 FC autoconnect from kernel commandline (rd.nvmf.discover=fc,auto)
     2 NBFT, if present
     3 discovery.conf or config.json, if present, and cmdline.d parameters,
       if present (rd.nvmf.discovery=...)
     4 FC autoconnect (without kernel command line)
    
    The reason for this priorization is that in the initial RAM fs, we try
    to activate only those connections that are necessary to mount the root
    file system. This avoids confusion, possible contradicting or ambiguous
    configuration, and timeouts from unavailable targets.
    
    A retry logic is implemented for enabling the NVMe-oF connections,
    using the "settled" initqueue, the netroot handler, and eventually, the
    "timeout" initqueue. This is similar to the retry logic of the iscsi module.
    In the "timeout" case, connection to all possible NVMe-oF subsystems
    is attempted.
    
    Two new command line parameters are introduced to make it possible to
    change the priorities above:
    
     - "rd.nvmf.nonbft" causes the NBFT to be ignored,
     - "rd.nvmf.nostatic" causes any statically configured NVMe-oF targets
       (config.json, discovery.conf, and cmdline.d) to be ignored.
    
    These parameters may be helpful to skip attempts to set up broken
    configurations.
    
    At initramfs build time, the nvmf module is now enabled if an NBFT
    table is detected in the system.
    
    [1] https://nvmexpress.org/wp-content/uploads/NVM-Express-Boot-Specification-2022.11.15-Ratified.pdf
    [2] NBFT support in nvme-cli requires the latest upstream code (> v2.4).
    
    Signed-off-by: Martin Wilck <mwilck@suse.com>
    Co-authored-by: John Meneghini <jmeneghi@redhat.com>
    Co-authored-by: Charles Rose <charles.rose@dell.com>

--- modules.d/95nvmf/module-setup.sh
+++ modules.d/95nvmf/module-setup.sh
@@ -2,7 +2,7 @@
 
 # called by dracut
 check() {
-    require_binaries nvme || return 1
+    require_binaries nvme jq || return 1
     [ -f /etc/nvme/hostnqn ] || return 255
     [ -f /etc/nvme/hostid ] || return 255
 
@@ -25,6 +25,16 @@ check() {
         [[ $trtype == "fc" ]] || [[ $trtype == "tcp" ]] || [[ $trtype == "rdma" ]]
     }
 
+    has_nbft() {
+        local f found=
+        for f in /sys/firmware/acpi/tables/NBFT*; do
+            [ -f "$f" ] || continue
+            found=1
+            break
+        done
+        [[ $found ]]
+    }
+
     [[ $hostonly ]] || [[ $mount_needs ]] && {
         pushd . > /dev/null
         for_each_host_dev_and_slaves is_nvmf
@@ -32,7 +42,7 @@ check() {
         popd > /dev/null || exit
         [[ $_is_nvmf == 0 ]] || return 255
         if [ ! -f /sys/class/fc/fc_udev_device/nvme_discovery ]; then
-            if [ ! -f /etc/nvme/discovery.conf ]; then
+            if [ ! -f /etc/nvme/discovery.conf ] && ! has_nbft; then
                 echo "No discovery arguments present"
                 return 255
             fi
@@ -126,8 +136,9 @@ install() {
     inst_multiple ip sed
 
     inst_script "${moddir}/nvmf-autoconnect.sh" /sbin/nvmf-autoconnect.sh
+    inst_script "${moddir}/nbftroot.sh" /sbin/nbftroot
 
-    inst_multiple nvme
+    inst_multiple nvme jq
     inst_hook cmdline 92 "$moddir/parse-nvmf-boot-connections.sh"
     inst_simple "/etc/nvme/discovery.conf"
     inst_simple "/etc/nvme/config.json"
