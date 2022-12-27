68d0653e35f79e78b75a71c122c091ba4f4d5759
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2022-12-27 09:29:44 +0100

    feat(fips): add progress messages
    This allows to better understand at what point a FIPS integrity test failed.
    
    Signed-off-by: Raymund Will <rw@suse.com>

--- modules.d/01fips/fips-boot.sh
+++ modules.d/01fips/fips-boot.sh
@@ -8,7 +8,9 @@ elif [ -z "$fipsmode" ]; then
     die "FIPS mode have to be enabled by 'fips=1' not just 'fips'"
 elif getarg boot= > /dev/null; then
     . /sbin/fips.sh
+    fips_info "fips-boot: start"
     if mount_boot; then
         do_fips || die "FIPS integrity test failed"
     fi
+    fips_info "fips-boot: done!"
 fi
--- modules.d/01fips/fips-load-crypto.sh
+++ modules.d/01fips/fips-load-crypto.sh
@@ -8,5 +8,7 @@ elif [ -z "$fipsmode" ]; then
     die "FIPS mode have to be enabled by 'fips=1' not just 'fips'"
 else
     . /sbin/fips.sh
+    fips_info "fips-load-crypto: start"
     fips_load_crypto || die "FIPS integrity test failed"
+    fips_info "fips-load-crypto: done!"
 fi
