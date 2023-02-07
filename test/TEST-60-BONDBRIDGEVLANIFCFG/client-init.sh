54c2604dc01710bd7f68124e267d7722ce0138fd
Beniamino Galvani <bgalvani@redhat.com>
2023-02-07 15:28:57 +0100

    fix(test): improve test 60
    The test has the following issues:
    
     - it sets up a bridge on net1 and net2, however those interfaces
       are part of the same qemu hub and this causes switching loops;
    
     - the bonds are created on two interfaces but only one has a DHCP server, and
       this causes random failures;
    
     - the checks performed on the client are pretty limited.
    
    Rework the test:
    
     - avoid the bridging loop;
    
     - update the topology to use a bond interface on the server side;
    
     - introduce additional checks on the client: start a ping and check that ports
       are properly attached to bonds and bridge;
    
     - make it possible to return module-specific information from the client; in
       the future this can be added for example to check that the NetworkManager or
       systemd-networkd configuration is correct.

--- test/TEST-60-BONDBRIDGEVLANIFCFG/client-init.sh
+++ test/TEST-60-BONDBRIDGEVLANIFCFG/client-init.sh
@@ -8,13 +8,55 @@ export PS1='initramfs-test:\w\$ '
 stty sane
 echo "made it to the rootfs! Powering down."
 
+testnum=$(grep -Eo "rd.dracut.test.num=[^[:space:]]+" /proc/cmdline | sed -nr 's/.*=(.*)/\1/p')
+netmodule=$(grep -Eo "rd.dracut.test.net-module=[^[:space:]]+" /proc/cmdline | sed -nr 's/.*=(.*)/\1/p')
+
 (
     echo OK
+
     ip -o -4 address show scope global | while read -r _ if rest; do echo "$if"; done | sort
-    for i in /run/initramfs/state/etc/sysconfig/network-scripts/ifcfg-*; do
-        echo "$i"
-        grep -v 'UUID=' "$i"
-    done
+
+    case "$testnum" in
+        1)
+            ping -c 2 192.168.50.1 > /dev/null
+            echo PING1=$?
+            ping -c 2 192.168.54.1 > /dev/null
+            echo PING2=$?
+            ping -c 2 192.168.55.1 > /dev/null
+            echo PING3=$?
+            ping -c 2 192.168.56.1 > /dev/null
+            echo PING4=$?
+            ping -c 2 192.168.57.1 > /dev/null
+            echo PING5=$?
+            ;;
+        2)
+            ping -c 2 192.168.51.1 > /dev/null
+            echo PING1=$?
+            ip link show net3 | grep "master bond0" > /dev/null
+            echo NET3=$?
+            ip link show net4 | grep "master bond0" > /dev/null
+            echo NET4=$?
+            ;;
+        3)
+            ping -c 2 192.168.51.1 > /dev/null
+            echo PING1=$?
+            ip link show net1 | grep "master br0" > /dev/null
+            echo NET1=$?
+            ip link show net5 | grep "master br0" > /dev/null
+            echo NET5=$?
+
+            ;;
+    esac
+
+    case "$netmodule" in
+        network-legacy)
+            for i in /run/initramfs/state/etc/sysconfig/network-scripts/ifcfg-*; do
+                basename "$i"
+                grep -v 'UUID=' "$i"
+            done
+            ;;
+    esac
+
     echo EOF
 ) | dd oflag=direct,dsync of=/dev/sda
 
