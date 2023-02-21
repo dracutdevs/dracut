3eeff0e30d17ad71ea00403add6a971e46f95560
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-02-21 06:51:51 +0000

    fix(crypt): move devnames function from base module to crypt

--- modules.d/90crypt/crypt-lib.sh
+++ modules.d/90crypt/crypt-lib.sh
@@ -2,6 +2,46 @@
 
 command -v getarg > /dev/null || . /lib/dracut-lib.sh
 
+# Get kernel name for given device.  Device may be the name too (then the same
+# is returned), a symlink (full path), UUID (prefixed with "UUID=") or label
+# (prefixed with "LABEL=").  If just a beginning of the UUID is specified or
+# even an empty, function prints all device names which UUIDs match - every in
+# single line.
+#
+# NOTICE: The name starts with "/dev/".
+#
+# Example:
+#   devnames UUID=123
+# May print:
+#   /dev/dm-1
+#   /dev/sdb1
+#   /dev/sdf3
+devnames() {
+    local dev="$1"
+    local d
+    local names
+
+    case "$dev" in
+        UUID=*)
+            # shellcheck disable=SC2016
+            dev="$(foreach_uuid_until '! blkid -U $___' "${dev#UUID=}")" \
+                && return 255
+            [ -z "$dev" ] && return 255
+            ;;
+        LABEL=*) dev="$(blkid -L "${dev#LABEL=}")" || return 255 ;;
+        /dev/?*) ;;
+        *) return 255 ;;
+    esac
+
+    for d in $dev; do
+        names="$names
+$(readlink -e -q "$d")" || return 255
+    done
+
+    echo "${names#
+}"
+}
+
 # check if the crypttab contains an entry for a LUKS UUID
 crypttab_contains() {
     local luks="$1"
