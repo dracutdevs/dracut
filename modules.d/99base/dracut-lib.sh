0c807a3733c38d48413dcd8590ec138986cefdb4
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-02-24 22:24:23 +0000

    chore: resolve shellcheck for SC2116

--- modules.d/99base/dracut-lib.sh
+++ modules.d/99base/dracut-lib.sh
@@ -1065,10 +1065,6 @@ make_trace_mem() {
         return
     fi
 
-    # FIXME? useless echo?
-    # shellcheck disable=SC2116
-    msg=$(echo "$msg")
-
     msg_printed=0
     while [ $# -gt 0 ]; do
         trace=${1%%:*}
