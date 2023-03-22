ec2c7e1aa4591f4eeb0bb85aa51bf1914e18ff78
Laszlo Gombos <laszlo.gombos@gmail.com>
2023-03-22 19:56:25 +0000

    test(UEFI): add test case for UEFI boot
    Include OVMF in containers if it is not already present.

---
+++ test/TEST-18-UEFI/test-init.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+export PATH=/usr/sbin:/usr/bin:/sbin:/bin
+
+[ -e /proc/self/mounts ] \
+    || (mkdir -p /proc && mount -t proc -o nosuid,noexec,nodev proc /proc)
+
+grep -q '^sysfs /sys sysfs' /proc/self/mounts \
+    || (mkdir -p /sys && mount -t sysfs -o nosuid,noexec,nodev sysfs /sys)
+
+grep -q '^devtmpfs /dev devtmpfs' /proc/self/mounts \
+    || (mkdir -p /dev && mount -t devtmpfs -o mode=755,noexec,nosuid,strictatime devtmpfs /dev)
+
+grep -q '^tmpfs /run tmpfs' /proc/self/mounts \
+    || (mkdir -p /run && mount -t tmpfs -o mode=755,noexec,nosuid,strictatime tmpfs /run)
+
+: > /dev/watchdog
+
+exec > /dev/console 2>&1
+
+echo "made it to the rootfs! Powering down."
+echo "dracut-root-block-success" | dd oflag=direct,dsync of=/dev/sdb
+poweroff -f
