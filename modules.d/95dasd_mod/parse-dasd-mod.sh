08686015c4f5359f67c9bbfd1f3ccec2efebd652
Steffen Maier <maier@linux.ibm.com>
2023-03-23 18:22:56 +0100

    feat(dasd_mod): minimize dasd handling consolidated in s390-tools
    Parsing of rd.dasd is handled by s390-tools zdev dracut module 95zdev as of
    https://github.com/ibm-s390-linux/s390-tools/commit/99270236805972544932feab9692ee7122a343b8
    ("zdev/dracut: add rd.dasd cmdline option handling").
    
    Even though this removes one implementation of parsing rd.dasd in dracut,
    above s390-tools change introduces another implementation of parsing the
    exact same rd.dasd syntax. Therefore, it would be good to keep the
    documentation in man/dracut.cmdline.7 of dracut as one central place
    describing all s390 device types that dracut handles.
    
    95dasd/modules-setup.sh copies /etc/dasd.conf from root-fs into initrd.
    Retain the call of dasd_cio_free here so /etc/dasd.conf gets processed even
    with the typical cio_ignore kernel boot parameter ignoring most devices.
    
    Preparation for consolidating persistent configuration with zdev.
    
    Signed-off-by: Steffen Maier <maier@linux.ibm.com>

--- modules.d/95dasd_mod/parse-dasd-mod.sh
+++ modules.d/95dasd_mod/parse-dasd-mod.sh
@@ -1,18 +1,4 @@
 #!/bin/sh
-mod_args=""
-
-for dasd_arg in $(getargs rd.dasd= -d rd_DASD= DASD=); do
-    mod_args="$mod_args,$dasd_arg"
-done
-
-mod_args="${mod_args#*,}"
-
-if [ -x /sbin/dasd_cio_free -a -n "$mod_args" ]; then
-    [ -d /etc/modprobe.d ] || mkdir -m 0755 -p /etc/modprobe.d
-    echo "options dasd_mod dasd=$mod_args" >> /etc/modprobe.d/dasd_mod.conf
-fi
-
-unset dasd_arg
 if [ -x /sbin/dasd_cio_free ]; then
     dasd_cio_free
 fi
