c5a122c76ecfc725e4ca806f72e8c7e142af0b8a
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2023-07-18 12:26:38 +0200

    fix(caps): bash is already installed by the bash module

--- modules.d/02caps/module-setup.sh
+++ modules.d/02caps/module-setup.sh
@@ -17,8 +17,6 @@ install() {
     if ! dracut_module_included "systemd"; then
         inst_hook pre-pivot 00 "$moddir/caps.sh"
         inst "$(find_binary capsh 2> /dev/null)" /usr/sbin/capsh
-        # capsh wants bash and we need bash also
-        inst /bin/bash
     else
         dwarning "caps: does not work with systemd in the initramfs"
     fi
