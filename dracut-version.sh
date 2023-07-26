b4e23ce4238821b0c2dd3c846da45f9cfeb57954
Henrik Gombos <henrik99999@gmail.com>
2023-07-26 12:43:37 -0400

    fix(release): maintain dracut-version.sh in the source tree
    Update dracut-version.sh as part of release generation.

---
+++ dracut-version.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# shellcheck disable=SC2034
+DRACUT_VERSION=059
--- tools/release.sh
+++ tools/release.sh
@@ -41,10 +41,13 @@ cat NEWS_header.md NEWS_header_new.md NEWS_body_new.md NEWS_body_with_conttribut
 # message for https://github.com/dracutdevs/dracut/releases/tag
 cat -s NEWS_body_new.md CONTRIBUTORS.md > release.md
 
+# dracut-version.sh
+printf "#!/bin/sh\n# shellcheck disable=SC2034\nDRACUT_VERSION=%s\n" "$NEW_VERSION" > dracut-version.sh
+
 # Check in AUTHORS and NEWS.md
 git config user.name "Dracut Release Bot"
 git config user.email "<>"
-git commit -m "docs: update NEWS.md and AUTHORS" NEWS.md AUTHORS
+git commit -m "docs: update NEWS.md and AUTHORS" NEWS.md AUTHORS dracut-version.sh
 git push origin master
 git tag "$NEW_VERSION" -m "$NEW_VERSION"
 git push --tags
