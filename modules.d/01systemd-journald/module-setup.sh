64f85edde10b676b1f2fdebf20d21f137a814cb9
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2023-07-27 11:08:49 +0200

    fix(systemd-journald): add systemd-sysusers dependency

--- modules.d/01systemd-journald/module-setup.sh
+++ modules.d/01systemd-journald/module-setup.sh
@@ -19,6 +19,8 @@ check() {
 # Module dependency requirements.
 depends() {
 
+    # This module has external dependency on other module(s).
+    echo systemd-sysusers
     # Return 0 to include the dependent module(s) in the initramfs.
     return 0
 
