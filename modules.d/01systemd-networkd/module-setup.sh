2d3cc2fbe55ac7290886ea62b06311fe3e62ce0c
Henrik Gombos <henrik99999@gmail.com>
2023-08-07 16:04:11 -0400

    fix(systemd-networkd): remove dbus hard dependency

--- modules.d/01systemd-networkd/module-setup.sh
+++ modules.d/01systemd-networkd/module-setup.sh
@@ -22,7 +22,7 @@ check() {
 depends() {
 
     # This module has external dependency on other module(s).
-    echo dbus kernel-network-modules systemd-sysusers
+    echo kernel-network-modules systemd-sysusers
     # Return 0 to include the dependent module(s) in the initramfs.
     return 0
 
