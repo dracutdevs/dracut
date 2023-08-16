ac50bf91b4aa8493916fadbb03a707e820633121
Benjamin Drung <benjamin.drung@canonical.com>
2023-08-16 13:56:41 +0200

    test: skip test 98 if dracut-util is not available
    Building `dracut-util` requires the source code to be present and
    prevents running the test against the installed system. Check that
    `dracut-util` and rely on the test environment to build `dracut-util`
    beforehand.

--- test/TEST-98-GETARG/test.sh
+++ test/TEST-98-GETARG/test.sh
@@ -7,11 +7,13 @@
 TEST_DESCRIPTION="dracut getarg command"
 
 test_check() {
-    return 0
+    if ! [[ -x "$PKGLIBDIR/dracut-util" ]]; then
+        echo "Test needs dracut-util... Skipping"
+        return 1
+    fi
 }
 
 test_setup() {
-    make -C "$basedir" dracut-util
     ln -sfnr "$PKGLIBDIR"/dracut-util "$TESTDIR"/dracut-getarg
     ln -sfnr "$PKGLIBDIR"/dracut-util "$TESTDIR"/dracut-getargs
     ln -sfnr "$PKGLIBDIR"/modules.d/99base/dracut-lib.sh "$TESTDIR"/dracut-lib.sh
