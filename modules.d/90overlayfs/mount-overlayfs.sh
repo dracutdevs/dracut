bddffedae038ceca263a904e40513a6e92f1b558
David Cassany <dcassany@suse.com>
2023-09-22 16:28:48 +0200

    fix(overlayfs): split overlayfs mount in two steps
    This commit splits the creation of required overlayfs underlaying
    directories and the actual overlayfs mount. This way it is still
    possible to mount the overlayfs with the generated sysroot.mount that
    dmsquash-live creates.
    
    The overlayfs tree is created in a pre-mount hook so it is executed
    before sysroot.mount is started. Otherwise sysroot.mount starts and
    fails before mount hooks are executed.
    
    Signed-off-by: David Cassany <dcassany@suse.com>

--- modules.d/90overlayfs/mount-overlayfs.sh
+++ modules.d/90overlayfs/mount-overlayfs.sh
@@ -3,24 +3,11 @@
 type getarg > /dev/null 2>&1 || . /lib/dracut-lib.sh
 
 getargbool 0 rd.live.overlay.overlayfs && overlayfs="yes"
-getargbool 0 rd.live.overlay.reset -d -y reset_overlay && reset_overlay="yes"
 getargbool 0 rd.live.overlay.readonly -d -y readonly_overlay && readonly_overlay="--readonly" || readonly_overlay=""
 
 ROOTFLAGS="$(getarg rootflags)"
 
 if [ -n "$overlayfs" ]; then
-    if ! [ -e /run/rootfsbase ]; then
-        mkdir -m 0755 -p /run/rootfsbase
-        mount --bind "$NEWROOT" /run/rootfsbase
-    fi
-
-    mkdir -m 0755 -p /run/overlayfs
-    mkdir -m 0755 -p /run/ovlwork
-    if [ -n "$reset_overlay" ] && [ -h /run/overlayfs ]; then
-        ovlfsdir=$(readlink /run/overlayfs)
-        info "Resetting the OverlayFS overlay directory."
-        rm -r -- "${ovlfsdir:?}"/* "${ovlfsdir:?}"/.* > /dev/null 2>&1
-    fi
     if [ -n "$readonly_overlay" ] && [ -h /run/overlayfs-r ]; then
         ovlfs=lowerdir=/run/overlayfs-r:/run/rootfsbase
     else
