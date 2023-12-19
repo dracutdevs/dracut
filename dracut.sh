03cd454845346c206194c214f9b9ec15ec91d9b4
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2023-12-19 15:26:17 +0100

    feat(dracut.sh): protect `push_host_devs` function
    Check at least that the arguments are not an empty string.

--- dracut.sh
+++ dracut.sh
@@ -305,6 +305,7 @@ long_version() {
 push_host_devs() {
     local _dev
     for _dev in "$@"; do
+        [[ -z $_dev ]] && continue
         [[ " ${host_devs[*]} " == *" $_dev "* ]] && return
         host_devs+=("$_dev")
     done
