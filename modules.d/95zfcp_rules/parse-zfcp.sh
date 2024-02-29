dracutdevs/dracut

commit c8e531239bf314ae532ca1bc820285250a3b35d7

95zfcp_rules: Auto-generate udev rule for ipl device
We should auto-generate an udev rule for the ipl device; it'll be
enabled anyway.

Signed-off-by: Hannes Reinecke <hare@suse.de>

modules.d/95zfcp_rules/parse-zfcp.sh
@@ -39,6 +39,17 @@ EOF
     fi
 }
 
+if [[ -f /sys/firmware/ipl/ipl_type &&
+            $(</sys/firmare/ipl/ipl_type) = "fcp" ]] ; then
+    (
+        local _wwpn=$(cat /sys/firmware/ipl/wwpn)
+        local _lun=$(cat /sys/firmware/ipl/lun)
+        local _ccw=$(cat /sys/firmware/ipl/device)
+
+        create_udev_rule $_ccw $_wwpn $_lun
+    )
+fi
+
 for zfcp_arg in $(getargs rd.zfcp); do
     (
         IFS=","
@@ -49,6 +60,9 @@ done
 
 for zfcp_arg in $(getargs root=) $(getargs resume=); do
     (
+        local _wwpn
+        local _lun
+
         case $zfcp_arg in
             /dev/disk/by-path/ccw-*)
                 ccw_arg=${zfcp_arg##*/}
@@ -57,8 +71,8 @@ for zfcp_arg in $(getargs root=) $(getargs resume=); do
         if [ -n "$ccw_arg" ] ; then
             IFS="-"
             set -- $ccw_arg
-            wwpn=${4%:*}
-            lun=${4#*:}
+            _wwpn=${4%:*}
+            _lun=${4#*:}
             create_udev_rule $2 $wwpn $lun
         fi
     )

commit 829aac6c0499df0bff912fd59f6a6644dd85a2b4

fix(zfcp_rules): use the right wwpn and lun vars and support new device format
Commit c8e531239bf314ae532ca1bc820285250a3b35d7 introduced a regression that
causes the parsed `_wwpn` and `_lun` from SCSI device nodes to not be passed
to the `create_udev_rule` function.

Also, the format of these udev-created SCSI device nodes has changed, now it
looks like: `ccw-<device_bus_id>-fc-<wwpn>-lun-<lun>-part<n>`

Fixes c8e531239bf314ae532ca1bc820285250a3b35d7

modules.d/95zfcp_rules/parse-zfcp.sh
@@ -65,9 +65,18 @@ for zfcp_arg in $(getargs root=) $(getargs resume=); do
             IFS="-"
             set -- "$ccw_arg"
             IFS="$OLDIFS"
-            _wwpn=${4%:*}
-            _lun=${4#*:}
-            create_udev_rule "$2" "$wwpn" "$lun"
+            # The format of udev-created SCSI device nodes has changed from:
+            #   ccw-<device_bus_id>-zfcp-<wwpn>:<lun>-part<n>
+            # to:
+            #   ccw-<device_bus_id>-fc-<wwpn>-lun-<lun>-part<n>
+            if [[ $3 == "fc" ]] && [[ $5 == "lun" ]]; then
+                _wwpn="$4"
+                _lun="$6"
+            elif [[ $3 == "zfcp" ]]; then
+                _wwpn=${4%:*}
+                _lun=${4#*:}
+            fi
+            create_udev_rule "$2" "$_wwpn" "$_lun"
         fi
     )
 done