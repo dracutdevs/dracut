5b4466ef79b4eca14caae6894ea1fa18f6de02f0
Antonio Alvarez Feijoo <antonio.feijoo@suse.com>
2024-04-04 13:49:26 +0200

    fix(systemd): explicitly install some libs that will not be statically included
    Some required libraries that used to be statically included are in the process
    to be opened via `dlopen()`.
    
    References:
    - https://github.com/systemd/systemd/pull/31131
    - https://github.com/systemd/systemd/pull/31550
    - https://github.com/systemd/systemd/pull/32019
    
    Closes #2642

--- modules.d/00systemd/module-setup.sh
+++ modules.d/00systemd/module-setup.sh
@@ -262,6 +262,8 @@ EOF
     # Install library file(s)
     _arch=${DRACUT_ARCH:-$(uname -m)}
     inst_libdir_file \
+        {"tls/$_arch/",tls/,"$_arch/",}"libgcrypt.so*" \
+        {"tls/$_arch/",tls/,"$_arch/",}"libkmod.so*" \
         {"tls/$_arch/",tls/,"$_arch/",}"libnss_*"
 
 }
