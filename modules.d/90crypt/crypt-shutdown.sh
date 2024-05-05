26fb96c45644c50e09ce3336f3950fa1b646bada
Daniel Winzen <daniel@danwin1210.de>
2024-05-05 13:08:27 +0200

    fix(crypt): close device with systemd-cryptsetup if cryptsetup unavailable

--- modules.d/90crypt/crypt-shutdown.sh
+++ modules.d/90crypt/crypt-shutdown.sh
@@ -8,8 +8,8 @@ rm -f /run/udev/control
 # that the encryption keys can be released.
 dmsetup ls --target crypt | while read -r name _; do
     if ! type "cryptsetup" > /dev/null 2>&1; then
-        warn "cryptsetup not installed, skipping closing of encrypted devices"
-        return
+        systemd-cryptsetup detach "$name" deferred 2>&1 | vinfo
+    else
+        cryptsetup close "$name" --deferred 2>&1 | vinfo
     fi
-    cryptsetup close "$name" --deferred 2>&1 | vinfo
 done
